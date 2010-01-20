4a94246fd14f427a6d47732e49122ec9059094d4
Jed Schmidt <tr@nslator.jp>
2010-01-20 10:18:45 -0700

    added obj.calle check to isArguments, to prevent misfiring for typeof String

--- underscore.js
+++ underscore.js
@@ -488,7 +488,7 @@
 
   // Is a given variable an arguments object?
   _.isArguments = function(obj) {
-    return obj && _.isNumber(obj.length) && !_.isArray(obj) && !propertyIsEnumerable.call(obj, 'length');
+    return obj && obj.callee && _.isNumber(obj.length) && !_.isArray(obj) && !propertyIsEnumerable.call(obj, 'length');
   };
 
   // Is a given value a function?

b07c802766354976a1e112c9298d225c817ed2a4
Jeremy Ashkenas <jashkenas@gmail.com>
2010-10-07 10:49:10 -0400

    tweaking documentation, license mention

--- underscore.js
+++ underscore.js
@@ -1,6 +1,6 @@
 //     (c) 2010 Jeremy Ashkenas, DocumentCloud Inc.
-//     Underscore is freely distributable under the terms of the MIT license.
-//     Portions of Underscore are inspired by or borrowed from Prototype.js,
+//     Underscore is freely distributable under the MIT license.
+//     Portions of Underscore are inspired or borrowed from Prototype,
 //     Oliver Steele's Functional, and John Resig's Micro-Templating.
 //     For all details and documentation:
 //     http://documentcloud.github.com/underscore
@@ -368,7 +368,7 @@
     return range;
   };
 
-  // Function Functions
+  // Function (ahem) Functions
   // ------------------
 
   // Create a function bound to a given object (assigning `this`, and arguments,

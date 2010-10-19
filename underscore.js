31efd854f8ec754d663d87c7bcb329e5bc90b8ad
Jeremy Ashkenas <jashkenas@gmail.com>
2010-10-18 20:45:18 -0400

    removing now unused propertyIsEnumerable reference.

--- underscore.js
+++ underscore.js
@@ -23,11 +23,10 @@
   var ArrayProto = Array.prototype, ObjProto = Object.prototype;
 
   // Create quick reference variables for speed access to core prototypes.
-  var slice                 = ArrayProto.slice,
-      unshift               = ArrayProto.unshift,
-      toString              = ObjProto.toString,
-      hasOwnProperty        = ObjProto.hasOwnProperty,
-      propertyIsEnumerable  = ObjProto.propertyIsEnumerable;
+  var slice            = ArrayProto.slice,
+      unshift          = ArrayProto.unshift,
+      toString         = ObjProto.toString,
+      hasOwnProperty   = ObjProto.hasOwnProperty;
 
   // All **ECMAScript 5** native function implementations that we hope to use
   // are declared here.

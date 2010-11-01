da3edddef998ce7b5a6f19c21ab80e113d3d7f13
Jeremy Ashkenas <jashkenas@gmail.com>
2010-11-01 08:44:31 -0400

    Merging in Issue #54

--- underscore.js
+++ underscore.js
@@ -112,9 +112,7 @@
   _.reduceRight = _.foldr = function(obj, iterator, memo, context) {
     if (nativeReduceRight && obj.reduceRight === nativeReduceRight) {
       if (context) iterator = _.bind(iterator, context);
-      var args = [iterator];
-      if (memo !== undefined) args.push(memo);
-      return obj.reduceRight.apply(obj, args);
+      return memo !== void 0 ? obj.reduceRight(iterator, memo) : obj.reduceRight(iterator);
     }
     var reversed = (_.isArray(obj) ? obj.slice() : _.toArray(obj)).reverse();
     return _.reduce(reversed, iterator, memo, context);

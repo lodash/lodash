ebde2daa4a73bb0570e59ce3423e3e0bff96aff0
Adam Craven <git@adamcraven.me>
2011-02-20 21:07:14 +0100

    Updated 'bind' documentation to reflect native support.

--- underscore.js
+++ underscore.js
@@ -407,6 +407,7 @@
 
   // Create a function bound to a given object (assigning `this`, and arguments,
   // optionally). Binding with arguments is also known as `curry`.
+  // Delegates to **ECMAScript 5**'s native `Object.bind` if available.
   _.bind = function(func, obj) {
     var args = slice.call(arguments, 2);
     if(nativeBind && func.bind === nativeBind) return FuncProto.bind.apply(func, slice.call(arguments, 1));

727db393d5ad2584d96941d8b69e0b651308b694
Jeremy Ashkenas <jashkenas@gmail.com>
2011-07-12 17:01:04 -0400

    Amending length check as discussed in 942d63

--- underscore.js
+++ underscore.js
@@ -72,7 +72,7 @@
     if (obj == null) return;
     if (nativeForEach && obj.forEach === nativeForEach) {
       obj.forEach(iterator, context);
-    } else if (obj.length || obj.length === 0) {
+    } else if (obj.length === +obj.length) {
       for (var i = 0, l = obj.length; i < l; i++) {
         if (i in obj && iterator.call(context, obj[i], i, obj) === breaker) return;
       }

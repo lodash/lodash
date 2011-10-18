86eedd2a7b2d02c282ce130c508b0c4f4e494789
Jeremy Ashkenas <jashkenas@gmail.com>
2011-10-18 16:51:45 -0400

    Fixes #335, exporting for Titanium.

--- underscore.js
+++ underscore.js
@@ -48,13 +48,13 @@
   // Create a safe reference to the Underscore object for use below.
   var _ = function(obj) { return new wrapper(obj); };
 
-  // Export the Underscore object for **CommonJS**, with backwards-compatibility
-  // for the old `require()` API. If we're not in CommonJS, add `_` to the
-  // global object.
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = _;
-    _._ = _;
-  } else if (typeof exports !== 'undefined' && exports) {
+  // Export the Underscore object for **Node.js** and **"CommonJS"**, with
+  // backwards-compatibility for the old `require()` API. If we're not in
+  // CommonJS, add `_` to the global object.
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = _;
+    }
     exports._ = _;
   } else {
     // Exported as a string, for Closure Compiler "advanced" mode.

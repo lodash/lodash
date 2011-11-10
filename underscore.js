f04c6bf779b288cf7a2bcd68840d28fb253c3438
Dan Tillberg <dan@tillberg.us>
2011-11-10 11:55:21 -0500

    _.after(0, callback) now fires callback immediately

--- underscore.js
+++ underscore.js
@@ -595,9 +595,9 @@
 
   // Returns a function that will only be executed after being called N times.
   _.after = function(times, func) {
-    return function() {
+    return times ? function() {
       if (--times < 1) { return func.apply(this, arguments); }
-    };
+    } : func();
   };
 
   // Object Functions

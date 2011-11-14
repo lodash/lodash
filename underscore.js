f29785a0b7871ddbc6dd86da233ae9031d57c857
Jeremy Ashkenas <jashkenas@gmail.com>
2011-11-14 14:27:44 -0500

    cleaning up _.last implementation.

--- underscore.js
+++ underscore.js
@@ -335,7 +335,11 @@
   // Get the last element of an array. Passing **n** will return the last N
   // values in the array. The **guard** check allows it to work with `_.map`.
   _.last = function(array, n, guard) {
-    return (n != null) && !guard ? slice.call(array, Math.max(array.length - n, 0)) : array[array.length - 1];
+    if ((n != null) && !guard) {
+      return slice.call(array, Math.max(array.length - n, 0));
+    } else {
+      return array[array.length - 1];
+    }
   };
 
   // Returns everything but the first entry of the array. Aliased as `tail`.

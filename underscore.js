5c6abc4453f3bdf9470bd98ec79d7f22105f87e3
Brad Dunbar <dunbarb2@gmail.com>
2012-01-30 10:15:04 -0500

    short circuit `isEmpty` for null/undefined

--- underscore.js
+++ underscore.js
@@ -761,6 +761,7 @@
   // Is a given array, string, or object empty?
   // An "empty" object has no enumerable own-properties.
   _.isEmpty = function(obj) {
+    if (obj == null) return true;
     if (_.isArray(obj) || _.isString(obj)) return obj.length === 0;
     for (var key in obj) if (_.has(obj, key)) return false;
     return true;

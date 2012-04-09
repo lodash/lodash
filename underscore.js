e80bbe6d03193743a8d3e3d2d8bb365451382de8
Jeremy Ashkenas <jashkenas@gmail.com>
2012-04-09 14:37:49 -0400

    Underscore.js 1.3.2

--- underscore.js
+++ underscore.js
@@ -1,4 +1,4 @@
-//     Underscore.js 1.3.1
+//     Underscore.js 1.3.2
 //     (c) 2009-2012 Jeremy Ashkenas, DocumentCloud Inc.
 //     Underscore is freely distributable under the MIT license.
 //     Portions of Underscore are inspired or borrowed from Prototype,
@@ -62,7 +62,7 @@
   }
 
   // Current version.
-  _.VERSION = '1.3.1';
+  _.VERSION = '1.3.2';
 
   // Collection Functions
   // --------------------

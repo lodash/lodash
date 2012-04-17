3619b04b06d56fa564fcd6ef6074c90d4eefaac1
John-David Dalton <john.david.dalton@gmail.com>
2012-04-16 23:48:53 -0400

    low dash: Cleanup repo and add submodules. [jddalton]

---
+++ doc/parse.php
@@ -0,0 +1,35 @@
+<?php
+
+  // cleanup requested filepath
+  $file = isset($_GET['f']) ? $_GET['f'] : 'lowdash';
+  $file = preg_replace('#(\.*[\/])+#', '', $file);
+  $file .= preg_match('/\.[a-z]+$/', $file) ? '' : '.js';
+
+  // output filename
+  if (isset($_GET['o'])) {
+    $output = $_GET['o'];
+  } else if (isset($_SERVER['argv'][1])) {
+    $output = $_SERVER['argv'][1];
+  } else {
+    $output = basename($file);
+  }
+
+  /*--------------------------------------------------------------------------*/
+
+  require('../vendor/docdown/docdown.php');
+
+  // generate Markdown
+  $markdown = docdown(array(
+    'path' => '../' . $file,
+    'title' => 'LowDash.js <sup>v0.1.0</sup>',
+    'url'  => 'https://github.com/bestiejs/lowdash/blob/master/lowdash.js'
+  ));
+
+  // save to a .md file
+  file_put_contents($output . '.md', $markdown);
+
+  // print
+  header('Content-Type: text/plain;charset=utf-8');
+  echo $markdown . PHP_EOL;
+
+?>

88327aeba2279b38b8a9978c15a59fb5dc3fb4ec
John-David Dalton <john.david.dalton@gmail.com>
2012-05-20 05:26:39 -0400

    Use nano.jar in perf/index.html.
    Former-commit-id: a5a3d79dd2c940dd654d2c91cf80665d72b22080

--- perf/run-perf.sh
+++ perf/run-perf.sh
@@ -1,7 +1,9 @@
 cd "$(dirname "$0")"
 for cmd in rhino ringo narwhal node; do
+	echo ""
 	echo "Running performance suite in $cmd..."
 	$cmd perf.js
 done
-echo "\nRunning performance suite in a browser..."
+echo ""
+echo "Running performance suite in a browser..."
 open index.html
--- test/run-test.sh
+++ test/run-test.sh
@@ -1,7 +1,9 @@
 cd "$(dirname "$0")"
 for cmd in rhino ringo narwhal node; do
+	echo ""
 	echo "Testing in $cmd..."
 	$cmd test.js
 done
+echo ""
 echo "Testing in a browser..."
 open index.html

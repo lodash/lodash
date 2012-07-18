ebc724e9e5e0a32ef8991f6aa0049f25faef576b
John-David Dalton <john.david.dalton@gmail.com>
2012-07-18 01:53:15 -0400

    Ensure `reEvaluateDelimiter` is assigned an initial value if `evaluateDelimiter` is undefined by default. [closes #52]
    Former-commit-id: d7344ace8b1deb9875d197d833e80edf4d2141a5

--- test/test.js
+++ test/test.js
@@ -77,6 +77,16 @@
   QUnit.module('lodash');
 
   (function() {
+    // ensure this test is executed before any other template tests to avoid false positives
+    test('should initialize `reEvaluateDelimiter` correctly (test with production build)', function() {
+      var data = { 'a': [1, 2] },
+          settings = _.templateSettings;
+
+      _.templateSettings = { 'interpolate': /\{\{(.+?)\}\}/g };
+      equal(_.template('{{ a.join(",") }}', data), '1,2');
+      _.templateSettings = settings;
+    });
+
     test('supports loading lodash.js as the "lodash" module', function() {
       if (window.document && window.require) {
         equal((lodashModule || {}).moduleName, 'lodash');

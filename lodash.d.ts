f572bb0f917b98b9314e4e0b1666f54c5b0166c3
jden <jason@denizac.org>
2012-10-03 03:29:43 -0700

    scope functions for requrn type

--- lodash.d.ts
+++ lodash.d.ts
@@ -5,6 +5,107 @@ interface ArrayLike {
     length: number;
 }
 
+interface lodashProto {
+    // aliases
+    drop(array: Array, n?: number): Array;
+    head(array: Array): any;
+    head(array: Array, n: number): Array;
+    methods(object: Object): string[];
+    tail(array: Array, n?: number): Array;
+    take(array: Array): any;
+    take(array: Array, n: number): Array;
+    unique(array: Array, isSorted?: bool, callback?: (any) => any, thisArg?: any): Array;
+    unique(array: Array, callback?: (any) => any, thisArg?: any): Array;
+    // functions
+    after(n: number, func: Function): Function;
+    bind(func: Function, thisArg?: any, ...args: any[]): Function;
+    bindAll(object: Object, ...methodNames: string[]): Object;
+    chain(value: any): Object;
+    clone(value: any, deep?: bool): any;
+    compact(array: Array): Array;
+    compose(...funcs: Function[]): Function;
+    debounce(func: Function, wait: number, immediate?: bool): Function;
+    defaults(object: Object, ...defaults: Object[]): Object;
+    defer(func: Function, args: any[]): number;
+    delay(func: Function, wait: number, ...args: any[]): number;
+    difference(array: Array, ...arrays: Array[]): Array;
+    escape(string): string;
+    extend(object: Object, ...sources: Object[]): Object;
+    first(array: Array): any;
+    first(array: Array, n: number): Array;
+    flatten(array: Array, shallow?: bool): Array;
+    forIn(object: Object, callback: ObjectIterator, thisArg?: any): Object;
+    forOwn(object: Object, callback: ObjectIterator, thisArg?: any): Object;
+    functions(object: Object): string[];
+    has(object: Object, property: string): bool;
+    identity(value: any): any;
+    indexOf(array: Array, value: any, fromIndex?: number): number;
+    indexOf(array: Array, value: any, fromIndex?: bool): number;
+    initial(array: Array, n?: number): Array;
+    intersection(...arrays: Array[]): Array;
+    invert(object: Object): Object;
+    isArguments: Predicate;
+    isArray: Predicate;
+    isBoolean: Predicate;
+    isDate: Predicate;
+    isElement: Predicate;
+    isEmpty: Predicate;
+    isEqual(a: any, b: any): bool;
+    isFinite: Predicate;
+    isFunction: Predicate;
+    isNaN: Predicate;
+    isNull: Predicate;
+    isNumber: Predicate;
+    isObject: Predicate;
+    isPlainObject: Predicate;
+    isRegExp: Predicate;
+    isString: Predicate;
+    isUndefined: Predicate;
+    keys(object: Object): string[];
+    last(array: Array): any;
+    last(array: Array, n: number): Array;
+    lastIndexOf(array: Array, value: any, fromIndex?: number): number;
+    lateBind(object: Object, methodName: string, ...args: any[]): Function;
+    max(array: Array, callback?: ArrayIterator, thisArg?: any): any;
+    memoize(func: Function, resolver?: Function): Function;
+    merge(object: Object, ...sources: Object[]): Object;
+    min(array: Array, callback?: ArrayIterator, thisArg?: any): any;
+    mixin(object: Object): void;
+    noConflict(): lodashStatic;
+    object(keys: Array, values?: Array): Object;
+    omit(object: Object, callback: (any) => bool, thisArg?: any): Object;
+    omit(object: Object, ...props: string[]): Object;
+    once(func: Function): Function;
+    pairs(object: Object): Array[];
+    partial(func, ...args: any[]): Function;
+    pick(object: Object, callback: (any) => bool, thisArg?: any): Object;
+    pick(object: Object, ...props: string[]): Object;
+    random(min?: number, max?: number): number;
+    range(start: number, end: number, step?: number): number[];
+    range(end: number, step?: number): number[];
+    rest(array: Array, n?: number): Array;
+    result(object: Object, property: string): any;
+    shuffle(array: Array): Array;
+    sortedIndex(array: Array, value: any, callback?: (value: any) => number, thisArg?: any): number;
+    sortedIndex(array: Array, value: any, property?: string): number;
+    tap(value: any, interceptor: Function): any;
+    template(text: string): (data: Object, options?: Object) => string;
+    template(text: string, data: Object, options: Object): string;
+    throttle(func: Function, wait: number): Function;
+    times(n: number, callback: (index: number) => any, thisArg?: any): Array;
+    unescape(string: string): string;
+    union(...arrays: Array[]): Array;
+    uniq(array: Array, isSorted?: bool, callback?: (any) => any, thisArg?: any): Array;
+    uniq(array: Array, callback?: (any) => any, thisArg?: any): Array;
+    values(object: Object): Array;
+    uniqueId(): number;
+    uniqueId(prefix: string): string;
+    without(array: Array, ...values: any[]): Array;
+    wrap(value: any, wrapper : Function): Function;
+    zip(...arrays: Array[]): Array;
+
+}
+
 
 interface lodashStatic {
     // aliases
@@ -189,8 +290,8 @@ interface lodashStatic {
     toArray(collection: string): Array;
     unescape(string: string): string;
     union(...arrays: Array[]): Array;
-    unique(array: Array, isSorted?: bool, callback?: (any) => any, thisArg?: any): Array;
-    unique(array: Array, callback?: (any) => any, thisArg?: any): Array;
+    uniq(array: Array, isSorted?: bool, callback?: (any) => any, thisArg?: any): Array;
+    uniq(array: Array, callback?: (any) => any, thisArg?: any): Array;
     values(object: Object): Array;
     uniqueId(): number;
     uniqueId(prefix: string): string;
@@ -202,6 +303,175 @@ interface lodashStatic {
     wrap(value: any, wrapper : Function): Function;
     zip(...arrays: Array[]): Array;
 }
+
+interface lodashCurried {
+        // aliases
+    all(callback?: ArrayIterator, thisArg?: any): bool;
+    all(callback?: ObjectIterator, thisArg?: any): bool;
+    all(callback?: StringIterator, thisArg?: any): bool;
+    any(callback?: ArrayIterator, thisArg?: any): bool;
+    any(callback?: ObjectIterator, thisArg?: any): bool;
+    any(callback?: StringIterator, thisArg?: any): bool;
+    collect(callback?: ArrayIterator, thisArg?: any): Array;
+    collect(callback?: ObjectIterator, thisArg?: any): Array;
+    collect(callback?: StringIterator, thisArg?: any): Array;
+    detect(callback: ArrayIterator, thisArg?: any): any;
+    detect(callback: ObjectIterator, thisArg?: any): any;
+    detect(callback: StringIterator, thisArg?: any): any;
+    drop(n?: number): Array;
+    each(callback: ArrayIterator, thisArg?: any): Array;
+    each(callback: ObjectIterator, thisArg?: any): Object;
+    each(callback: StringIterator, thisArg?: any): string;
+    foldl(callback: ArrayAccumulator, accumulator?: any, thisArg?: any): any;
+    foldl(callback: ObjectAccumulator, accumulator?: any, thisArg?: any): any;
+    foldl(callback: StringAccumulator, accumulator?: any, thisArg?: any): any;
+    foldr(callback: ArrayAccumulator, accumulator?: any, thisArg?: any): any;
+    foldr(callback: ObjectAccumulator, accumulator?: any, thisArg?: any): any;
+    foldr(callback: StringAccumulator, accumulator?: any, thisArg?: any): any;
+    head(): any;
+    head(n: number): Array;
+    include(target: any): bool;
+    inject(callback: ArrayAccumulator, accumulator?: any, thisArg?: any): any;
+    inject(callback: ObjectAccumulator, accumulator?: any, thisArg?: any): any;
+    inject(callback: StringAccumulator, accumulator?: any, thisArg?: any): any;
+    methods(): string[];
+    select(callback?: ArrayIterator, thisArg?: any): Array;
+    select(callback?: ObjectIterator, thisArg?: any): Array;
+    select(callback?: StringIterator, thisArg?: any): Array;
+    tail(n?: number): Array;
+    take(): any;
+    take(n: number): Array;
+    unique(isSorted?: bool, callback?: (any) => any, thisArg?: any): Array;
+    unique(callback?: (any) => any, thisArg?: any): Array;
+    // functions
+    after(n: number, func: Function): Function;
+    bind(thisArg?: any, ...args: any[]): Function;
+    bindAll(...methodNames: string[]): Object;
+    chain(): Object;
+    clone(deep?: bool): any;
+    compact(): Array;
+    compose(...funcs: Function[]): Function;
+    contains(target: any): bool;
+    countBy(callback: Function): Object;
+    countBy(property: string): Object;
+    debounce(wait: number, immediate?: bool): Function;
+    defaults(...defaults: Object[]): Object;
+    defer(args: any[]): number;
+    delay(wait: number, ...args: any[]): number;
+    difference(...arrays: Array[]): Array;
+    escape(): string;
+    every(callback?: ArrayIterator, thisArg?: any): bool;
+    every(callback?: ObjectIterator, thisArg?: any): bool;
+    every(callback?: StringIterator, thisArg?: any): bool;
+    extend(...sources: Object[]): Object;
+    filter(callback?: ArrayIterator, thisArg?: any): Array;
+    filter(callback?: ObjectIterator, thisArg?: any): Array;
+    filter(callback?: StringIterator, thisArg?: any): Array;
+    find(callback: ArrayIterator, thisArg?: any): any;
+    find(callback: ObjectIterator, thisArg?: any): any;
+    find(callback: StringIterator, thisArg?: any): any;
+    first(): any;
+    first(n: number): Array;
+    flatten(shallow?: bool): Array;
+    forEach(callback: ArrayIterator, thisArg?: any): Array;
+    forEach(callback: ObjectIterator, thisArg?: any): Object;
+    forEach(callback: StringIterator, thisArg?: any): string;
+    forIn(callback: ObjectIterator, thisArg?: any): Object;
+    forOwn(callback: ObjectIterator, thisArg?: any): Object;
+    functions(): string[];
+    groupBy(callback: Function): Object;
+    groupBy(property: string): Object;
+    has(property: string): bool;
+    identity(): any;
+    indexOf(value: any, fromIndex?: number): number;
+    indexOf(value: any, fromIndex?: bool): number;
+    initial(n?: number): Array;
+    intersection(...arrays: Array[]): Array;
+    invert(): Object;
+    invoke(...args: any[]): Array;
+    invoke(func: Function, ...args: any[]): Array;
+    invoke(methodName: string, ...args: any[]): Array;
+    isArguments: CurriedPredicate;
+    isArray: CurriedPredicate;
+    isBoolean: CurriedPredicate;
+    isDate: CurriedPredicate;
+    isElement: CurriedPredicate;
+    isEmpty: CurriedPredicate;
+    isEqual(a: any, b: any): bool;
+    isFinite: CurriedPredicate;
+    isFunction: CurriedPredicate;
+    isNaN: CurriedPredicate;
+    isNull: CurriedPredicate;
+    isNumber: CurriedPredicate;
+    isObject: CurriedPredicate;
+    isPlainObject: CurriedPredicate;
+    isRegExp: CurriedPredicate;
+    isString: CurriedPredicate;
+    isUndefined: CurriedPredicate;
+    keys(): string[];
+    last(): any;
+    last(n: number): Array;
+    lastIndexOf(value: any, fromIndex?: number): number;
+    lateBind(methodName: string, ...args: any[]): Function;
+    map(callback?: ArrayIterator, thisArg?: any): Array;
+    map(callback?: ObjectIterator, thisArg?: any): Array;
+    map(callback?: StringIterator, thisArg?: any): Array;
+    max(callback?: ArrayIterator, thisArg?: any): any;
+    memoize(resolver?: Function): Function;
+    merge(...sources: Object[]): Object;
+    min(callback?: ArrayIterator, thisArg?: any): any;
+    mixin(object: Object): void;
+    noConflict(): lodashStatic;
+    object(keys: Array, values?: Array): Object;
+    omit(callback: (any) => bool, thisArg?: any): Object;
+    omit(...props: string[]): Object;
+    once(): Function;
+    pairs(): Array[];
+    partial(...args: any[]): Function;
+    pick(callback: (any) => bool, thisArg?: any): Object;
+    pick(...props: string[]): Object;
+    pluck(property: string): Array;
+    random(min?: number, max?: number): number;
+    range(start: number, end: number, step?: number): number[];
+    range(end: number, step?: number): number[];
+    reduce(callback: ArrayAccumulator, accumulator?: any, thisArg?: any): any;
+    reduce(callback: ObjectAccumulator, accumulator?: any, thisArg?: any): any;
+    reduce(callback: StringAccumulator, accumulator?: any, thisArg?: any): any;
+    reduceRight(callback: ArrayAccumulator, accumulator?: any, thisArg?: any): any;
+    reduceRight(callback: ObjectAccumulator, accumulator?: any, thisArg?: any): any;
+    reduceRight(callback: StringAccumulator, accumulator?: any, thisArg?: any): any;
+    reject(callback?: Predicate, thisArg?: any): Array;
+    rest(array: Array, n?: number): Array;
+    result(property: string): any;
+    shuffle(array: Array): Array;
+    size(): number;
+    some(callback?: ArrayIterator, thisArg?: any): bool;
+    some(callback?: ObjectIterator, thisArg?: any): bool;
+    some(callback?: StringIterator, thisArg?: any): bool;
+    sortBy(callback: Function, thisArg?: any): Object;
+    sortBy(property: string): Object;
+    sortedIndex(value: any, callback?: (value: any) => number, thisArg?: any): number;
+    sortedIndex(value: any, property?: string): number;
+    tap(value: any, interceptor: Function): any;
+    template(text: string): (data: Object, options?: Object) => string;
+    template(text: string, data: Object, options: Object): string;
+    throttle(func: Function, wait: number): Function;
+    times(n: number, callback: (index: number) => any, thisArg?: any): Array;
+    toArray(): Array;
+    unescape(string: string): string;
+    union(...arrays: Array[]): Array;
+    uniq(isSorted?: bool, callback?: (any) => any, thisArg?: any): Array;
+    uniq(callback?: (any) => any, thisArg?: any): Array;
+    uniqueId(): number;
+    uniqueId(prefix: string): string;
+    values(properties: Object): Array;
+    where(properties: Object): Array;
+    without(...values: any[]): Array;
+    wrap(): Function;
+    zip(...arrays: Array[]): Array;
+}
+
+
 
 interface ArrayAccumulator {
     (accumulator: any, value: any, index: number, collection: Array): any;
@@ -219,6 +489,10 @@ interface Predicate {
     (value: any): bool;
 }
 
+interface CurriedPredicate {
+    (): bool;
+}
+
 interface ArrayIterator {
     (value: any, index: number, collection: Array): any;
 }
@@ -232,39 +506,11 @@ interface StringIterator {
 }
 
 
-
-// Arrays
-interface lodashArraysStatic {
-
-}
-
-// Chaining
-
-// Collections
-
-// Functions
-
-// Objects
-
-// Utilities
-
-
-
-interface lodash extends lodashStatic {
+interface lodash extends lodashProto, lodashStatic {
     (value: Array): lodashCurried;
     (value: Object): lodashCurried;
     (value: string): lodashCurried;
     VERISON: string;
 }
 
-interface lodashCurried {
-
-}
-
-
-
-
 declare var _: lodash;
-
-_.any([], a => a % 2 === 0);
-

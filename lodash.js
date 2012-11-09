607eec6ff051f884e6d0e122e3e248e3bad0a6e8
John-David Dalton <john.david.dalton@gmail.com>
2012-11-09 04:58:41 -0800

    Bump to v0.9.2.
    Former-commit-id: cd4b7bd3db92f07e0ac1af74ee70bd6525f36c55

--- lodash.js
+++ lodash.js
@@ -1,5 +1,5 @@
 /*!
- * Lo-Dash v0.9.1 <http://lodash.com>
+ * Lo-Dash v0.9.2 <http://lodash.com>
  * (c) 2012 John-David Dalton <http://allyoucanleet.com/>
  * Based on Underscore.js 1.4.2 <http://underscorejs.org>
  * (c) 2009-2012 Jeremy Ashkenas, DocumentCloud Inc.
@@ -4052,7 +4052,7 @@
    * @memberOf _
    * @type String
    */
-  lodash.VERSION = '0.9.1';
+  lodash.VERSION = '0.9.2';
 
   // assign static methods
   lodash.after = after;

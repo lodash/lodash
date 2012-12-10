0b48b9c7d4c00d4b163942f17f189e57afdbeac3
John-David Dalton <john.david.dalton@gmail.com>
2012-12-10 00:25:07 -0800

    Optimize `_.isArray` fallback, `_.isDate`, `_.isFunction`, and `_.isRegExp`.
    Former-commit-id: a3e350bf1fa91a9582ba6a0effe61d4589cc8afe

--- lodash.js
+++ lodash.js
@@ -1184,7 +1184,7 @@
    * // => true
    */
   var isArray = nativeIsArray || function(value) {
-    return toString.call(value) == arrayClass;
+    return value instanceof Array || toString.call(value) == arrayClass;
   };
 
   /**
@@ -1218,7 +1218,7 @@
    * // => true
    */
   function isDate(value) {
-    return toString.call(value) == dateClass;
+    return value instanceof Date || toString.call(value) == dateClass;
   }
 
   /**
@@ -1476,7 +1476,7 @@
   // fallback for older versions of Chrome and Safari
   if (isFunction(/x/)) {
     isFunction = function(value) {
-      return toString.call(value) == funcClass;
+      return value instanceof Function || toString.call(value) == funcClass;
     };
   }
 
@@ -1626,7 +1626,7 @@
    * // => true
    */
   function isRegExp(value) {
-    return toString.call(value) == regexpClass;
+    return value instanceof RegExp || toString.call(value) == regexpClass;
   }
 
   /**

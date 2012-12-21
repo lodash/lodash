1b347fc185cc47baa5b81b52af5b0bc749340271
John-David Dalton <john.david.dalton@gmail.com>
2012-12-21 12:38:55 -0600

    Tweak `_.uniqueId` to avoid problems with buggy minifiers.
    Former-commit-id: e940c336b227ce89661cd6ada5f3e722a0204318

--- vendor/underscore/underscore.js
+++ vendor/underscore/underscore.js
@@ -1075,7 +1075,7 @@
   // Useful for temporary DOM ids.
   var idCounter = 0;
   _.uniqueId = function(prefix) {
-    var id = '' + (++idCounter);
+    var id = ++idCounter + '';
     return prefix ? prefix + id : id;
   };
 

e97882b1860a1001ea640cc75bdb544472e52162
John-David Dalton <john.david.dalton@gmail.com>
2013-01-29 22:43:56 -0800

    Update vendors.
    Former-commit-id: 55a30b9cbfe7c513dd13d709aa4552fe715c262b

--- vendor/backbone/test/events.js
+++ vendor/backbone/test/events.js
@@ -101,6 +101,38 @@ $(document).ready(function() {
     b.trigger('event event2');
   });
 
+  test("listenToOnce and stopListening", 1, function() {
+    var a = _.extend({}, Backbone.Events);
+    var b = _.extend({}, Backbone.Events);
+    a.listenToOnce(b, 'all', function() { ok(true); });
+    b.trigger('anything');
+    b.trigger('anything');
+    a.listenToOnce(b, 'all', function() { ok(false); });
+    a.stopListening();
+    b.trigger('anything');
+  });
+
+  test("listenTo, listenToOnce and stopListening", 1, function() {
+    var a = _.extend({}, Backbone.Events);
+    var b = _.extend({}, Backbone.Events);
+    a.listenToOnce(b, 'all', function() { ok(true); });
+    b.trigger('anything');
+    b.trigger('anything');
+    a.listenTo(b, 'all', function() { ok(false); });
+    a.stopListening();
+    b.trigger('anything');
+  });
+
+  test("listenTo and stopListening with event maps", 1, function() {
+    var a = _.extend({}, Backbone.Events);
+    var b = _.extend({}, Backbone.Events);
+    a.listenTo(b, {change: function(){ ok(true); }});
+    b.trigger('change');
+    a.listenTo(b, {change: function(){ ok(false); }});
+    a.stopListening();
+    b.trigger('change');
+  });
+
   test("listenTo yourself", 1, function(){
     var e = _.extend({}, Backbone.Events);
     e.listenTo(e, "foo", function(){ ok(true); });
@@ -259,18 +291,6 @@ $(document).ready(function() {
     obj.trigger('x y');
   });
 
-  test("off is chainable", 3, function() {
-    var obj = _.extend({}, Backbone.Events);
-    // With no events
-    ok(obj.off() === obj);
-    // When removing all events
-    obj.on('event', function(){}, obj);
-    ok(obj.off() === obj);
-    // When removing some events
-    obj.on('event', function(){}, obj);
-    ok(obj.off('event') === obj);
-  });
-
   test("#1310 - off does not skip consecutive events", 0, function() {
     var obj = _.extend({}, Backbone.Events);
     obj.on('event', function() { ok(false); }, obj);
@@ -398,6 +418,23 @@ $(document).ready(function() {
 
   test("once without a callback is a noop", 0, function() {
     _.extend({}, Backbone.Events).once('event').trigger('event');
+  });
+
+  test("event functions are chainable", function() {
+    var obj = _.extend({}, Backbone.Events);
+    var obj2 = _.extend({}, Backbone.Events);
+    var fn = function() {};
+    equal(obj, obj.trigger('noeventssetyet'));
+    equal(obj, obj.off('noeventssetyet'));
+    equal(obj, obj.stopListening('noeventssetyet'));
+    equal(obj, obj.on('a', fn));
+    equal(obj, obj.once('c', fn));
+    equal(obj, obj.trigger('a'));
+    equal(obj, obj.listenTo(obj2, 'a', fn));
+    equal(obj, obj.listenToOnce(obj2, 'b', fn));
+    equal(obj, obj.off('a c'));
+    equal(obj, obj.stopListening(obj2, 'a'));
+    equal(obj, obj.stopListening());
   });
 
 });

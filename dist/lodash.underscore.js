578743617794748a850c44ca57b7e3330248f27e
John-David Dalton <john.david.dalton@gmail.com>
2013-02-05 22:54:18 -0800

    Don't remove `nonEnumArgs` fixes for `mobile` builds and cleanup debug builds.
    Former-commit-id: b1fbb7ed362d0c94a92e4b12e0385c77973654b3

--- dist/lodash.underscore.js
+++ dist/lodash.underscore.js
@@ -74,7 +74,6 @@
   var ceil = Math.ceil,
       concat = arrayRef.concat,
       floor = Math.floor,
-      getPrototypeOf = reNative.test(getPrototypeOf = Object.getPrototypeOf) && getPrototypeOf,
       hasOwnProperty = objectRef.hasOwnProperty,
       push = arrayRef.push,
       toString = objectRef.toString;
@@ -420,54 +419,6 @@
       };
     }
     return func;
-  }
-
-  /**
-   * Creates compiled iteration functions.
-   *
-   * @private
-   * @param {Object} [options1, options2, ...] The compile options object(s).
-   *  arrays - A string of code to determine if the iterable is an array or array-like.
-   *  useHas - A boolean to specify using `hasOwnProperty` checks in the object loop.
-   *  args - A string of comma separated arguments the iteration function will accept.
-   *  top - A string of code to execute before the iteration branches.
-   *  loop - A string of code to execute in the object loop.
-   *  bottom - A string of code to execute after the iteration branches.
-   *
-   * @returns {Function} Returns the compiled function.
-   */
-  function createIterator() {
-    var data = {
-      // support properties
-
-      // iterator options
-      'arrays': 'isArray(iterable)',
-      'bottom': '',
-      'loop': '',
-      'top': '',
-      'useHas': true
-    };
-
-    // merge options into a template data object
-    for (var object, index = 0; object = arguments[index]; index++) {
-      for (var key in object) {
-        data[key] = object[key];
-      }
-    }
-    var args = data.args;
-    data.firstArg = /^[^,]+/.exec(args)[0];
-
-    // create the function factory
-    var factory = Function(
-        'createCallback, hasOwnProperty, isArguments, isArray, isString, ' +
-        'objectTypes, nativeKeys',
-      'return function(' + args + ') {\n' + (data) + '\n}'
-    );
-    // return the compiled function
-    return factory(
-      createCallback, hasOwnProperty, isArguments, isArray, isString,
-      objectTypes, nativeKeys
-    );
   }
 
   /**

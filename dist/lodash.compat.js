2d4a9050417f84f2fdaadb62466d628d190ea426
John-David Dalton <john.david.dalton@gmail.com>
2013-03-03 14:06:51 -0800

    Ensure callback shorthands work with `_.uniq`. [\ht @jden, @motowilliams]
    Former-commit-id: d9a0e688414edc4167b5f8e9ca123bab587f8264

--- dist/lodash.compat.js
+++ dist/lodash.compat.js
@@ -3943,7 +3943,7 @@
           seen = result;
 
       // juggle arguments
-      if (typeof isSorted == 'function') {
+      if (typeof isSorted != 'boolean' && isSorted != null) {
         thisArg = callback;
         callback = isSorted;
         isSorted = false;
@@ -3953,7 +3953,7 @@
       if (isLarge) {
         var cache = {};
       }
-      if (callback) {
+      if (callback != null) {
         seen = [];
         callback = createCallback(callback, thisArg);
       }

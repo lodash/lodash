2f0948957ba82e8ffba58ec000aa8571ff76ee75
John-David Dalton <john.david.dalton@gmail.com>
2013-03-09 18:15:32 -0800

    Cleanup test.js and perf.js and add `_(array)` benchmark.
    Former-commit-id: bcd120c9ee332db39c7b637894751f2861bac069

--- perf/run-perf.sh
+++ perf/run-perf.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 echo "Running performance suite in node..."
 node perf.js ../dist/lodash.js && node perf.js ../dist/lodash.min.js
 
-for cmd in rhino narwhal ringo phantomjs; do
+for cmd in rhino "rhino -require" narwhal ringo phantomjs; do
   echo ""
   echo "Running performance suite in $cmd..."
   $cmd perf.js ../dist/lodash.compat.js && $cmd perf.js ../dist/lodash.compat.min.js

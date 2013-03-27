212b05c658de7bc7851c8a1d87159503313f1523
John-David Dalton <john.david.dalton@gmail.com>
2013-03-26 20:46:02 -0700

    Ensure `underscore` build internal `forOwn` will accept a `thisArg` argument. [closes #220].

--- dist/lodash.backbone.js
+++ dist/lodash.backbone.js
@@ -645,7 +645,6 @@
     var index, iterable = collection, result = iterable;
     if (!iterable) return result;
     if (!objectTypes[typeof iterable]) return result;
-    callback || (callback = identity);
 
       for (index in iterable) {
         if (callback(iterable[index], index, collection) === indicatorObject) return result;    
@@ -678,7 +677,6 @@
     var index, iterable = collection, result = iterable;
     if (!iterable) return result;
     if (!objectTypes[typeof iterable]) return result;
-    callback || (callback = identity);
 
       for (index in iterable) {
         if (hasOwnProperty.call(iterable, index)) {    
@@ -1451,15 +1449,15 @@
     var index = -1,
         length = collection ? collection.length : 0;
 
+    callback = callback && typeof thisArg == 'undefined' ? callback : createCallback(callback, thisArg);
     if (typeof length == 'number') {
-      callback = callback && typeof thisArg == 'undefined' ? callback : createCallback(callback, thisArg);
       while (++index < length) {
         if (callback(collection[index], index, collection) === indicatorObject) {
           break;
         }
       }
     } else {
-      forOwn(collection, callback, thisArg);
+      forOwn(collection, callback);
     };
   }
 

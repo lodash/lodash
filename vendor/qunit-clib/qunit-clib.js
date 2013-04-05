7f637e4acc680041b563c957c0acc0484d92e71f
John-David Dalton <john.david.dalton@gmail.com>
2013-04-05 09:04:01 -0700

    Update builds, vendors, and docs.
    Former-commit-id: 67aec5a64de61ac2b8948b31315395a727c10071

--- vendor/qunit-clib/qunit-clib.js
+++ vendor/qunit-clib/qunit-clib.js
@@ -10,9 +10,9 @@
   /** Detect free variable `exports` */
   var freeExports = typeof exports == 'object' && exports;
 
-  /** Detect free variable `global` and use it as `window` */
+  /** Detect free variable `global`, from Node.js or Browserified code, and use it as `window` */
   var freeGlobal = typeof global == 'object' && global;
-  if (freeGlobal.global === freeGlobal) {
+  if (freeGlobal.global === freeGlobal || freeGlobal.window === freeGlobal) {
     window = freeGlobal;
   }
 

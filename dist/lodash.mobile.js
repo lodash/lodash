baa07758dc426bb986dffa235259f6c1622012d9
John-David Dalton <john.david.dalton@gmail.com>
2013-04-28 16:48:02 -0700

    Ensure the `_.isRegExp` is correct for various builds.

--- dist/lodash.mobile.js
+++ dist/lodash.mobile.js
@@ -1752,7 +1752,7 @@
      * // => true
      */
     function isRegExp(value) {
-      return value ? (typeof value == 'object' && toString.call(value) == regexpClass) : false;
+      return value ? (objectTypes[typeof value] && toString.call(value) == regexpClass) : false;
     }
 
     /**
--- lodash.js
+++ lodash.js
@@ -1932,7 +1932,7 @@
      * // => true
      */
     function isRegExp(value) {
-      return value ? (typeof value == 'object' && toString.call(value) == regexpClass) : false;
+      return value ? (objectTypes[typeof value] && toString.call(value) == regexpClass) : false;
     }
 
     /**

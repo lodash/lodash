6de2b7d331dc515c3c70aef8e942616484009e23
John-David Dalton <john.david.dalton@gmail.com>
2013-05-30 03:38:55 -0400

    Move istanbul to travis.yml.
    Former-commit-id: 6d38406eafbfc880c0b5aabf853c987c65f03482

---
+++ test/run-rest.sh
@@ -0,0 +1,18 @@
+cd "$(dirname "$0")"
+
+for cmd in rhino "rhino -require" narwhal ringo phantomjs; do
+  echo "Testing in $cmd..."
+  $cmd test.js ../dist/lodash.compat.js && $cmd test.js ../dist/lodash.compat.min.js
+  echo ""
+done
+
+echo "Testing in node..."
+node test.js ../dist/lodash.js && node test.js ../dist/lodash.min.js
+
+echo ""
+echo "Testing build..."
+node test-build.js
+
+echo ""
+echo "Testing in a browser..."
+open index.html

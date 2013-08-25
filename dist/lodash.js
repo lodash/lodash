d47cac563a0d55c342242789b2fb54296c9c7cf9
John-David Dalton <john.david.dalton@gmail.com>
2013-08-25 01:39:18 -0700

    Ensure `cacheIndexOf` works correctly with boolean values.
    Former-commit-id: 21f65952b14ddfff892a8c151545ad807a8ec9ef

--- dist/lodash.js
+++ dist/lodash.js
@@ -172,17 +172,17 @@
     cache = cache.cache;
 
     if (type == 'boolean' || value == null) {
-      return cache[value];
+      return cache[value] ? 0 : -1;
     }
     if (type != 'number' && type != 'string') {
       type = 'object';
     }
     var key = type == 'number' ? value : keyPrefix + value;
-    cache = cache[type] || (cache[type] = {});
+    cache = (cache = cache[type]) && cache[key];
 
     return type == 'object'
-      ? (cache[key] && baseIndexOf(cache[key], value) > -1 ? 0 : -1)
-      : (cache[key] ? 0 : -1);
+      ? (cache && baseIndexOf(cache, value) > -1 ? 0 : -1)
+      : (cache ? 0 : -1);
   }
 
   /**

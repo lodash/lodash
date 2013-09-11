b5bd57b32513b10490b26ef8352f835b5148a0be
John-David Dalton <john.david.dalton@gmail.com>
2013-09-11 00:18:23 -0700

    Update Underscore version in copyright header. [ci skip]

--- lodash.js
+++ lodash.js
@@ -2,7 +2,7 @@
  * @license
  * Lo-Dash 1.3.1 <http://lodash.com/>
  * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
- * Based on Underscore.js 1.5.1 <http://underscorejs.org/LICENSE>
+ * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
  * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <http://lodash.com/license>
  */

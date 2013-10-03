4977fa2a7cb16a631588919eac5cc4f27de7beb6
John-David Dalton <john.david.dalton@gmail.com>
2013-10-02 23:21:41 -0700

    Make doc/parse.php read the version from package.json. [ci skip]

--- doc/parse.php
+++ doc/parse.php
@@ -1,36 +1,38 @@
 <?php
 
-  // cleanup requested filepath
-  $file = isset($_GET['f']) ? $_GET['f'] : 'lodash';
-  $file = preg_replace('#(\.*[\/])+#', '', $file);
-  $file .= preg_match('/\.[a-z]+$/', $file) ? '' : '.js';
+  // cleanup requested file path
+  $filePath = isset($_GET['f']) ? $_GET['f'] : 'lodash';
+  $filePath = preg_replace('#(\.*[\/])+#', '', $filePath);
+  $filePath .= preg_match('/\.[a-z]+$/', $filePath) ? '' : '.js';
 
   // output filename
   if (isset($_GET['o'])) {
-    $output = $_GET['o'];
+    $outputName = $_GET['o'];
   } else if (isset($_SERVER['argv'][1])) {
-    $output = $_SERVER['argv'][1];
+    $outputName = $_SERVER['argv'][1];
   } else {
-    $output = basename($file);
+    $outputName = basename($filePath);
   }
 
   /*--------------------------------------------------------------------------*/
 
   require('../vendor/docdown/docdown.php');
 
+  // get package version
+  $version = json_decode(file_get_contents('../package.json'))->version;
+
   // generate Markdown
   $markdown = docdown(array(
-    'path'  => '../' . $file,
-    'title' => 'Lo-Dash <span>v2.2.0</span>',
+    'path'  => '../' . $filePath,
+    'title' => 'Lo-Dash <span>v' . $version . '</span>',
     'toc'   => 'categories',
     'url'   => 'https://github.com/lodash/lodash/blob/master/lodash.js'
   ));
 
-  // save to a .md file
-  file_put_contents($output . '.md', $markdown);
+  // save to a `.md` file
+  file_put_contents($outputName . '.md', $markdown);
 
   // print
   header('Content-Type: text/plain;charset=utf-8');
   echo $markdown . PHP_EOL;
-
 ?>

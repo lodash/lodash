cd9332cddf10c75f6bcdd5be27cd74244b27e6cb
John-David Dalton <john.david.dalton@gmail.com>
2013-10-18 22:18:15 -0700

    Add comment to `_.parseInt`. [ci skip]

--- lodash.js
+++ lodash.js
@@ -6053,7 +6053,7 @@
      * // => 8
      */
     var parseInt = nativeParseInt(whitespace + '08') == 8 ? nativeParseInt : function(value, radix) {
-      // Firefox and Opera still follow the ES3 specified implementation of `parseInt`
+      // Firefox < 21 and Opera < 15 follow the ES3 specified implementation of `parseInt`
       return nativeParseInt(isString(value) ? value.replace(reLeadingSpacesAndZeros, '') : value, radix || 0);
     };
 

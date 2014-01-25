2cf64a4996fbec1fedf36c95da60ef00688aac4a
John-David Dalton <john.david.dalton@gmail.com>
2014-01-25 02:52:14 -0800

    Tweak documented arg name `key` for `_.pluck`. [ci skip]

--- lodash.js
+++ lodash.js
@@ -4063,7 +4063,7 @@
      * @type Function
      * @category Collections
      * @param {Array|Object|string} collection The collection to iterate over.
-     * @param {string} prop The name of the property to pluck.
+     * @param {string} key The name of the property to pluck.
      * @returns {Array} Returns a new array of property values.
      * @example
      *

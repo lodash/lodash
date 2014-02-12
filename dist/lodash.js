048486506f63be8b6367069f80f3986f123922b3
John-David Dalton <john.david.dalton@gmail.com>
2014-02-11 17:53:17 -0800

    Update Underscore version in doc header. [ci skip]

--- dist/lodash.js
+++ dist/lodash.js
@@ -3,7 +3,7 @@
  * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
  * Build: `lodash modern -o ./dist/lodash.js`
  * Copyright 2012-2014 The Dojo Foundation <http://dojofoundation.org/>
- * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
+ * Based on Underscore.js 1.6.0 <http://underscorejs.org/LICENSE>
  * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <http://lodash.com/license>
  */

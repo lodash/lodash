1bdf74951a9cb4cf96a1b2fbcbcb16458c651428
Benjamin Tan <demoneaux@gmail.com>
2014-03-25 11:30:06 +0800

    Simplify `_.chain`

--- lodash.js
+++ lodash.js
@@ -784,7 +784,7 @@
      *
      * @private
      * @param {*} value The value to wrap in a `lodash` instance.
-     * @param {boolean} [chainAll=false] A flag to enable chaining for all methods
+     * @param {boolean} [chainAll=false] A flag to enable chaining for all methods.
      * @returns {Object} Returns a `lodash` instance.
      */
     function lodashWrapper(value, chainAll) {
@@ -3446,9 +3446,7 @@
      * // => 'pebbles is 1'
      */
     function chain(value) {
-      value = new lodashWrapper(value);
-      value.__chain__ = true;
-      return value;
+      return new lodashWrapper(value, true);
     }
 
     /**

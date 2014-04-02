9f2bea8ac18fdf86ffdbf385a3c2dbf1b442b5f8
John-David Dalton <john.david.dalton@gmail.com>
2014-04-02 01:42:43 -0700

    Wrap `support.nonEnumArgs` assignment in a try-catch to avoid errors in Ringo, Rhino, and Narwhal.

--- lodash.js
+++ lodash.js
@@ -876,21 +876,6 @@
       support.funcNames = typeof Function.name == 'string';
 
       /**
-       * Detect if `arguments` object indexes are non-enumerable
-       * (Firefox < 4, IE < 9, PhantomJS, Safari < 5.1).
-       *
-       * Chrome < 25 and Node.js < 0.11.0 will treat `arguments` object indexes
-       * that exceed their function's formal parameters and whose associated
-       * values are `0` as non-enumerable and incorrectly return `false` from
-       * `Object#hasOwnProperty`.
-       *
-       * @memberOf _.support
-       * @type boolean
-       */
-      support.nonEnumArgs = !(argsKey == '1' && hasOwnProperty.call(arguments, '1') &&
-        propertyIsEnumerable.call(arguments, '1'));
-
-      /**
        * Detect if string indexes are non-enumerable
        * (IE < 9, RingoJS, Rhino, Narwhal).
        *
@@ -971,6 +956,25 @@
         support.nodeClass = !(toString.call(document) == objectClass && !({ 'toString': 0 } + ''));
       } catch(e) {
         support.nodeClass = true;
+      }
+
+      /**
+       * Detect if `arguments` object indexes are non-enumerable
+       * (Firefox < 4, IE < 9, PhantomJS, Safari < 5.1).
+       *
+       * Chrome < 25 and Node.js < 0.11.0 will treat `arguments` object indexes
+       * that exceed their function's formal parameters and whose associated
+       * values are `0` as non-enumerable and incorrectly return `false` from
+       * `Object#hasOwnProperty`.
+       *
+       * @memberOf _.support
+       * @type boolean
+       */
+      try {
+        support.nonEnumArgs = !(argsKey == '1' && hasOwnProperty.call(arguments, '1') &&
+          propertyIsEnumerable.call(arguments, '1'));
+      } catch(e) {
+        support.nonEnumArgs = true;
       }
     }(0, 0));
 

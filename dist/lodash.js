f40c8f95e666216a7c67780b033a9604f15227a0
John-David Dalton <john.david.dalton@gmail.com>
2014-04-07 09:24:36 -0700

    Rebuild dist.

--- dist/lodash.js
+++ dist/lodash.js
@@ -3360,7 +3360,7 @@
      *
      * @name valueOf
      * @memberOf _
-     * @alias value
+     * @alias value, toJSON
      * @category Chaining
      * @returns {*} Returns the wrapped value.
      * @example
@@ -8184,6 +8184,7 @@
 
     // add "Chaining" functions to the wrapper
     lodash.prototype.chain = wrapperChain;
+    lodash.prototype.toJSON = wrapperValueOf;
     lodash.prototype.toString = wrapperToString;
     lodash.prototype.value = wrapperValueOf;
     lodash.prototype.valueOf = wrapperValueOf;

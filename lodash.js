c555681c8bb3aa871fe4261a252d5f19dbc668fd
John-David Dalton <john.david.dalton@gmail.com>
2014-05-28 15:35:00 -0400

    Add extra object in doc examples of `_.assign` and `_.defaults`. [ci skip]

--- lodash.js
+++ lodash.js
@@ -5910,15 +5910,15 @@
      * @returns {Object} Returns the destination object.
      * @example
      *
-     * _.assign({ 'name': 'fred' }, { 'employer': 'slate' });
-     * // => { 'name': 'fred', 'employer': 'slate' }
+     * _.assign({ 'name': 'fred' }, { 'age': 40 }, { 'employer': 'slate' });
+     * // => { 'name': 'fred', 'age': 40, 'employer': 'slate' }
      *
      * var defaults = _.partialRight(_.assign, function(value, other) {
      *   return typeof value == 'undefined' ? other : value;
      * });
      *
-     * defaults({ 'name': 'barney' }, { 'name': 'fred', 'employer': 'slate' });
-     * // => { 'name': 'barney', 'employer': 'slate' }
+     * defaults({ 'name': 'barney' }, { 'age': 36 }, { 'name': 'fred', 'employer': 'slate' });
+     * // => { 'name': 'barney', 'age': 36, 'employer': 'slate' }
      */
     var assign = createAssigner(baseAssign);
 
@@ -6086,8 +6086,8 @@
      * @returns {Object} Returns the destination object.
      * @example
      *
-     * _.defaults({ 'name': 'barney' }, { 'name': 'fred', 'employer': 'slate' });
-     * // => { 'name': 'barney', 'employer': 'slate' }
+     * _.defaults({ 'name': 'barney' }, { 'age': 36 }, { 'name': 'fred', 'employer': 'slate' });
+     * // => { 'name': 'barney', 'age': 36, 'employer': 'slate' }
      */
     function defaults(object) {
       if (!object) {

6ad7a20c077f7a21de51109a7a5c97a54376e34d
John-David Dalton <john.david.dalton@gmail.com>
2014-08-26 23:56:15 -0700

    Update Underscore build compat to 1.7.0.

--- lodash.js
+++ lodash.js
@@ -2,7 +2,7 @@
  * @license
  * Lo-Dash 3.0.0-pre <http://lodash.com/>
  * Copyright 2012-2014 The Dojo Foundation <http://dojofoundation.org/>
- * Based on Underscore.js 1.6.0 <http://underscorejs.org/LICENSE>
+ * Based on Underscore.js 1.7.0 <http://underscorejs.org/LICENSE>
  * Copyright 2009-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <http://lodash.com/license>
  */

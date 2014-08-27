1d82d591d3a6a8be224633038910d48ef06d44a5
John-David Dalton <john.david.dalton@gmail.com>
2014-08-26 21:36:04 -0700

    Fix tests for `rhino -require`.

--- test/asset/weakmap.js
+++ test/asset/weakmap.js
@@ -12,6 +12,9 @@
   /** Used as a reference to the global object */
   var root = (objectTypes[typeof window] && window) || this;
 
+  /** Detect free variable `exports` */
+  var freeExports = objectTypes[typeof exports] && exports && !exports.nodeType && exports;
+
   /** Detect free variable `global` from Node.js or Browserified code and use it as `root` */
   var freeGlobal = objectTypes[typeof global] && global;
   if (freeGlobal && (freeGlobal.global === freeGlobal || freeGlobal.window === freeGlobal || freeGlobal.self === freeGlobal)) {
@@ -21,56 +24,68 @@
   /*--------------------------------------------------------------------------*/
 
   /**
-   * Creates a `WeakMap` object.
+   * Installs `WeakMap` on the given `context` object.
+   *
+   * @memberOf exports
+   * @param {Object} context The context object.
    */
-  function WeakMap() {
-    // no-op
+  function runInContext(context) {
+
+    /**
+     * Creates a `WeakMap` object.
+     */
+    function WeakMap() {
+      // no-op
+    }
+
+    /**
+     * Gets the value associated with the given key.
+     *
+     * @memberOf WeakMap
+     * @param {Object} key The key object.
+     * @returns {*} Returns the associated value, else `undefined`.
+     */
+    function get(key) {
+      return key.__weakmap__;
+    }
+
+    /**
+     * Sets a value for the given key.
+     *
+     * @memberOf WeakMap
+     * @param {Object} key The key object.
+     * @param {*} value The value to set.
+     */
+    function set(key, value) {
+      key.__weakmap__ = value;
+      return this;
+    }
+
+    /**
+     * Produces the `toString` result of `WeakMap`.
+     *
+     * @static
+     * @memberOf WeakMap
+     * @returns {string} Returns the string result.
+     */
+    function toString() {
+      return nativeString;
+    }
+
+    WeakMap.toString = toString;
+    WeakMap.prototype.get = get;
+    WeakMap.prototype.set = set;
+
+    if (!root.WeakMap) {
+      context.WeakMap = WeakMap;
+    }
   }
 
   /*--------------------------------------------------------------------------*/
 
-  /**
-   * Gets the value associated with the given key.
-   *
-   * @memberOf WeakMap
-   * @param {Object} key The key object.
-   * @returns {*} Returns the associated value, else `undefined`.
-   */
-  function get(key) {
-    return key.__weakmap__;
-  }
-
-  /**
-   * Sets a value for the given key.
-   *
-   * @memberOf WeakMap
-   * @param {Object} key The key object.
-   * @param {*} value The value to set.
-   */
-  function set(key, value) {
-    key.__weakmap__ = value;
-    return this;
-  }
-
-  /**
-   * Produces the `toString` result of `WeakMap`.
-   *
-   * @static
-   * @memberOf WeakMap
-   * @returns {string} Returns the string result.
-   */
-  function toString() {
-    return nativeString;
-  }
-
-  /*--------------------------------------------------------------------------*/
-
-  WeakMap.toString = toString;
-  WeakMap.prototype.get = get;
-  WeakMap.prototype.set = set;
-
-  // expose `WeakMap`
-  if (!root.WeakMap) {
-    root.WeakMap = WeakMap;
+  if (freeExports) {
+    freeExports.runInContext = runInContext;
+  } else {
+    runInContext(root);
   }
 }.call(this));

7cf40576dd5fc6f46f67e98884201e4524af5aea
John-David Dalton <john.david.dalton@gmail.com>
2014-11-19 19:32:51 -0800

    Uncomment Android now that Sauce has fixed their issue.

--- test/saucelabs.js
+++ test/saucelabs.js
@@ -102,8 +102,8 @@ var browserNameMap = {
 
 /** List of platforms to load the runner on. */
 var platforms = [
-  //['Linux', 'android', '4.3'],
-  //['Linux', 'android', '4.0'],
+  ['Linux', 'android', '4.3'],
+  ['Linux', 'android', '4.0'],
   ['Windows 8.1', 'firefox', '33'],
   ['Windows 8.1', 'firefox', '32'],
   ['Windows 8.1', 'firefox', '20'],

3f9a878b03ecf6409ecb267acb9b349b3cfffbf1
John-David Dalton <john.david.dalton@gmail.com>
2014-11-28 14:23:40 -0800

    Correct `name` jsdoc entries. [ci skip]

--- lodash.js
+++ lodash.js
@@ -1449,7 +1449,7 @@
      * Removes `key` and its value from the cache.
      *
      * @private
-     * @name get
+     * @name delete
      * @memberOf _.memoize.Cache
      * @param {string} key The key of the value to remove.
      * @returns {boolean} Returns `true` if the entry was removed successfully, else `false`.
@@ -5031,7 +5031,7 @@
      *
      * **Note:** This method mutates the wrapped array.
      *
-     * @name chain
+     * @name reverse
      * @memberOf _
      * @category Chain
      * @returns {Object} Returns the new reversed `lodash` object.

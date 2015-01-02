42eb180b6cfb5ad708f312800f4c9fafa0afcf10
John-David Dalton <john.david.dalton@gmail.com>
2015-01-02 10:57:11 -0600

    Avoid unnecessary coercion of `radix` in `_.parseInt`.

--- lodash.js
+++ lodash.js
@@ -9444,7 +9444,11 @@
         // Firefox < 21 and Opera < 15 follow ES3 for `parseInt` and
         // Chrome fails to trim leading <BOM> whitespace characters.
         // See https://code.google.com/p/v8/issues/detail?id=3109.
-        radix = (guard && isIterateeCall(string, radix, guard)) ? 0 : +radix;
+        if (guard ? isIterateeCall(string, radix, guard) : radix == null) {
+          radix = 0;
+        } else if (radix) {
+          radix = +radix;
+        }
         string = trim(string);
         return nativeParseInt(string, radix || (reHexPrefix.test(string) ? 16 : 10));
       };

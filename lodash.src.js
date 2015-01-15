c7c791001a0b041006d10e6c864732735e5ba220
John-David Dalton <john.david.dalton@gmail.com>
2015-01-15 09:35:58 -0800

    Correct `_.toPlainObject` docs & add examples. [ci skip]

--- lodash.src.js
+++ lodash.src.js
@@ -8339,13 +8339,27 @@
     }
 
     /**
-     * Converts `value` to a plain object.
+     * Converts `value` to a plain object flattening inherited enumerable
+     * properties of `value` to own properties of the plain object.
      *
      * @static
      * @memberOf _
      * @category Lang
      * @param {*} value The value to convert.
-     * @returns {Object} Returns the converted object.
+     * @returns {Object} Returns the converted plain object.
+     * @example
+     *
+     * function Foo() {
+     *   this.b = 2;
+     * }
+     *
+     * Foo.prototype.c = 3;
+     *
+     * _.assign({ 'a': 1 }, new Foo);
+     * // => { 'a': 1, 'b': 2 }
+     *
+     * _.assign({ 'a': 1 }, _.toPlainObject(new Foo));
+     * // => { 'a': 1, 'b': 2, 'c': 3 }
      */
     function toPlainObject(value) {
       return (isArray(value) || isArguments(value) || isTypedArray(value))

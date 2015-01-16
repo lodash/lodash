1826cc8870ca2fa153be6202ba3201840b9c7375
John-David Dalton <john.david.dalton@gmail.com>
2015-01-15 20:08:06 -0800

    Cleanup `_.isPlainObject` docs. [ci skip]

--- lodash.src.js
+++ lodash.src.js
@@ -8192,8 +8192,8 @@
     }
 
     /**
-     * Checks if `value` is an object created by the `Object` constructor or has
-     * a `[[Prototype]]` of `null`.
+     * Checks if `value` is a plain object, that is, an object created by the
+     * `Object` constructor or one with a `[[Prototype]]` of `null`.
      *
      * **Note:** This method assumes objects created by the `Object` constructor
      * have no inherited enumerable properties.

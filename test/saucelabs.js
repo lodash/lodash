b825e3e66fd68a3e1b1fda243ad50706c6a93252
John-David Dalton <john.david.dalton@gmail.com>
2015-01-22 09:22:46 -0800

    Update tested version of Firefox in saucelabs.js.

--- test/saucelabs.js
+++ test/saucelabs.js
@@ -104,8 +104,8 @@ var browserNameMap = {
 var platforms = [
   ['Linux', 'android', '4.3'],
   ['Linux', 'android', '4.0'],
+  ['Windows 8.1', 'firefox', '35'],
   ['Windows 8.1', 'firefox', '34'],
-  ['Windows 8.1', 'firefox', '33'],
   ['Windows 8.1', 'firefox', '20'],
   ['Windows 8.1', 'chrome', '39'],
   ['Windows 8.1', 'chrome', '38'],

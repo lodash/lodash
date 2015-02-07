e91a662491146da1c25e25e4da234c8bcafd00b3
Nicolas Chambrier <naholyr@gmail.com>
2015-02-07 20:28:34 +0100

    Add _.spread.

--- lodash.src.js
+++ lodash.src.js
@@ -7580,6 +7580,45 @@
     }
 
     /**
+     * Creates a function accepting an array as argument, that invokes `func`
+     * with arguments taken from this array, like `Function#apply`.
+     * This works obviously well with `Promise.all`.
+     *
+     * @static
+     * @memberOf _
+     * @category Function
+     * @param {Function} The function to alter.
+     * @param {*} [thisArg] The `this` binding of `func`.
+     * @returns {*} Returns the new function.
+     * @example
+     *
+     * var spread = _.spread(function (who, what) {
+     *   return who + ' says ' + what;
+     * });
+     *
+     * spread(['John', 'hello']) // => 'John says hello'
+     *
+     * // With Promise
+     * var numbers = Promise.all([
+     *   Promise.resolve(42),
+     *   Promise.resolve(33)
+     * ])
+     *
+     * numbers.then(_.spread(function (a, b) { return a + b })) // Promise of 75
+     * // instead of...
+     * numbers.then(function (nums) { return nums[0] + nums[1] })
+     */
+    function spread (func, thisArg) {
+      if (typeof func !== 'function') {
+        throw new TypeError(FUNC_ERROR_TEXT);
+      }
+
+      return function(array) {
+        return func.apply(thisArg, array);
+      };
+    }
+
+    /**
      * Creates a function that only invokes `func` at most once per every `wait`
      * milliseconds. The created function comes with a `cancel` method to cancel
      * delayed invocations. Provide an options object to indicate that `func`
@@ -10752,6 +10791,7 @@
     lodash.slice = slice;
     lodash.sortBy = sortBy;
     lodash.sortByAll = sortByAll;
+    lodash.spread = spread;
     lodash.take = take;
     lodash.takeRight = takeRight;
     lodash.takeRightWhile = takeRightWhile;

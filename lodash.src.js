b1366696c41acba7c9e6e458fcdc3471a7057480
jdalton <john.david.dalton@gmail.com>
2015-03-03 09:18:17 -0800

    Simplify guard check in `_.sortByAll`.

--- lodash.src.js
+++ lodash.src.js
@@ -6909,8 +6909,10 @@
       if (collection == null) {
         return [];
       }
-      var args = arguments;
-      if (args.length > 3 && isIterateeCall(args[1], args[2], args[3])) {
+      var args = arguments,
+          guard = args[3];
+
+      if (guard && isIterateeCall(args[1], args[2], guard)) {
         args = [collection, args[1]];
       }
       return baseSortByOrder(collection, baseFlatten(args, false, false, 1), []);

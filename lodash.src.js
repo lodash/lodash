9a8345d4f9f73e3fd38f021a5b6fe90d367a71ab
h7lin <i@h7lin.com>
2015-03-03 11:59:00 +0800

    Fix doc example in `_.partition`. [ci skip]

--- lodash.src.js
+++ lodash.src.js
@@ -6489,7 +6489,7 @@
      * _.partition([1.2, 2.3, 3.4], function(n) {
      *   return this.floor(n) % 2;
      * }, Math);
-     * // => [[1, 3], [2]]
+     * // => [[1.2, 3.4], [2.3]]
      *
      * var users = [
      *   { 'user': 'barney',  'age': 36, 'active': false },

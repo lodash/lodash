90dc45d7fc89940ed38b773069fb87b9b61fb21d
jdalton <john.david.dalton@gmail.com>
2015-04-16 00:36:13 -0700

    Bump to v3.4.2.

--- lodash.add/index.js
+++ lodash.add/index.js
@@ -1,9 +1,9 @@
 /**
- * lodash 3.4.1 (Custom Build) <https://lodash.com/>
- * Build: `lodash modern modularize exports="npm" -o ./`
- * Copyright 2012-2015 The Dojo Foundation <http://dojofoundation.org/>
+ * lodash 3.4.2 (Custom Build) <https://lodash.com/>
+ * Build: `lodash modularize exports="npm" -o ./`
+ * Copyright 2012-2016 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
- * Copyright 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Copyright 2009-2016 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <https://lodash.com/license>
  */
 
@@ -13,16 +13,23 @@
  * @static
  * @memberOf _
  * @category Math
- * @param {number} augend The first number to add.
- * @param {number} addend The second number to add.
- * @returns {number} Returns the sum.
+ * @param {number} augend The first number in an addition.
+ * @param {number} addend The second number in an addition.
+ * @returns {number} Returns the total.
  * @example
  *
  * _.add(6, 4);
  * // => 10
  */
 function add(augend, addend) {
-  return (+augend || 0) + (+addend || 0);
+  var result;
+  if (augend !== undefined) {
+    result = augend;
+  }
+  if (addend !== undefined) {
+    result = result === undefined ? addend : (result + addend);
+  }
+  return result;
 }
 
 module.exports = add;

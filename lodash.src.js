5f845aa6f84528138f10114ed0c346b5787855db
jdalton <john.david.dalton@gmail.com>
2015-05-22 22:08:29 -0700

    Optimize `baseIsEqual`.

--- lodash.src.js
+++ lodash.src.js
@@ -2285,7 +2285,7 @@
       if (value === other) {
         return true;
       }
-      if (value == null || other == null || (!isObject(value) && !isObject(other))) {
+      if (value == null || other == null || (!isObject(value) && !isObjectLike(other))) {
         return value !== value && other !== other;
       }
       return baseIsEqualDeep(value, other, baseIsEqual, customizer, isLoose, stackA, stackB);

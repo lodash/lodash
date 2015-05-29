79990cbb27c290a0f6588fc5b31d2379f313121c
jdalton <john.david.dalton@gmail.com>
2015-05-29 06:24:02 -0400

    Remove `baseIsFunction`.

--- lodash.src.js
+++ lodash.src.js
@@ -350,20 +350,6 @@
   }
 
   /**
-   * The base implementation of `_.isFunction` without support for environments
-   * with incorrect `typeof` results.
-   *
-   * @private
-   * @param {*} value The value to check.
-   * @returns {boolean} Returns `true` if `value` is correctly classified, else `false`.
-   */
-  function baseIsFunction(value) {
-    // Avoid a Chakra JIT bug in compatibility modes of IE 11.
-    // See https://github.com/jashkenas/underscore/issues/1621 for more details.
-    return typeof value == 'function' || false;
-  }
-
-  /**
    * Converts `value` to a string if it's not one. An empty string is returned
    * for `null` or `undefined` values.
    *

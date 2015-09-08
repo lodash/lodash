8bad4ae636fe3011a213cc9d28dda4ca345e0804
John-David Dalton <john.david.dalton@gmail.com>
2015-09-07 23:35:06 -0700

    Rename `multiValue` param to `multiVal`. [ci skip]

--- lodash.js
+++ lodash.js
@@ -9474,13 +9474,13 @@
     /**
      * Creates an object composed of the inverted keys and values of `object`.
      * If `object` contains duplicate values, subsequent values overwrite property
-     * assignments of previous values unless `multiValue` is `true`.
+     * assignments of previous values unless `multiVal` is `true`.
      *
      * @static
      * @memberOf _
      * @category Object
      * @param {Object} object The object to invert.
-     * @param {boolean} [multiValue] Allow multiple values per key.
+     * @param {boolean} [multiVal] Allow multiple values per key.
      * @param- {Object} [guard] Enables use as an iteratee for functions like `_.map`.
      * @returns {Object} Returns the new inverted object.
      * @example
@@ -9490,14 +9490,14 @@
      * _.invert(object);
      * // => { '1': 'c', '2': 'b' }
      *
-     * // with `multiValue`
+     * // with `multiVal`
      * _.invert(object, true);
      * // => { '1': ['a', 'c'], '2': ['b'] }
      */
-    function invert(object, multiValue, guard) {
+    function invert(object, multiVal, guard) {
       return arrayReduce(keys(object), function(result, key) {
         var value = object[key];
-        if (multiValue && !guard) {
+        if (multiVal && !guard) {
           if (hasOwnProperty.call(result, value)) {
             result[value].push(key);
           } else {

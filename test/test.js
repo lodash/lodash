c604be31e0dbc07ecd2b40c347ccfc6816a7431c
John-David Dalton <john.david.dalton@gmail.com>
2015-09-09 08:31:28 -0700

    Add `_.flip` test.

--- test/test.js
+++ test/test.js
@@ -4663,6 +4663,23 @@
 
   /*--------------------------------------------------------------------------*/
 
+  QUnit.module('lodash.flip');
+
+  (function() {
+    function fn() {
+      return slice.call(arguments);
+    }
+
+    QUnit.test('should flip arguments provided to `func`', function(assert) {
+      assert.expect(1);
+
+      var flipped = _.flip(fn);
+      assert.deepEqual(flipped('a', 'b', 'c'), ['c', 'b', 'a']);
+    });
+  }());
+
+  /*--------------------------------------------------------------------------*/
+
   QUnit.module('lodash.take');
 
   (function() {

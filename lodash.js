e42d29611c3bfa68fec67a4a5a801f5146f9e870
John-David Dalton <john.david.dalton@gmail.com>
2015-09-11 18:22:34 -0700

    Ensure wrapper actions are copied in `wrapperClone`.

--- lodash.js
+++ lodash.js
@@ -4479,7 +4479,7 @@
         return wrapper.clone();
       }
       var result = new LodashWrapper(wrapper.__wrapped__, wrapper.__chain__);
-      result.__actions__ = wrapper.__actions__;
+      result.__actions__ = copyArray(wrapper.__actions__);
       result.__index__  = wrapper.__index__;
       result.__values__ = wrapper.__values__;
       return result;

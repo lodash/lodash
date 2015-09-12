8cbb8befd4220a4de50237b3827001adde2d417f
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-09-12 12:45:28 +1000

    Clarify `_.merge` description. [ci skip]
    Mention behaviour difference between merging plain objects and objects with non-Object prototypes. As discussed in #1455.

--- lodash.js
+++ lodash.js
@@ -9708,9 +9708,13 @@
     }
 
     /**
-     * Recursively merges own enumerable properties of the source object(s) into the
-     * destination object, for source properties which don't resolve to `undefined`.
-     * Subsequent sources overwrite property assignments of previous sources.
+     * Recursively merges own and inherited enumerable properties of source
+     * objects into the destination object, skipping source properties that resolve
+     * to `undefined`. Array and plain object properties are merged recursively.
+     * Other objects and value types are overriden by assignment. Subsequent
+     * sources overwrite property assignments of previous sources.
+     *
+     * **Note:** This method mutates `object`.
      *
      * @static
      * @memberOf _

a84e44b2a30c19e24c7307873334b438ab1801a9
John-David Dalton <john.david.dalton@gmail.com>
2015-09-15 23:48:36 -0700

    Use `MAX_ARRAY_LENGTH` instead of `INFINITY` for the default `__takeCount__` value.

--- lodash.js
+++ lodash.js
@@ -1587,7 +1587,7 @@
       this.__dir__ = 1;
       this.__filtered__ = false;
       this.__iteratees__ = [];
-      this.__takeCount__ = INFINITY;
+      this.__takeCount__ = MAX_ARRAY_LENGTH;
       this.__views__ = [];
     }
 

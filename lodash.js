a4b333ceb8ab33dd82466abd0a2abb7c7e658c86
John-David Dalton <john.david.dalton@gmail.com>
2015-09-19 10:31:59 -0700

    Use `_.toArray` in `wrapperNext`.

--- lodash.js
+++ lodash.js
@@ -6065,7 +6065,7 @@
      */
     function wrapperNext() {
       if (this.__values__ === undefined) {
-        this.__values__ = values(this.value());
+        this.__values__ = toArray(this.value());
       }
       var done = this.__index__ >= this.__values__.length,
           value = done ? undefined : this.__values__[this.__index__++];

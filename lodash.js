0fb414d4f4d43b27a7713154e9d8d9c85cf03c4b
John-David Dalton <john.david.dalton@gmail.com>
2015-10-11 23:30:26 -0700

    Fix `_.differenceBy` doc example. [ci skip]

--- lodash.js
+++ lodash.js
@@ -5021,8 +5021,8 @@
      * // => [3.1, 1.3]
      *
      * // using the `_.property` callback shorthand
-     * _.differenceBy([{ 'x': 1 }], [{ 'x': 2 }, { 'x': 1 }], 'x');
-     * // => [{ 'x': 1 }, { 'x': 2 }]
+     * _.differenceBy([{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x');
+     * // => [{ 'x': 2 }]
      */
     var differenceBy = restParam(function(array, values) {
       var iteratee = last(values);

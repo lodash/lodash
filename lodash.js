3995a0dbbc0a443b0c9237aa000d3ab64648ed7f
Xotic750 <xotic750@gmail>
2015-10-22 02:08:18 +0200

    Use nativeMin and nativeMax vars as suggested. https://github.com/lodash/lodash/pull/1551#discussion_r42698561

--- lodash.js
+++ lodash.js
@@ -11025,7 +11025,7 @@
      * // => 5
      */
     function clamp(number, min, max) {
-      return Math.min(Math.max(number, min), max);
+      return nativeMin(nativeMax(number, min), max);
     }
 
     /*------------------------------------------------------------------------*/

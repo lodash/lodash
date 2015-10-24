cc8e4956f542df1de99c62c5df237229fc40f4fa
John-David Dalton <john.david.dalton@gmail.com>
2015-10-23 22:12:17 -0700

    Clarify docs of `_.matches` and `_.modArgs` methods. [ci skip]

--- lodash.js
+++ lodash.js
@@ -2874,7 +2874,7 @@
      *
      * @private
      * @param {string} path The path of the property to get.
-     * @param {*} srcValue The value to compare.
+     * @param {*} srcValue The value to match.
      * @returns {Function} Returns the new function.
      */
     function baseMatchesProperty(path, srcValue) {
@@ -8355,7 +8355,7 @@
 
     /**
      * This method is like `_.modArgs` except that each of the `transforms` is
-     * provided all arguments the created function is invoked with.
+     * provided the complete set of arguments the created function is invoked with.
      *
      * @static
      * @memberOf _
@@ -12285,9 +12285,9 @@
     }
 
     /**
-     * Creates a function that performs a deep comparison between a given object
-     * and `source`, returning `true` if the given object has equivalent property
-     * values, else `false`.
+     * Creates a function that performs a deep partial comparison between a given
+     * object and `source`, returning `true` if the given object has equivalent
+     * property values, else `false`.
      *
      * **Note:** This method supports comparing arrays, booleans, `Date` objects,
      * numbers, `Object` objects, regexes, and strings. Objects are compared by
@@ -12314,8 +12314,9 @@
     }
 
     /**
-     * Creates a function that compares the value at `path` of a given object
-     * to `srcValue`.
+     * Creates a function that performs a deep partial comparison between the
+     * value at `path` of a given object to `srcValue`, returning `true` if the
+     * object value is equivalent, else `false`.
      *
      * **Note:** This method supports comparing arrays, booleans, `Date` objects,
      * numbers, `Object` objects, regexes, and strings.

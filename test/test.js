eb636f95382a5206bb7cdc4f4239ac67b55eb486
John-David Dalton <john.david.dalton@gmail.com>
2015-10-31 10:46:48 -0700

    Cleanup `_.toLength` tests.

--- test/test.js
+++ test/test.js
@@ -19349,21 +19349,21 @@
   QUnit.module('lodash.toLength');
 
   (function() {
-    QUnit.test('should return number literal integers in range unchanged', function(assert) {
-      assert.expect(3);
-
-      assert.strictEqual(_.toLength(0), 0);
-      assert.strictEqual(_.toLength(3), 3);
-      assert.strictEqual(_.toLength(MAX_ARRAY_LENGTH), MAX_ARRAY_LENGTH);
-    });
-
-    QUnit.test('should return number as integer clamped to range', function(assert) {
+    QUnit.test('should return a valid length', function(assert) {
       assert.expect(4);
 
       assert.strictEqual(_.toLength(-1), 0);
       assert.strictEqual(_.toLength('1'), 1);
       assert.strictEqual(_.toLength(1.1), 1);
       assert.strictEqual(_.toLength(MAX_INTEGER), MAX_ARRAY_LENGTH);
+    });
+
+    QUnit.test('should return `value` if a valid length', function(assert) {
+      assert.expect(3);
+
+      assert.strictEqual(_.toLength(0), 0);
+      assert.strictEqual(_.toLength(3), 3);
+      assert.strictEqual(_.toLength(MAX_ARRAY_LENGTH), MAX_ARRAY_LENGTH);
     });
   }());
 

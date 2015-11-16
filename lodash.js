b89045b20f8d735b1bd83f2aef9bd58bbb5fdeca
John-David Dalton <john.david.dalton@gmail.com>
2015-11-16 08:16:47 -0800

    Remove unused case in `apply`.

--- lodash.js
+++ lodash.js
@@ -384,7 +384,6 @@
    */
   function apply(func, thisArg, args) {
     switch (args.length) {
-      case 0: return func.call(thisArg);
       case 1: return func.call(thisArg, args[0]);
       case 2: return func.call(thisArg, args[0], args[1]);
       case 3: return func.call(thisArg, args[0], args[1], args[2]);

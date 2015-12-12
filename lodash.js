ac0a0ea07eb6044cb000e52a13b96e2eaee230ae
John-David Dalton <john.david.dalton@gmail.com>
2015-12-12 11:03:34 -0800

    Add `delete` to `Map` interface note. [ci skip]

--- lodash.js
+++ lodash.js
@@ -8596,7 +8596,7 @@
      * **Note:** The cache is exposed as the `cache` property on the memoized
      * function. Its creation may be customized by replacing the `_.memoize.Cache`
      * constructor with one whose instances implement the [`Map`](http://ecma-international.org/ecma-262/6.0/#sec-properties-of-the-map-prototype-object)
-     * method interface of `get`, `has`, and `set`.
+     * method interface of `delete`, `get`, `has`, and `set`.
      *
      * @static
      * @memberOf _

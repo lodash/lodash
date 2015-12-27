0d8779734d53222ac1197d119ad402819905f468
John-David Dalton <john.david.dalton@gmail.com>
2015-12-26 22:07:15 -0600

    Cleanup comment. [ci skip]

--- lodash.js
+++ lodash.js
@@ -4836,7 +4836,7 @@
       var length = array.length,
           result = new array.constructor(length);
 
-      // Add array properties assigned by `RegExp#exec`.
+      // Add properties assigned by `RegExp#exec`.
       if (length && typeof array[0] == 'string' && hasOwnProperty.call(array, 'index')) {
         result.index = array.index;
         result.input = array.input;

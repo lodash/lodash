cfe05b7d3421fea42b487f3578f031fbacb72da5
John-David Dalton <john.david.dalton@gmail.com>
2016-01-01 10:04:36 -0600

    Bump copyright year. [ci skip]

--- lodash.js
+++ lodash.js
@@ -1,9 +1,9 @@
 /**
  * @license
  * lodash 4.0.0-pre <https://lodash.com/>
- * Copyright 2012-2015 The Dojo Foundation <http://dojofoundation.org/>
+ * Copyright 2012-2016 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
- * Copyright 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Copyright 2009-2016 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  * Available under MIT license <https://lodash.com/license>
  */
 ;(function() {

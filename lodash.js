ff5f06b9f050c178ca6580e691b47b6b6ec10136
mori_9tarou <mori_kyutaro@cyberagent.co.jp>
2016-01-14 14:47:28 +0900

    Fix `_.pullAll` doc example typo. [ci skip]

--- lodash.js
+++ lodash.js
@@ -6029,7 +6029,7 @@
      *
      * var array = [1, 2, 3, 1, 2, 3];
      *
-     * _.pull(array, [2, 3]);
+     * _.pullAll(array, [2, 3]);
      * console.log(array);
      * // => [1, 1]
      */

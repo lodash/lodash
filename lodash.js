9d033247e961bdda09a9295910fc647bd32b1b65
John-David Dalton <john.david.dalton@gmail.com>
2016-01-17 23:08:58 -0600

    Correct arg count in `_.isEqualWit` doc note. [ci skip]

--- lodash.js
+++ lodash.js
@@ -9496,7 +9496,7 @@
     /**
      * This method is like `_.isEqual` except that it accepts `customizer` which is
      * invoked to compare values. If `customizer` returns `undefined` comparisons are
-     * handled by the method instead. The `customizer` is invoked with up to seven arguments:
+     * handled by the method instead. The `customizer` is invoked with up to six arguments:
      * (objValue, othValue [, index|key, object, other, stack]).
      *
      * @static

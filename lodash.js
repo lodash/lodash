e4c9916e17746183c297f4d2f967923391eac04a
John-David Dalton <john.david.dalton@gmail.com>
2016-01-28 22:11:43 -0800

    Add default accumulator values to doc examples. [ci skip]

--- lodash.js
+++ lodash.js
@@ -7920,7 +7920,7 @@
      *
      * _.reduce([1, 2], function(sum, n) {
      *   return sum + n;
-     * });
+     * }, 0);
      * // => 3
      *
      * _.reduce({ 'a': 1, 'b': 2, 'c': 1 }, function(result, value, key) {
@@ -11589,12 +11589,12 @@
      * _.transform([2, 3, 4], function(result, n) {
      *   result.push(n *= n);
      *   return n % 2 == 0;
-     * });
+     * }, []);
      * // => [4, 9]
      *
      * _.transform({ 'a': 1, 'b': 2, 'c': 1 }, function(result, value, key) {
      *   (result[value] || (result[value] = [])).push(key);
-     * });
+     * }, {});
      * // => { '1': ['a', 'c'], '2': ['b'] }
      */
     function transform(object, iteratee, accumulator) {

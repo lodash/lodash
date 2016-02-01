b61b674fec91f9a6186516de6a1b261cc5143600
Mohammed Nauage <mohammed.nauage.gsa@gmail.com>
2016-01-31 23:50:57 -0500

    Remove sorted behavior from `_.indexOf` docs. [ci skip]

--- lodash.js
+++ lodash.js
@@ -5891,8 +5891,7 @@
      * Gets the index at which the first occurrence of `value` is found in `array`
      * using [`SameValueZero`](http://ecma-international.org/ecma-262/6.0/#sec-samevaluezero)
      * for equality comparisons. If `fromIndex` is negative, it's used as the offset
-     * from the end of `array`. If `array` is sorted providing `true` for `fromIndex`
-     * performs a faster binary search.
+     * from the end of `array`.
      *
      * @static
      * @memberOf _

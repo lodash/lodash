18f74aa5b2b8e1cc85a498f485d14882c25225b4
John-David Dalton <john.david.dalton@gmail.com>
2016-02-01 22:13:38 -0800

    Rename `spreadArgs` to `otherArgs`.

--- lodash.js
+++ lodash.js
@@ -9069,12 +9069,12 @@
       start = start === undefined ? 0 : nativeMax(toInteger(start), 0);
       return rest(function(args) {
         var array = args[start],
-            spreadArgs = args.slice(0, start);
+            otherArgs = args.slice(0, start);
 
         if (array) {
-          arrayPush(spreadArgs, array);
+          arrayPush(otherArgs, array);
         }
-        return apply(func, this, spreadArgs);
+        return apply(func, this, otherArgs);
       });
     }
 

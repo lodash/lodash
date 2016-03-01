7f00e8f2f18a68e68ad07a8cdd38789c4a330008
John-David Dalton <john.david.dalton@gmail.com>
2016-02-29 21:18:51 -0800

    Rebuild lodash and docs.

--- lodash.js
+++ lodash.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * lodash 4.5.1 <https://lodash.com/>
+ * lodash 4.6.0 <https://lodash.com/>
  * Copyright 2012-2016 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
  * Copyright 2009-2016 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
@@ -12,7 +12,7 @@
   var undefined;
 
   /** Used as the semantic version number. */
-  var VERSION = '4.5.1';
+  var VERSION = '4.6.0';
 
   /** Used as the size to enable large array optimizations. */
   var LARGE_ARRAY_SIZE = 200;

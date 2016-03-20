6494a072d2d9937e9e322aa375cc9328d910c651
John-David Dalton <john.david.dalton@gmail.com>
2016-03-20 01:02:53 -0700

    Fix test fails in Safari.

--- lodash.js
+++ lodash.js
@@ -15441,23 +15441,16 @@
     arrayEach(['pop', 'push', 'shift', 'sort', 'splice', 'unshift'], function(methodName) {
       var func = arrayProto[methodName],
           chainName = /^(?:push|sort|unshift)$/.test(methodName) ? 'tap' : 'thru',
-          mutates = methodName != 'sort',
           retUnwrapped = /^(?:pop|shift)$/.test(methodName);
 
       lodash.prototype[methodName] = function() {
         var args = arguments;
         if (retUnwrapped && !this.__chain__) {
           var value = this.value();
-          if ((mutates ? !isArray(value) : (value == null))) {
-            value = [];
-          }
-          return func.apply(value, args);
+          return func.apply(isArray(value) ? value : [], args);
         }
         return this[chainName](function(value) {
-          if ((mutates ? !isArray(value) : (value == null))) {
-            value = [];
-          }
-          return func.apply(value, args);
+          return func.apply(isArray(value) ? value : [], args);
         });
       };
     });

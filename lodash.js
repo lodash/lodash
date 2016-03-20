234e693d924576db77089b25159b47233d98e8e4
John-David Dalton <john.david.dalton@gmail.com>
2016-03-20 11:29:22 -0700

    Flip priority of "object" and "collection" in `_.isEmpty` doc description. [ci skip]

--- lodash.js
+++ lodash.js
@@ -10134,7 +10134,7 @@
     }
 
     /**
-     * Checks if `value` is an empty collection or object. A value is considered
+     * Checks if `value` is an empty object or collection. A value is considered
      * empty if it's an `arguments` object, array, string, or jQuery-like collection
      * with a length of `0` or has no own enumerable string keyed properties.
      *

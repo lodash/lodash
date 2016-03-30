171d9eab78eb6631760a78680c12b59df3841d15
John-David Dalton <john.david.dalton@gmail.com>
2016-03-30 08:36:16 -0700

    Use `copyArray` in `concat`.

--- lodash.js
+++ lodash.js
@@ -5852,13 +5852,13 @@
      * // => [1]
      */
     function concat() {
-      var length = arguments.length;
-      if (!length) {
-        return [];
-      }
-      var array = castArray(arguments[0]),
-          args = Array(length - 1);
+      var length = arguments.length,
+          array = castArray(arguments[0]);
 
+      if (length < 2) {
+        return length ? copyArray(array) : [];
+      }
+      var args = Array(length - 1);
       while (length--) {
         args[length - 1] = arguments[length];
       }

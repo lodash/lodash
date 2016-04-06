e120d63ee253021e1d640af086ef8f0c4f4fb64b
John-David Dalton <john.david.dalton@gmail.com>
2016-04-06 07:59:05 -0700

    Add doc note about group ordering of `_.groupBy`. [ci skip] [closes #2212]

--- lodash.js
+++ lodash.js
@@ -8349,9 +8349,10 @@
 
     /**
      * Creates an object composed of keys generated from the results of running
-     * each element of `collection` thru `iteratee`. The corresponding value of
-     * each key is an array of elements responsible for generating the key. The
-     * iteratee is invoked with one argument: (value).
+     * each element of `collection` thru `iteratee`. The order of grouped values
+     * is determined by the order they occur in `collection`. The corresponding
+     * value of each key is an array of elements responsible for generating the
+     * key. The iteratee is invoked with one argument: (value).
      *
      * @static
      * @memberOf _

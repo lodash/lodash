977e9c5c6428c947a8e4e3fb7c997f7a674beeff
John-David Dalton <john.david.dalton@gmail.com>
2016-04-12 23:55:34 -0700

    Bump to v4.3.5.

--- lodash.defaultsdeep/index.js
+++ lodash.defaultsdeep/index.js
@@ -1,5 +1,5 @@
 /**
- * lodash 4.3.4 (Custom Build) <https://lodash.com/>
+ * lodash 4.3.5 (Custom Build) <https://lodash.com/>
  * Build: `lodash modularize exports="npm" -o ./`
  * Copyright jQuery Foundation and other contributors <https://jquery.org/>
  * Released under MIT license <https://lodash.com/license>
@@ -301,24 +301,10 @@ function copyArray(source, array) {
  * @param {Object} source The object to copy properties from.
  * @param {Array} props The property identifiers to copy.
  * @param {Object} [object={}] The object to copy properties to.
- * @returns {Object} Returns `object`.
- */
-function copyObject(source, props, object) {
-  return copyObjectWith(source, props, object);
-}
-
-/**
- * This function is like `copyObject` except that it accepts a function to
- * customize copied values.
- *
- * @private
- * @param {Object} source The object to copy properties from.
- * @param {Array} props The property identifiers to copy.
- * @param {Object} [object={}] The object to copy properties to.
  * @param {Function} [customizer] The function to customize copied values.
  * @returns {Object} Returns `object`.
  */
-function copyObjectWith(source, props, object, customizer) {
+function copyObject(source, props, object, customizer) {
   object || (object = {});
 
   var index = -1,

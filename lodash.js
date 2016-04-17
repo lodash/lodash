7239bf791faa51c80215fd9fdadc506d175dcc76
John-David Dalton <john.david.dalton@gmail.com>
2016-04-16 18:16:27 -0700

    Comma nit in `_.inRange` doc. [ci skip]

--- lodash.js
+++ lodash.js
@@ -13132,7 +13132,7 @@
     }
 
     /**
-     * Checks if `n` is between `start` and up to but not including, `end`. If
+     * Checks if `n` is between `start` and up to, but not including, `end`. If
      * `end` is not specified, it's set to `start` with `start` then set to `0`.
      * If `start` is greater than `end` the params are swapped to support
      * negative ranges.

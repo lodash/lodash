6f600ebeac7a98ddf47eef1594b4ac1de5635991
Clinton Montague <clinton@slightlymore.co.uk>
2016-04-26 19:55:46 +0100

    Add _.flip bitmask value to `createWrapper` documentation. [ci skip]

--- lodash.js
+++ lodash.js
@@ -4908,6 +4908,7 @@
      *    64 - `_.partialRight`
      *   128 - `_.rearg`
      *   256 - `_.ary`
+     *   512 - `_.flip`
      * @param {*} [thisArg] The `this` binding of `func`.
      * @param {Array} [partials] The arguments to be partially applied.
      * @param {Array} [holders] The `partials` placeholder indexes.

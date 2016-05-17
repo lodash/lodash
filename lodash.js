315e751f43931548f3deeca8a58c655c13b942b2
John-David Dalton <john.david.dalton@gmail.com>
2016-05-16 18:29:30 -0700

    Add `_.isNative` doc note. [ci skip]

--- lodash.js
+++ lodash.js
@@ -11191,6 +11191,14 @@
     /**
      * Checks if `value` is a pristine native function.
      *
+     * **Note:** This method can't reliably detect native functions in the
+     * presence of the `core-js` package because `core-js` circumvents this kind
+     * of detection. Despite multiple requests, the `core-js` maintainer has made
+     * it clear: any attempt to fix the detection will be obstructed. As a result,
+     * we're left with little choice but to throw an error. Unfortunately, this
+     * also affects packages, like [babel-polyfill](https://www.npmjs.com/package/babel-polyfill),
+     * which rely on `core-js`.
+     *
      * @static
      * @memberOf _
      * @since 3.0.0

b2da5ee1a7b778a31833a9241746b5581a5808cd
John-David Dalton <john.david.dalton@gmail.com>
2016-05-16 18:30:12 -0700

    Add backticks to error message. [ci skip]

--- lodash.js
+++ lodash.js
@@ -11216,7 +11216,7 @@
      */
     function isNative(value) {
       if (isMaskable(value)) {
-        throw new Error('This method is not supported with core-js. Try https://github.com/es-shims.');
+        throw new Error('This method is not supported with `core-js`. Try https://github.com/es-shims.');
       }
       return baseIsNative(value);
     }

02ff3e625861154c90b6eb3842c631045207b3d0
John-David Dalton <john.david.dalton@gmail.com>
2016-05-23 11:03:10 -0700

    Remove "unique" mention from `_.difference` docs. [ci skip] [closes #2372]

--- lodash.js
+++ lodash.js
@@ -6202,8 +6202,8 @@
     }
 
     /**
-     * Creates an array of unique `array` values not included in the other given
-     * arrays using [`SameValueZero`](http://ecma-international.org/ecma-262/6.0/#sec-samevaluezero)
+     * Creates an array of `array` values not included in the other given arrays
+     * using [`SameValueZero`](http://ecma-international.org/ecma-262/6.0/#sec-samevaluezero)
      * for equality comparisons. The order of result values is determined by the
      * order they occur in the first array.
      *

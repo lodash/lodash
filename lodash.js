32158935f84f6d523886396ca8f4b3aa940c1c3c
Dan J Miller <danjm.com@gmail.com>
2016-06-24 23:40:41 -0230

    Change second param of `_.invokeMap` as `path` in docs. [ci skip]

--- lodash.js
+++ lodash.js
@@ -8921,8 +8921,8 @@
     /**
      * Invokes the method at `path` of each element in `collection`, returning
      * an array of the results of each invoked method. Any additional arguments
-     * are provided to each invoked method. If `methodName` is a function, it's
-     * invoked for and `this` bound to, each element in `collection`.
+     * are provided to each invoked method. If `path` is a function, it's invoked
+     * for, and `this` bound to, each element in `collection`.
      *
      * @static
      * @memberOf _

e1a4ac70ccfbd219b900da75711e4fc0a9773d66
John-David Dalton <john.david.dalton@gmail.com>
2016-07-12 15:15:29 -0700

    Remove `thisGlobal` use.

--- lodash.js
+++ lodash.js
@@ -366,11 +366,8 @@
   /** Detect free variable `self`. */
   var freeSelf = checkGlobal(typeof self == 'object' && self);
 
-  /** Detect `this` as the global object. */
-  var thisGlobal = checkGlobal(typeof this == 'object' && this);
-
   /** Used as a reference to the global object. */
-  var root = freeGlobal || freeSelf || thisGlobal || Function('return this')();
+  var root = freeGlobal || freeSelf || Function('return this')();
 
   /** Detect free variable `exports`. */
   var freeExports = freeGlobal && typeof exports == 'object' && exports;

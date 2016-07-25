408b44a913156ce3403d29ea6f69c2cd17c9062f
John-David Dalton <john.david.dalton@gmail.com>
2016-07-24 23:56:12 -0700

    Bump to v4.14.0.

---
+++ lodash.defaultto/README.md
@@ -0,0 +1,18 @@
+# lodash.defaultto v4.14.0
+
+The [lodash](https://lodash.com/) method `_.defaultTo` exported as a [Node.js](https://nodejs.org/) module.
+
+## Installation
+
+Using npm:
+```bash
+$ {sudo -H} npm i -g npm
+$ npm i --save lodash.defaultto
+```
+
+In Node.js:
+```js
+var defaultTo = require('lodash.defaultto');
+```
+
+See the [documentation](https://lodash.com/docs#defaultTo) or [package source](https://github.com/lodash/lodash/blob/4.14.0-npm-packages/lodash.defaultto) for more details.
---
+++ lodash.defaultto/index.js
@@ -0,0 +1,34 @@
+/**
+ * lodash (Custom Build) <https://lodash.com/>
+ * Build: `lodash modularize exports="npm" -o ./`
+ * Copyright jQuery Foundation and other contributors <https://jquery.org/>
+ * Released under MIT license <https://lodash.com/license>
+ * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
+ * Copyright Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ */
+
+/**
+ * Checks `value` to determine whether a default value should be returned in
+ * its place. The `defaultValue` is returned if `value` is `NaN`, `null`,
+ * or `undefined`.
+ *
+ * @static
+ * @memberOf _
+ * @since 4.14.0
+ * @category Util
+ * @param {*} value The value to check.
+ * @param {*} defaultValue The default value.
+ * @returns {*} Returns the resolved value.
+ * @example
+ *
+ * _.defaultTo(1, 10);
+ * // => 1
+ *
+ * _.defaultTo(undefined, 10);
+ * // => 10
+ */
+function defaultTo(value, defaultValue) {
+  return (value == null || value !== value) ? defaultValue : value;
+}
+
+module.exports = defaultTo;

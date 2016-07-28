616ced12dc24a838e2d7044294cda64f86d994cf
Johannes Scharlach <johannes.scharlach@gmail.com>
2016-07-28 21:54:51 +0200

    re-add `stack.delete` to `baseMergeDeep`

--- lodash.js
+++ lodash.js
@@ -3409,6 +3409,7 @@
         // Recursively merge objects and arrays (susceptible to call stack limits).
         stack.set(srcValue, newValue);
         mergeFunc(newValue, srcValue, srcIndex, customizer, stack);
+        stack['delete'](srcValue);
       }
       assignMergeValue(object, key, newValue);
     }

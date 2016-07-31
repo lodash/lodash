73b2f2050960b7a1ae234ed8eada3f4ffc8bf715
John-David Dalton <john.david.dalton@gmail.com>
2016-07-30 20:08:30 -0700

    Use lodash-doc-globals.

--- .markdown-doctest-setup.js
+++ .markdown-doctest-setup.js
@@ -2,47 +2,10 @@
 
 delete global['__core-js_shared__'];
 
-var _ = require('./lodash.js');
-
-function mockQuery() {
-  return {
-    'on': function(eventName, callback) {
-      callback();
-    }
-  };
-}
-
-mockQuery.each = _.each;
+var _ = require('./lodash.js'),
+    globals = require('lodash-doc-globals');
 
 module.exports = {
   'babel': false,
-  'globals': {
-    '_': _,
-
-    // Example mocks.
-    'asyncSave': _.noop,
-    'addContactToList': _.noop,
-    'batchLog': _.noop,
-    'calculateLayout': _.noop,
-    'createApplication': _.noop,
-    'data': { 'user': 'mock' },
-    'mainText': '',
-    'renewToken': _.noop,
-    'sendMail': _.noop,
-    'updatePosition': _.noop,
-
-    // DOM mocks.
-    'document': { 'body': { 'childNodes': [], 'nodeName': 'BODY' } },
-    'element': {},
-    'EventSource': _.noop,
-    'jQuery': mockQuery,
-    'window': {},
-
-    // Node.js mocks.
-    'Buffer': Buffer,
-    'fs': { 'writeFileSync': _.noop },
-    'path': require('path'),
-    'process': process,
-    'setImmediate': setImmediate
-  }
+  'globals': _.assign({ '_': _ }, globals)
 };

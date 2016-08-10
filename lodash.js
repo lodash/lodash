ba7ddc8cdc1cc822b60835ca4617ffee6927a3b6
John-David Dalton <john.david.dalton@gmail.com>
2016-08-09 18:18:45 -0700

    Minor format nit. [ci skip]

--- lodash.js
+++ lodash.js
@@ -267,8 +267,8 @@
     'Array', 'Buffer', 'DataView', 'Date', 'Error', 'Float32Array', 'Float64Array',
     'Function', 'Int8Array', 'Int16Array', 'Int32Array', 'Map', 'Math', 'Object',
     'Promise', 'RegExp', 'Set', 'String', 'Symbol', 'TypeError', 'Uint8Array',
-    'Uint8ClampedArray', 'Uint16Array', 'Uint32Array', 'WeakMap', '_', 'clearTimeout',
-    'isFinite', 'parseInt', 'setTimeout'
+    'Uint8ClampedArray', 'Uint16Array', 'Uint32Array', 'WeakMap',
+    '_', 'clearTimeout', 'isFinite', 'parseInt', 'setTimeout'
   ];
 
   /** Used to make template sourceURLs easier to identify. */

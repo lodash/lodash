37813c3826232c9a33d86e7bb8464d1da4559654
John-David Dalton <john.david.dalton@gmail.com>
2016-08-11 18:43:43 -0700

    Add `_.negate` test for accepting multiple arguments.

--- test/test.js
+++ test/test.js
@@ -15943,6 +15943,37 @@
       assert.strictEqual(negate(1), true);
       assert.strictEqual(negate(2), false);
     });
+
+    QUnit.test('should create a function that negates the result of `func`', function(assert) {
+      assert.expect(2);
+
+      var negate = _.negate(isEven);
+
+      assert.strictEqual(negate(1), true);
+      assert.strictEqual(negate(2), false);
+    });
+
+    QUnit.test('should create a function that accepts multiple arguments', function(assert) {
+      assert.expect(1);
+
+      var argCount,
+          count = 4,
+          negate = _.negate(function() { argCount = arguments.length; }),
+          expected = lodashStable.times(count, stubTrue);
+
+      var actual = lodashStable.times(count, function(index) {
+        switch (index) {
+          case 0: negate(); break;
+          case 1: negate(1); break;
+          case 2: negate(1, 2); break;
+          case 3: negate(1, 2, 3); break;
+          case 4: negate(1, 2, 3, 4);
+        }
+        return argCount == index;
+      });
+
+      assert.deepEqual(actual, expected);
+    });
   }());
 
   /*--------------------------------------------------------------------------*/

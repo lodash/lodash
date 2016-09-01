d47123779ed1f78a4c32abdde85f816160ba13dd
John-David Dalton <john.david.dalton@gmail.com>
2016-09-01 01:30:29 -0700

    Avoid inserting empty details.

--- lodash.js
+++ lodash.js
@@ -6039,9 +6039,11 @@
      * @returns {string} Returns the modified source.
      */
     function insertWrapDetails(source, details) {
-      var length = details.length,
-          lastIndex = length - 1;
-
+      var length = details.length;
+      if (!length) {
+        return source;
+      }
+      var lastIndex = length - 1;
       details[lastIndex] = (length > 1 ? '& ' : '') + details[lastIndex];
       details = details.join(length > 2 ? ', ' : ' ');
       return source.replace(reWrapComment, '{\n/* [wrapped with ' + details + '] */\n');

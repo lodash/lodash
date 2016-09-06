8176d56c725489156e19d68aeaf8851d313f4446
John-David Dalton <john.david.dalton@gmail.com>
2016-09-06 14:24:53 -0700

    Move `arraySample` definition to fix builds.

--- lodash.js
+++ lodash.js
@@ -713,19 +713,6 @@
   }
 
   /**
-   * A specialized version of `_.sample` for arrays without support for iteratee
-   * shorthands.
-   *
-   * @private
-   * @param {Array} array The array to sample.
-   * @returns {*} Returns the random element.
-   */
-  function arraySample(array) {
-    var length = array.length;
-    return length ? array[baseRandom(0, length - 1)] : undefined;
-  }
-
-  /**
    * A specialized version of `_.some` for arrays without support for iteratee
    * shorthands.
    *
@@ -2390,6 +2377,19 @@
         }
       }
       return result;
+    }
+
+    /**
+     * A specialized version of `_.sample` for arrays without support for iteratee
+     * shorthands.
+     *
+     * @private
+     * @param {Array} array The array to sample.
+     * @returns {*} Returns the random element.
+     */
+    function arraySample(array) {
+      var length = array.length;
+      return length ? array[baseRandom(0, length - 1)] : undefined;
     }
 
     /**

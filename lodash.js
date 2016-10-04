9fba7be2960e3882e8c32cd8d816d523b0c7f5c2
John-David Dalton <john.david.dalton@gmail.com>
2016-10-04 08:41:08 -0700

    Add comments to `arrayLikeKeys`. [ci skip]

--- lodash.js
+++ lodash.js
@@ -2397,9 +2397,13 @@
       for (var key in value) {
         if ((inherited || hasOwnProperty.call(value, key)) &&
             !(skipIndexes && (
+               // Safari 9 has enumerable `arguments.length` in strict mode.
                key == 'length' ||
+               // Node.js 0.10 has enumerable non-index properties on buffers.
                (isBuff && (key == 'offset' || key == 'parent')) ||
+               // PhantomJS 2 has enumerable non-index properties on typed arrays.
                (isType && (key == 'buffer' || key == 'byteLength' || key == 'byteOffset')) ||
+               // Skip index properties.
                isIndex(key, length)
             ))) {
           result.push(key);

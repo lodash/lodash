014982cb2c4e75680bf30938695389406623d4c7
John-David Dalton <john.david.dalton@gmail.com>
2016-11-14 17:32:52 -0800

    Remove `isKey` use from `invokeMap`.

--- lodash.js
+++ lodash.js
@@ -9476,12 +9476,10 @@
     var invokeMap = baseRest(function(collection, path, args) {
       var index = -1,
           isFunc = typeof path == 'function',
-          isProp = isKey(path),
           result = isArrayLike(collection) ? Array(collection.length) : [];
 
       baseEach(collection, function(value) {
-        var func = isFunc ? path : ((isProp && value != null) ? value[path] : undefined);
-        result[++index] = func ? apply(func, value, args) : baseInvoke(value, path, args);
+        result[++index] = isFunc ? apply(path, value, args) : baseInvoke(value, path, args);
       });
       return result;
     });

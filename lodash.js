1a05fea96799e450b434c750e767257f9e5008fe
Ophir LOJKINE <pere.jobs@gmail.com>
2016-11-25 01:09:42 +0400

    remove newline
    remove newline and change line break

--- lodash.js
+++ lodash.js
@@ -11521,9 +11521,8 @@
      * **Note:** This method supports comparing arrays, array buffers, booleans,
      * date objects, error objects, maps, numbers, `Object` objects, regexes,
      * sets, strings, symbols, and typed arrays. `Object` objects are compared
-     * by their own, not inherited, enumerable properties. 
-     * Functions and DOM nodes are simply compared with the `===` operator.
-
+     * by their own, not inherited, enumerable properties. Functions and DOM
+     * nodes are simply compared with the `===` operator.
      *
      * @static
      * @memberOf _

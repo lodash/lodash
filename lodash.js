671b106b1c92c12b2b1a71c329206dbc5e7f5bc3
John-David Dalton <john.david.dalton@gmail.com>
2016-12-04 05:37:45 +0900

    Minor doc space nit. [ci skip]

--- lodash.js
+++ lodash.js
@@ -8285,7 +8285,7 @@
      *
      * var users = [
      *   { 'user': 'barney',  'active': false },
-     *   { 'user': 'fred',    'active': false},
+     *   { 'user': 'fred',    'active': false },
      *   { 'user': 'pebbles', 'active': true }
      * ];
      *

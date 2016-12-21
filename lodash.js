02fdf4e673df417058971ed0d97b4d9b5aa84672
John-David Dalton <john.david.dalton@gmail.com>
2016-12-21 15:21:40 -0600

    Wrap in ternary value within `hashHas` in parens for clarity.

--- lodash.js
+++ lodash.js
@@ -1990,7 +1990,7 @@
      */
     function hashHas(key) {
       var data = this.__data__;
-      return nativeCreate ? data[key] !== undefined : hasOwnProperty.call(data, key);
+      return nativeCreate ? (data[key] !== undefined) : hasOwnProperty.call(data, key);
     }
 
     /**

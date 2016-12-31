4d4e452ade1e78c7eb890968d851f837be37e429
John-David Dalton <john.david.dalton@gmail.com>
2016-12-30 22:26:46 -0600

    Modularize master branch.

---
+++ invert.js
@@ -0,0 +1,27 @@
+import constant from './constant.js';
+import createInverter from './_createInverter.js';
+import identity from './identity.js';
+
+/**
+ * Creates an object composed of the inverted keys and values of `object`.
+ * If `object` contains duplicate values, subsequent values overwrite
+ * property assignments of previous values.
+ *
+ * @static
+ * @memberOf _
+ * @since 0.7.0
+ * @category Object
+ * @param {Object} object The object to invert.
+ * @returns {Object} Returns the new inverted object.
+ * @example
+ *
+ * var object = { 'a': 1, 'b': 2, 'c': 1 };
+ *
+ * _.invert(object);
+ * // => { '1': 'c', '2': 'b' }
+ */
+var invert = createInverter(function(result, value, key) {
+  result[value] = key;
+}, constant(identity));
+
+export default invert;

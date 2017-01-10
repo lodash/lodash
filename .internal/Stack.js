71f49dda0ff9c014a485ced445d23d7a86649828
John-David Dalton <john.david.dalton@gmail.com>
2017-01-10 11:10:08 -0800

    Move `Stack` modules into `Stack`.

--- .internal/Stack.js
+++ .internal/Stack.js
@@ -1,29 +1,93 @@
 import ListCache from './.internal/ListCache.js';
-import stackClear from './.internal/stackClear.js';
-import stackDelete from './.internal/stackDelete.js';
-import stackGet from './.internal/stackGet.js';
-import stackHas from './.internal/stackHas.js';
-import stackSet from './.internal/stackSet.js';
+import MapCache from './.internal/MapCache.js';
 
-/**
- * Creates a stack cache object to store key-value pairs.
- *
- * @private
- * @constructor
- * @param {Array} [entries] The key-value pairs to cache.
- */
+/** Used as the size to enable large array optimizations. */
+const LARGE_ARRAY_SIZE = 200;
+
 class Stack {
+
+  /**
+   * Creates a stack cache object to store key-value pairs.
+   *
+   * @private
+   * @constructor
+   * @param {Array} [entries] The key-value pairs to cache.
+   */
   constructor(entries) {
     const data = this.__data__ = new ListCache(entries);
     this.size = data.size;
   }
-}
 
-// Add methods to `Stack`.
-Stack.prototype.clear = stackClear;
-Stack.prototype['delete'] = stackDelete;
-Stack.prototype.get = stackGet;
-Stack.prototype.has = stackHas;
-Stack.prototype.set = stackSet;
+  /**
+   * Removes all key-value entries from the stack.
+   *
+   * @memberOf Stack
+   */
+  clear() {
+    this.__data__ = new ListCache;
+    this.size = 0;
+  }
+
+  /**
+   * Removes `key` and its value from the stack.
+   *
+   * @memberOf Stack
+   * @param {string} key The key of the value to remove.
+   * @returns {boolean} Returns `true` if the entry was removed, else `false`.
+   */
+  delete(key) {
+    const data = this.__data__;
+    const result = data['delete'](key);
+
+    this.size = data.size;
+    return result;
+  }
+
+  /**
+   * Gets the stack value for `key`.
+   *
+   * @memberOf Stack
+   * @param {string} key The key of the value to get.
+   * @returns {*} Returns the entry value.
+   */
+  get(key) {
+    return this.__data__.get(key);
+  }
+
+  /**
+   * Checks if a stack value for `key` exists.
+   *
+   * @memberOf Stack
+   * @param {string} key The key of the entry to check.
+   * @returns {boolean} Returns `true` if an entry for `key` exists, else `false`.
+   */
+  has(key) {
+    return this.__data__.has(key);
+  }
+
+  /**
+   * Sets the stack `key` to `value`.
+   *
+   * @memberOf Stack
+   * @param {string} key The key of the value to set.
+   * @param {*} value The value to set.
+   * @returns {Object} Returns the stack cache instance.
+   */
+  set(key, value) {
+    let data = this.__data__;
+    if (data instanceof ListCache) {
+      const pairs = data.__data__;
+      if (!Map || (pairs.length < LARGE_ARRAY_SIZE - 1)) {
+        pairs.push([key, value]);
+        this.size = ++data.size;
+        return this;
+      }
+      data = this.__data__ = new MapCache(pairs);
+    }
+    data.set(key, value);
+    this.size = data.size;
+    return this;
+  }
+}
 
 export default Stack;

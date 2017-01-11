e52c421c1d706aab9f05db8e4ad067c364c2165c
John-David Dalton <john.david.dalton@gmail.com>
2017-01-10 17:45:08 -0800

    Remove `overArg`.

--- .internal/nativeKeys.js
+++ .internal/nativeKeys.js
@@ -1,6 +1,14 @@
-import overArg from './.internal/overArg.js';
-
-/* Built-in method references for those with the same name as other `lodash` methods. */
-const nativeKeys = overArg(Object.keys, Object);
+/**
+ * This function is a thin wrapper around
+ * [`Object.keys`](http://ecma-international.org/ecma-262/7.0/#sec-object.keys)
+ * which ensures non-object values are coerced to objects beforehand.
+ *
+ * @private
+ * @param {Object} object The object to query.
+ * @returns {Array} Returns the array of property names.
+ */
+function nativeKeys(object) {
+  return Object.keys(Object(object));
+}
 
 export default nativeKeys;

5baad4df101599af5b4c80df210b0581651ddedb
Michał Lipiński <mylith@gmail.com>
2017-02-21 10:40:51 +0100

    Missing iteratee call in `groupBy` and `keyBy`.

--- groupBy.js
+++ groupBy.js
@@ -23,6 +23,7 @@ const hasOwnProperty = Object.prototype.hasOwnProperty
  */
 function groupBy(collection, iteratee) {
   return reduce(collection, (result, value, key) => {
+    key = iteratee(value)
     if (hasOwnProperty.call(result, key)) {
       result[key].push(value)
     } else {

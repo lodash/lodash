402ca89c50778bf8bb1ce9c6158d461a7d1ba1ba
Michał Lipiński <mylith@gmail.com>
2017-04-04 19:15:11 +0200

    Fixin declaration in `baseSum` (3e2b0bb).

--- .internal/baseSum.js
+++ .internal/baseSum.js
@@ -9,7 +9,7 @@
 function baseSum(array, iteratee) {
   let result
 
-  for (value of array) {
+  for (const value of array) {
     const current = iteratee(value)
     if (current !== undefined) {
       result = result === undefined ? current : (result + current)

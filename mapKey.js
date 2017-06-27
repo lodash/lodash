b4b10dd4d0612004bfaeae1430d452d30a814c06
James <aretecode@gmail.com>
2017-06-26 20:33:17 -0700

    fix typo on key case from copy

--- mapKey.js
+++ mapKey.js
@@ -21,7 +21,7 @@ function mapKey(object, iteratee) {
   object = Object(object)
   const result = {}
 
-  const keys = Object.Keys(object)
+  const keys = Object.keys(object)
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i]
     const value = object[key]

46d015f3b3f6d27b95f89d0ffe950777e7e01fb6
Nisse Bergman <nisse.bergman@springworks.se>
2017-08-09 09:40:47 +0200

    clarify documentation about sortedUniq
    clarify that sortedUniq only works on sorted arrays

--- sortedUniq.js
+++ sortedUniq.js
@@ -1,8 +1,10 @@
 import baseSortedUniq from './.internal/baseSortedUniq.js'
 
 /**
- * This method is like `uniq` except that it's designed and optimized
+ * This method is like `uniq` except that it only works
  * for sorted arrays.
+ * If the input array is known to be sorted `sortedUniq` is 
+ * faster than `uniq`.
  *
  * @since 4.0.0
  * @category Array

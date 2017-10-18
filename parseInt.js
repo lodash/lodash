bcd2c356049988f4910653fb52fb620a36541d01
Younes Fkihi <13110140+younesfkihi@users.noreply.github.com>
2017-10-18 09:44:38 +0200

    Fix typo in `parseInt` (#3433)

--- parseInt.js
+++ parseInt.js
@@ -8,7 +8,7 @@ const nativeParseInt = root.parseInt
 
 /**
  * Converts `string` to an integer of the specified radix. If `radix` is
- * `undefined` or `0`, a `radix` of `10` is used unless `value` is a
+ * `undefined` or `0`, a `radix` of `10` is used unless `string` is a
  * hexadecimal, in which case a `radix` of `16` is used.
  *
  * **Note:** This method aligns with the
@@ -17,7 +17,7 @@ const nativeParseInt = root.parseInt
  * @since 1.1.0
  * @category String
  * @param {string} string The string to convert.
- * @param {number} [radix=10] The radix to interpret `value` by.
+ * @param {number} [radix=10] The radix to interpret `string` by.
  * @returns {number} Returns the converted integer.
  * @example
  *

e9562a6ce33bcbae23c9bba759fdda560cd19a7f
John-David Dalton <john.david.dalton@gmail.com>
2018-01-09 10:08:44 -0800

    Update argument count for `cloneWith` customizer. [closes #3582]

--- cloneWith.js
+++ cloneWith.js
@@ -7,7 +7,7 @@ const CLONE_SYMBOLS_FLAG = 4
  * This method is like `clone` except that it accepts `customizer` which
  * is invoked to produce the cloned value. If `customizer` returns `undefined`,
  * cloning is handled by the method instead. The `customizer` is invoked with
- * up to four arguments (value [, index|key, object, stack]).
+ * one argument (value).
  *
  * @since 4.0.0
  * @category Lang

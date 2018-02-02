7cb477a3a055c9ba35fa9a7e0f6366fe4f614b3c
John-David Dalton <john.david.dalton@gmail.com>
2018-02-02 15:47:39 -0800

    Use `new` to create the array clone.

--- lodash.js
+++ lodash.js
@@ -6160,7 +6160,7 @@
      */
     function initCloneArray(array) {
       var length = array.length,
-          result = array.constructor(length);
+          result = new array.constructor(length);
 
       // Add properties assigned by `RegExp#exec`.
       if (length && typeof array[0] == 'string' && hasOwnProperty.call(array, 'index')) {

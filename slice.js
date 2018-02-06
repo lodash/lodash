96983eaf5a5da55ac52b0a0e750f3288459b64ba
Jan Niklas Fingerle <Jan.Niklas@Fingerle.org>
2018-02-06 11:03:24 +0100

    Update documentation in slice.js
    Even though #3627 has been closed, negative indexes should be documented.

--- slice.js
+++ slice.js
@@ -8,8 +8,8 @@
  * @since 3.0.0
  * @category Array
  * @param {Array} array The array to slice.
- * @param {number} [start=0] The start position.
- * @param {number} [end=array.length] The end position.
+ * @param {number} [start=0] The start position. A negative index will be treated as an offset from the end. 
+ * @param {number} [end=array.length] The end position. A negative index will be treated as an offset from the end. 
  * @returns {Array} Returns the slice of `array`.
  */
 function slice(array, start, end) {

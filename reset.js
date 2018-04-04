a028d78c5e5a1f87cec306dee5e8e15fc5df3ca7
Narendra Singh Rathore <narendrasinghrathore@users.noreply.github.com>
2018-04-04 15:33:28 +0530

    optional parameter
    1) changed optional parameter as second param.
    2) unused code removed

--- reset.js
+++ reset.js
@@ -3,41 +3,46 @@
  * Helpful if we want to rest object value to default 
  * For.eg: property typeof number will be set to 0, property typeof boolean set to false
  */
-function reset(options,data){
-if(options === undefined || options === null){
-  options ={};
-  options.number = 0;
-  options.boolean = false;
-  options.string = '';
-}
-
-  for (var prop in data) {
-                    if (typeof data[prop] === 'object') {
-                        data[prop] = CommonService.reset(data[prop]);
-                    }
-                    else {
-                        if (typeof data[prop] !== 'function') {
-                            var dataType = typeof data[prop];
-                            var returnType = null;
-                            switch (dataType) {
-                                case 'number':
-                                    returnType = options.number?options.number:0;
-                                    break;
-                                case 'string':
-                                    returnType = options.string?options.string:'';
-                                    break;
-                                case 'boolean':
-                                    returnType = options.boolean?options.boolean:false;
-                                    break;
-                                default:
-                                    break;
-                            }
-                            data[prop] = returnType;
+function reset(data, options){
+  if(data){
+        if(options === undefined || options === null){
+            options ={};
+            options.number = 0;
+            options.boolean = false;
+            options.string = '';
+       }
+    
+      for (var prop in data) {
+                        if (typeof data[prop] === 'object') {
+                            data[prop] = reset(data[prop]);
                         }
-
-                    }
-                }
-                return data;
-
+                        else {
+                            if (typeof data[prop] !== 'function') {
+                                var dataType = typeof data[prop];
+                                var returnType = null;
+                                switch (dataType) {
+                                    case 'number':
+                                        returnType = options.number?options.number:0;
+                                        break;
+                                    case 'string':
+                                        returnType = options.string?options.string:'';
+                                        break;
+                                    case 'boolean':
+                                        returnType = options.boolean?options.boolean:false;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                data[prop] = returnType;
+                            }
+    
+                        }
+       }
+       return data;
+  
+    }
+    else{
+     return undefined; 
+    }
 }
 export default reset

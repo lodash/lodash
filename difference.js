04069817d543b46308672827bb963ca8002c5d43
Shubham Koli (FaultyCarry) <37935825+Shubham-koli@users.noreply.github.com>
2018-06-27 16:14:40 +0530

    Update difference.js
    There was one extra star. I don't think we need it.

--- difference.js
+++ difference.js
@@ -2,7 +2,7 @@ import baseDifference from './.internal/baseDifference.js'
 import baseFlatten from './.internal/baseFlatten.js'
 import isArrayLikeObject from './isArrayLikeObject.js'
 
-/**
+/*
  * Creates an array of `array` values not included in the other given arrays
  * using [`SameValueZero`](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero)
  * for equality comparisons. The order and references of result values are

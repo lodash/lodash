6018350ac10d5ce6a5b7db625140b82aeab804df
M Somerville <dracos@users.noreply.github.com>
2018-09-02 23:00:30 +0100

    Add extended and supplement combining marks ranges (#3932)

--- deburr.js
+++ deburr.js
@@ -7,7 +7,9 @@ const reLatin = /[\xc0-\xd6\xd8-\xf6\xf8-\xff\u0100-\u017f]/g
 const rsComboMarksRange = '\\u0300-\\u036f'
 const reComboHalfMarksRange = '\\ufe20-\\ufe2f'
 const rsComboSymbolsRange = '\\u20d0-\\u20ff'
-const rsComboRange = rsComboMarksRange + reComboHalfMarksRange + rsComboSymbolsRange
+const rsComboMarksExtendedRange = '\\u1ab0-\\u1aff'
+const rsComboMarksSupplementRange = '\\u1dc0-\\u1dff'
+const rsComboRange = rsComboMarksRange + reComboHalfMarksRange + rsComboSymbolsRange + rsComboMarksExtendedRange + rsComboMarksSupplementRange
 
 /** Used to compose unicode capture groups. */
 const rsCombo = `[${rsComboRange}]`

4313900933c46fdc7f17da5f63980ac3eff17264
Andros Fenollosa <tanrax@users.noreply.github.com>
2018-11-16 14:45:56 +0100

    Update description

--- disorder.js
+++ disorder.js
@@ -1,9 +1,5 @@
 /**
- * Creates a slice of `array` from `start` up to, but not including, `end`.
- *
- * **Note:** This method is used instead of
- * [`Array#slice`](https://mdn.io/Array/slice) to ensure dense arrays are
- * returned.
+ * Disorders the contents of an `array` randomly.
  *
  * @since 0.1.0
  * @category Array

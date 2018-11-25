c2a47c7d6121d65d661419fe35ceeb14a5bd6250
Luiz Américo <luizamericop@gmail.com>
2018-11-24 21:44:08 -0300

    Call upperFirst directly, instead of capitalize, in camelCase

--- lodash.js
+++ lodash.js
@@ -14133,7 +14133,7 @@
      */
     var camelCase = createCompounder(function(result, word, index) {
       word = word.toLowerCase();
-      return result + (index ? capitalize(word) : word);
+      return result + (index ? upperFirst(word) : word);
     });
 
     /**

c7488532ba81c33d09ee6e056f10f5833c6cdb02
Luiz Américo <luizamericop@gmail.com>
2018-11-25 10:31:03 -0300

    Remove unnecessary null check in arrayEach

--- .internal/arrayEach.js
+++ .internal/arrayEach.js
@@ -8,7 +8,7 @@
  */
 function arrayEach(array, iteratee) {
   let index = -1
-  const length = array == null ? 0 : array.length
+  const length = array.length
 
   while (++index < length) {
     if (iteratee(array[index], index, array) === false) {

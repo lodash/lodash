de23f5b97ea5f2b1e8847fd7ea7c3a993d2357d1
Neek Sandhu <neek.sandhu@outlook.com>
2018-11-28 20:58:57 -0800

    createRound 12x faster using pure arithmetic only
    <kbd>Ctrl</kbd>

--- .internal/createRound.js
+++ .internal/createRound.js
@@ -10,13 +10,8 @@ function createRound(methodName) {
   return (number, precision) => {
     precision = precision == null ? 0 : (precision >= 0 ? Math.min(precision, 292) : Math.max(precision, -292))
     if (precision) {
-      // Shift with exponential notation to avoid floating-point issues.
-      // See [MDN](https://mdn.io/round#Examples) for more details.
-      let pair = `${number}e`.split('e')
-      const value = func(`${pair[0]}e${+pair[1] + precision}`)
-
-      pair = `${value}e`.split('e')
-      return +`${pair[0]}e${+pair[1] - precision}`
+	    const modifier = Math.pow(10, precision)
+	    return func(number * modifier) / modifier
     }
     return func(number)
   }

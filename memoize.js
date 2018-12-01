508d46a7a4e448ed576cc268556d6bd10b1b994f
Luiz Américo <camara_luiz@yahoo.com.br>
2018-12-01 14:09:30 -0300

    Replace MapCache by Map in memoize (#4094) (#4095)

--- memoize.js
+++ memoize.js
@@ -1,5 +1,3 @@
-import MapCache from './.internal/MapCache.js'
-
 /**
  * Creates a function that memoizes the result of `func`. If `resolver` is
  * provided, it determines the cache key for storing the result based on the
@@ -57,10 +55,10 @@ function memoize(func, resolver) {
     memoized.cache = cache.set(key, result) || cache
     return result
   }
-  memoized.cache = new (memoize.Cache || MapCache)
+  memoized.cache = new (memoize.Cache || Map)
   return memoized
 }
 
-memoize.Cache = MapCache
+memoize.Cache = Map
 
 export default memoize

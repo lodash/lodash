f5c23a020aff2f06e155a180b87883c5b243af05
moonformeli <mgyang95@gmail.com>
2019-01-12 00:45:11 +0900

    Use the exported functions

--- hasPathIn.js
+++ hasPathIn.js
@@ -3,6 +3,10 @@ import isArguments from './isArguments.js'
 import isIndex from './.internal/isIndex.js'
 import isLength from './isLength.js'
 import toKey from './.internal/toKey.js'
+import cloneDeep from './cloneDeep'
+import hasIn from './hasIn.js'
+import isNull from './isNull.js'
+import isObject from './isObject.js'
 
 /**
  * Checks if `path` is a direct property of `object`.
@@ -25,7 +29,11 @@ import toKey from './.internal/toKey.js'
  * // => true
  */
 function hasPathIn(object, path) {
-  path = castPath(path, object)
+  if (!isObject(object)) {
+    throw new TypeError('Expected an object')
+  }
+  let copyObj = cloneDeep(object)
+  path = castPath(path, copyObj)
 
   let index = -1
   let { length } = path
@@ -34,17 +42,17 @@ function hasPathIn(object, path) {
 
   while (++index < length) {
     key = toKey(path[index])
-    if (!(result = object != null && key in Object(object))) {
+    if (!(result = hasIn(copyObj, key))) {
       break
     }
-    object = object[key]
+    copyObj = copyObj[key]
   }
-  if (result || ++index != length) {
+  if (result || ++index !== length) {
     return result
   }
-  length = object == null ? 0 : object.length
+  length = isNull(copyObj) ? 0 : copyObj.length
   return !!length && isLength(length) && isIndex(key, length) &&
-    (Array.isArray(object) || isArguments(object))
+    (Array.isArray(copyObj) || isArguments(copyObj))
 }
 
 export default hasPathIn

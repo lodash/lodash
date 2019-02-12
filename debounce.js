1e1e039439b48b7b0864899e09ea819e26dbca17
moonformeli <mgyang95@gmail.com>
2019-02-12 14:19:42 +0900

    Oops, fixed the typo!

--- debounce.js
+++ debounce.js
@@ -101,7 +101,7 @@ function debounce(func, wait, options) {
 
   function startTimer(pendingFunc, wait) {
     if (useRAF) {
-      root.cancelAnimationFrame(pendingFunc);
+      root.cancelAnimationFrame(timerId);
       return root.requestAnimationFrame(pendingFunc)
     }
     return setTimeout(pendingFunc, wait)

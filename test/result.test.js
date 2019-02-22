ebc1a4c884de96f1fcecd0387342865eed488649
Mateus Gabi Moreira <mateusgabimoreira@gmail.com>
2019-02-21 23:00:14 -0400

    Added testing cases

--- test/result.test.js
+++ test/result.test.js
@@ -1,32 +1,73 @@
-import assert from 'assert';
-import lodashStable from 'lodash';
-import { stubB } from './utils.js';
-import result from '../result.js';
+import assert from "assert";
+import lodashStable from "lodash";
+import result from "../result.js";
+import { stubB } from "./utils.js";
 
-describe('result', function() {
-  var object = { 'a': 1, 'b': stubB };
+describe("result", function() {
+  var object = { a: 1, b: stubB };
+  var nullObject = null;
+  var undefinedObject = undefined;
+  var emptyObject = {};
+  var nullInnerObject = { any: null };
+  var undefinedInnerObject = { any: undefined };
 
-  it('should invoke function values', function() {
-    assert.strictEqual(result(object, 'b'), 'b');
+  it("should invoke function values", function() {
+    assert.strictEqual(result(object, "b"), "b");
   });
 
-  it('should invoke default function values', function() {
-    var actual = result(object, 'c', object.b);
-    assert.strictEqual(actual, 'b');
+  it("should return undefined", function() {
+    assert.strictEqual(result(nullObject, "any"), undefined);
+    assert.strictEqual(result(undefinedObject, "any"), undefined);
+    assert.strictEqual(result(emptyObject, "any"), undefined);
+    assert.strictEqual(result(nullInnerObject, "any"), undefined);
+    assert.strictEqual(result(undefinedInnerObject, "any"), undefined);
   });
 
-  it('should invoke nested function values', function() {
-    var value = { 'a': lodashStable.constant({ 'b': stubB }) };
+  it("should return default value", function() {
+    assert.deepEqual(result(nullObject, "any", { default: true }), {
+      default: true
+    });
 
-    lodashStable.each(['a.b', ['a', 'b']], function(path) {
-      assert.strictEqual(result(value, path), 'b');
+    assert.deepEqual(result(undefinedObject, "any", { foo: "bar" }), {
+      foo: "bar"
+    });
+
+    assert.deepEqual(result(emptyObject, "any", { foo: "bizz" }), {
+      foo: "bizz"
+    });
+
+    assert.deepEqual(result(nullInnerObject, "any", { foo: "bizz" }), {
+      foo: "bizz"
+    });
+    assert.deepEqual(result(undefinedInnerObject, "any", { foo: "bizz" }), {
+      foo: "bizz"
     });
   });
 
-  it('should invoke deep property methods with the correct `this` binding', function() {
-    var value = { 'a': { 'b': function() { return this.c; }, 'c': 1 } };
+  it("should invoke default function values", function() {
+    var actual = result(object, "c", object.b);
+    assert.strictEqual(actual, "b");
+  });
 
-    lodashStable.each(['a.b', ['a', 'b']], function(path) {
+  it("should invoke nested function values", function() {
+    var value = { a: lodashStable.constant({ b: stubB }) };
+
+    lodashStable.each(["a.b", ["a", "b"]], function(path) {
+      assert.strictEqual(result(value, path), "b");
+    });
+  });
+
+  it("should invoke deep property methods with the correct `this` binding", function() {
+    var value = {
+      a: {
+        b: function() {
+          return this.c;
+        },
+        c: 1
+      }
+    };
+
+    lodashStable.each(["a.b", ["a", "b"]], function(path) {
       assert.strictEqual(result(value, path), 1);
     });
   });

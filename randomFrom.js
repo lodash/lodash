a941aa34f99567c7199b5d0e2b717f74ec5e2360
eslam-gl <48776169+eslam-gl@users.noreply.github.com>
2019-06-11 18:40:21 +0200

    Add randomFrom.js
    Useful for generating passwords, tokens or hex.

---
+++ randomFrom.js
@@ -0,0 +1,36 @@
+import flattenDeep from './flattenDeep.js'
+
+/**
+ * Produces a random string from a given collection.
+ *
+ * @param {Array|string} [collection=''] The collection to generate the string from.
+ * @param {number} [length=6] The length of the generated random string.
+ * @returns {string} Returns the random string.
+ * @example
+ *
+ * randomFrom('abcdefghijklmnopqrstuvwxyz !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~', 8)
+ * // => '@d!9$#gk'
+ *
+ * randomFrom(['0123456789', 'ABCDEF'])
+ * // => 'F6ABG9'
+ *
+ * randomFrom(['Ford', ['#b2fe94', ['!@(%)', ['42']]]], 16)
+ * => '#!)4dr(@!94e4)e@'
+ */
+function randomFrom(collection = '', length = 6) {
+  let result = ''
+  
+  if (collection.length == 0) {
+    return ''
+  }
+  
+  if (typeof collection == 'object' && typeof collection.length == 'number') {
+    collection = flattenDeep(collection).toString().replace(/,/g, '')
+  }
+
+  for (let i = 0; i < length; i++) {
+    result += collection[Math.floor(Math.random() * collection.length)]
+  }
+
+  return result
+}

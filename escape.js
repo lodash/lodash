dce7fccbb68c469565ced5ccf41fd5028e1ab730
John-David Dalton <john.david.dalton@gmail.com>
2019-07-02 16:08:37 -0700

    Add back accidentally removed semicolons to escape.js.

--- escape.js
+++ escape.js
@@ -1,10 +1,10 @@
 /** Used to map characters to HTML entities. */
 const htmlEscapes = {
-  '&': '&amp',
-  '<': '&lt',
-  '>': '&gt',
-  '"': '&quot',
-  "'": '&#39'
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;'
 }
 
 /** Used to match HTML entities and HTML characters. */
@@ -36,7 +36,7 @@ const reHasUnescapedHtml = RegExp(reUnescapedHtml.source)
  * @example
  *
  * escape('fred, barney, & pebbles')
- * // => 'fred, barney, &amp pebbles'
+ * // => 'fred, barney, &amp; pebbles'
  */
 function escape(string) {
   return (string && reHasUnescapedHtml.test(string))

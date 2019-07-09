e77d68121ff00ba86b53eed5893d35adfe94c9dd
John-David Dalton <john.david.dalton@gmail.com>
2019-07-09 10:14:16 -0700

    Rebuild lodash and docs.

--- lodash.js
+++ lodash.js
@@ -12,7 +12,7 @@
   var undefined;
 
   /** Used as the semantic version number. */
-  var VERSION = '4.17.11';
+  var VERSION = '4.17.12';
 
   /** Used as the size to enable large array optimizations. */
   var LARGE_ARRAY_SIZE = 200;

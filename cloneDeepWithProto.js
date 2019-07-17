f0b8cc0fd6883c23e5fd6332180673be24e873b5
gkio <webdevra@bk.ru>
2019-07-17 14:22:36 +0300

    clone deep with proto

---
+++ cloneDeepWithProto.js
@@ -0,0 +1,31 @@
+import baseClone from './.internal/baseClone.js'
+import isObject from './isObject.js'
+
+/** Used to compose bitmasks for cloning. */
+const CLONE_DEEP_FLAG = 1
+const CLONE_SYMBOLS_FLAG = 4
+
+/**
+ * This method is like `clone` except that it recursively clones `value`. and __proto__
+ * Object inheritance is preserved.
+ *
+ * @since 1.0.0
+ * @category Lang
+ * @param {*} value The value to recursively clone.
+ * @returns {*} Returns the deep cloned value with __proto__.
+ * @see clone
+ * @example
+ *
+ * const objects = [{ 'a': 1 }, { 'b': 2 }]
+ *
+ * const deep = cloneDeepWithProto(objects)
+ * console.log(deep[0] === objects[0])
+ * // => false
+ */
+function cloneDeepWithProto(value) {
+  return isObject(value) 
+    ? Object.assign({}, value) 
+    : baseClone(value, CLONE_DEEP_FLAG | CLONE_SYMBOLS_FLAG)
+}
+
+export default cloneDeepWithProto

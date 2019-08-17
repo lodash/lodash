56f60df4d4680a75d26876b12f579516c8c2c099
Luiz Américo <luizamericop@gmail.com>
2019-08-17 16:16:44 -0300

    Implement asciiWords and unicodeWords using String.match instead of binding RegExp.exec

--- words.js
+++ words.js
@@ -1,12 +1,15 @@
 import unicodeWords from './.internal/unicodeWords.js'
 
-const asciiWords = RegExp.prototype.exec.bind(
-  /[^\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+/g
-)
-
 const hasUnicodeWord = RegExp.prototype.test.bind(
   /[a-z][A-Z]|[A-Z]{2,}[a-z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]|[^a-zA-Z0-9 ]/
 )
+
+/** Used to match words composed of alphanumeric characters. */
+const reAsciiWord = /[^\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+/g
+
+function asciiWords(string) {
+  return string.match(reAsciiWord)
+}
 
 /**
  * Splits `string` into an array of its words.

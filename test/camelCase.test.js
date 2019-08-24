4a31d9db550863afef04482a1d4f361d62d312c5
Phap <centralparkloop@yahoo.com>
2019-08-24 15:05:12 -0400

    auto lint files

--- test/camelCase.test.js
+++ test/camelCase.test.js
@@ -1,28 +1,28 @@
-import assert from 'assert';
-import lodashStable from 'lodash';
-import camelCase from '../camelCase.js';
+import assert from 'assert'
+import lodashStable from 'lodash'
+import camelCase from '../camelCase.js'
 
-describe('camelCase', function() {
-  it('should work with numbers', function() {
-    assert.strictEqual(camelCase('12 feet'), '12Feet');
-    assert.strictEqual(camelCase('enable 6h format'), 'enable6HFormat');
-    assert.strictEqual(camelCase('enable 24H format'), 'enable24HFormat');
-    assert.strictEqual(camelCase('too legit 2 quit'), 'tooLegit2Quit');
-    assert.strictEqual(camelCase('walk 500 miles'), 'walk500Miles');
-    assert.strictEqual(camelCase('xhr2 request'), 'xhr2Request');
-  });
+describe('camelCase', () => {
+  it('should work with numbers', () => {
+    assert.strictEqual(camelCase('12 feet'), '12Feet')
+    assert.strictEqual(camelCase('enable 6h format'), 'enable6HFormat')
+    assert.strictEqual(camelCase('enable 24H format'), 'enable24HFormat')
+    assert.strictEqual(camelCase('too legit 2 quit'), 'tooLegit2Quit')
+    assert.strictEqual(camelCase('walk 500 miles'), 'walk500Miles')
+    assert.strictEqual(camelCase('xhr2 request'), 'xhr2Request')
+  })
 
-  it('should handle acronyms', function() {
-    lodashStable.each(['safe HTML', 'safeHTML'], function(string) {
-      assert.strictEqual(camelCase(string), 'safeHtml');
-    });
+  it('should handle acronyms', () => {
+    lodashStable.each(['safe HTML', 'safeHTML'], (string) => {
+      assert.strictEqual(camelCase(string), 'safeHtml')
+    })
 
-    lodashStable.each(['escape HTML entities', 'escapeHTMLEntities'], function(string) {
-      assert.strictEqual(camelCase(string), 'escapeHtmlEntities');
-    });
+    lodashStable.each(['escape HTML entities', 'escapeHTMLEntities'], (string) => {
+      assert.strictEqual(camelCase(string), 'escapeHtmlEntities')
+    })
 
-    lodashStable.each(['XMLHttpRequest', 'XmlHTTPRequest'], function(string) {
-      assert.strictEqual(camelCase(string), 'xmlHttpRequest');
-    });
-  });
-});
+    lodashStable.each(['XMLHttpRequest', 'XmlHTTPRequest'], (string) => {
+      assert.strictEqual(camelCase(string), 'xmlHttpRequest')
+    })
+  })
+})
--- test/upperCase.test.js
+++ test/upperCase.test.js
@@ -1,10 +1,10 @@
-import assert from 'assert';
-import upperCase from '../upperCase.js';
+import assert from 'assert'
+import upperCase from '../upperCase.js'
 
-describe('upperCase', function() {
-  it('should uppercase as space-separated words', function() {
-    assert.strictEqual(upperCase('--foo-bar--'), 'FOO BAR');
-    assert.strictEqual(upperCase('fooBar'), 'FOO BAR');
-    assert.strictEqual(upperCase('__foo_bar__'), 'FOO BAR');
-  });
-});
+describe('upperCase', () => {
+  it('should uppercase as space-separated words', () => {
+    assert.strictEqual(upperCase('--foo-bar--'), 'FOO BAR')
+    assert.strictEqual(upperCase('fooBar'), 'FOO BAR')
+    assert.strictEqual(upperCase('__foo_bar__'), 'FOO BAR')
+  })
+})

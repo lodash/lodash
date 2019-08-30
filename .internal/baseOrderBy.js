5df17774773c9b0368b89e9da8588644c740fefe
Mohit Y <42065586+Mohit21GoJs@users.noreply.github.com>
2019-08-31 00:38:49 +0400

    add order by behaviour with array similar to get method (#4453)
    * add order by behaviour with array similar to get method
    
    * remove typo for fixing orderby iteratee assignment
    
    * lint fixes
    
    * update test case
    
    * include package-lock from master
    
    * Add identity function for default iteratee

--- .internal/baseOrderBy.js
+++ .internal/baseOrderBy.js
@@ -1,7 +1,11 @@
 import baseEach from './baseEach.js'
 import baseSortBy from './baseSortBy.js'
+import baseGet from './baseGet.js'
 import compareMultiple from './compareMultiple.js'
 import isArrayLike from '../isArrayLike.js'
+
+// As existing identity function is in ../test/utils.js, so defining it here, it can be moved to utils
+const identity = value => value;
 
 /**
  * The base implementation of `orderBy` without param guards.
@@ -15,7 +19,12 @@ import isArrayLike from '../isArrayLike.js'
 function baseOrderBy(collection, iteratees, orders) {
   let criteriaIndex = -1
   let eachIndex = -1
-  iteratees = iteratees.length ? iteratees : [(value) => value]
+  iteratees = iteratees.length ? iteratees.map((iteratee) => {
+    if (Array.isArray(iteratee)) {
+      return (value) => baseGet(value, iteratee)
+    }
+    return iteratee
+  }) : [identity]
 
   const result = isArrayLike(collection) ? new Array(collection.length) : []
 

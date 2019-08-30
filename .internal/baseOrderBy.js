17df9e9bfe20fe18518ec5ab323a613669608407
Mohit Yadav <mohit@sovereignwallet.network>
2019-08-30 15:54:44 +0400

    fix 4438 code changes

--- .internal/baseOrderBy.js
+++ .internal/baseOrderBy.js
@@ -1,5 +1,6 @@
 import baseEach from './baseEach.js'
 import baseSortBy from './baseSortBy.js'
+import baseGet from './baseGet.js'
 import compareMultiple from './compareMultiple.js'
 import isArrayLike from '../isArrayLike.js'
 
@@ -15,7 +16,12 @@ import isArrayLike from '../isArrayLike.js'
 function baseOrderBy(collection, iteratees, orders) {
   let criteriaIndex = -1
   let eachIndex = -1
-  iteratees = iteratees.length ? iteratees : [(value) => value]
+  iteratees = iteratees.length ? iteratees.map((iteratee) => {
+    if(Array.isArray(iteratee)){
+      return value => baseGet(value, iteratee)
+    }
+    return iteratee;
+  }) : [(value) => value]
 
   const result = isArrayLike(collection) ? new Array(collection.length) : []
 

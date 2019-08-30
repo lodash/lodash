31684a5015e8974520b070a910d8d040cd00f31d
Mohit Yadav <mohit@sovereignwallet.network>
2019-08-30 13:12:36 +0400

    add file changes for style fixes

--- lodash.js
+++ lodash.js
@@ -3725,16 +3725,13 @@
         return baseUnary(getIteratee(enhancedIteratee));
       });
 
-      
       var result = baseMap(collection, function(value, key, collection) {
         var criteria = arrayMap(iteratees, function(iteratee) {
           return iteratee(value);
         });
-        
         return { 'criteria': criteria, 'index': ++index, 'value': value };
       });
 
-      
       return baseSortBy(result, function(object, other) {
         return compareMultiple(object, other, orders);
       });

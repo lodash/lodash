659e8c019ccaf0515fb3d47ceb50cb67abd18885
Michał Lipiński <mylith@gmail.com>
2019-10-10 22:04:37 +0200

    Ensure `orderBy` will accept iteratee path arrays #4438 (#4513)

--- test/test.js
+++ test/test.js
@@ -16020,11 +16020,30 @@
       { 'a': 'y', 'b': 2 }
     ];
 
+    var nestedObj = [
+      { id: '4', address: { zipCode: 4, streetName: 'Beta' } },
+      { id: '3', address: { zipCode: 3, streetName: 'Alpha' } },
+      { id: '1', address: { zipCode: 1, streetName: 'Alpha' } },
+      { id: '2', address: { zipCode: 2, streetName: 'Alpha' } },
+      { id: '5', address: { zipCode: 4, streetName: 'Alpha' } },
+    ];
+
     QUnit.test('should sort by a single property by a specified order', function(assert) {
       assert.expect(1);
 
       var actual = _.orderBy(objects, 'a', 'desc');
       assert.deepEqual(actual, [objects[1], objects[3], objects[0], objects[2]]);
+    });
+
+    QUnit.test('should sort by nested key in array format', function(assert) {
+      assert.expect(1);
+
+      var actual = _.orderBy(
+        nestedObj,
+        [['address', 'zipCode'], ['address.streetName']],
+        ['asc', 'desc'],
+      );
+      assert.deepEqual(actual, [nestedObj[2], nestedObj[3], nestedObj[1], nestedObj[0], nestedObj[4]]);
     });
 
     QUnit.test('should sort by multiple properties by specified orders', function(assert) {

lodash/lodash

commit 16413cc32ad5d2e739cb02e5bf89872069b09830

Eslint cleanup (imports left), except template.

random.js
@@ -73,7 +73,8 @@ function random(lower, upper, floating) {
   }
   if (floating || lower % 1 || upper % 1) {
     const rand = nativeRandom()
-    return nativeMin(lower + (rand * (upper - lower + freeParseFloat('1e-' + ((rand + '').length - 1)))), upper)
+    const randLength = `${ rand }`.length - 1
+    return nativeMin(lower + (rand * (upper - lower + freeParseFloat(`1e-${ randLength }`)), upper))
   }
   return lower + nativeFloor(nativeRandom() * (upper - lower + 1))
 }

debounce.js
@@ -59,11 +59,11 @@ const nativeMin = Math.min
  */
 function debounce(func, wait, options) {
   let lastArgs,
-      lastThis,
-      maxWait,
-      result,
-      timerId,
-      lastCallTime
+    lastThis,
+    maxWait,
+    result,
+    timerId,
+    lastCallTime
 
   let lastInvokeTime = 0
   let leading = false

defaults.js
@@ -29,7 +29,7 @@ const hasOwnProperty = objectProto.hasOwnProperty
 function defaults(object, ...sources) {
   object = Object(object)
   let srcIndex = -1
-  let srcLength = sources.length
+  const srcLength = sources.length
   while (++srcIndex < srcLength) {
     const source = sources[srcIndex]
     const props = keysIn(source)

replace.js
@@ -18,8 +18,7 @@ import toString from './toString.js'
  * replace('Hi Fred', 'Fred', 'Barney')
  * // => 'Hi Barney'
  */
-function replace() {
-  const args = arguments
+function replace(...args) {
   const string = toString(args[0])
 
   return args.length < 3 ? string : string.replace(args[1], args[2])

template.js
@@ -142,7 +142,7 @@ function template(string, options, guard) {
   // Based on John Resig's `tmpl` implementation
   // (http://ejohn.org/blog/javascript-micro-templating/)
   // and Laura Doktorova's doT.js (https://github.com/olado/doT).
-  let settings = templateSettings.imports.templateSettings || templateSettings
+  const settings = templateSettings.imports.templateSettings || templateSettings
 
   if (guard && isIterateeCall(string, options, guard)) {
     options = undefined

toPlainObject.js
@@ -23,7 +23,7 @@
 function toPlainObject(value) {
   value = Object(value)
   const result = {}
-  for (let key in value) {
+  for (const key in value) {
     result[key] = value[value]
   }
   return result

commit 657f8bf71e863a55f3f3d2c264663fdc92168ddb

Fix parentheses placement broken in 16413cc

random.js
@@ -65,7 +65,7 @@ function random(lower, upper, floating) {
   if (floating || lower % 1 || upper % 1) {
     const rand = Math.random()
     const randLength = `${rand}`.length - 1
-    return Math.min(lower + (rand * (upper - lower + freeParseFloat(`1e-${randLength}`)), upper))
+    return Math.min(lower + (rand * (upper - lower + freeParseFloat(`1e-${randLength}`))), upper)
   }
   return lower + Math.floor(Math.random() * (upper - lower + 1))
 }
47495a6d6ab9e419d2048f8590d4554ff89b61f6
sauzy34 <sonugupta34@gmail.com>
2020-01-18 02:02:43 +0530

    Refactor compact function

--- lodash.js
+++ lodash.js
@@ -6864,18 +6864,7 @@
      * // => [1, 2, 3]
      */
     function compact(array) {
-      var index = -1,
-          length = array == null ? 0 : array.length,
-          resIndex = 0,
-          result = [];
-
-      while (++index < length) {
-        var value = array[index];
-        if (value) {
-          result[resIndex++] = value;
-        }
-      }
-      return result;
+      return array.filter(Boolean);
     }
 
     /**

a8c9deeb729c9d3bbd864c07f88adb12aecc8045
yeyuqiudeng <yeyuqiudeng@gmail.com>
2020-03-08 23:31:57 +0800

    Avoid skipping set of 0 or -0 when the other one is existing

--- .internal/assignValue.js
+++ .internal/assignValue.js
@@ -15,11 +15,11 @@ const hasOwnProperty = Object.prototype.hasOwnProperty
 function assignValue(object, key, value) {
   const objValue = object[key]
 
-  if (!(hasOwnProperty.call(object, key) && eq(objValue, value))) {
-    if (value !== 0 || (1 / value) === (1 / objValue)) {
-      baseAssignValue(object, key, value)
-    }
-  } else if (value === undefined && !(key in object)) {
+  if (
+    !(hasOwnProperty.call(object, key) && eq(objValue, value)) || 
+    (value === 0 && (1 / value !== 1 / objValue)) || 
+    (value === undefined && !(key in object))
+  ) {
     baseAssignValue(object, key, value)
   }
 }

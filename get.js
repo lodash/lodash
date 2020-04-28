2230124b1341d20f59a53b420f2c6a4a9909cf16
Gustavo <gugurock50@gmail.com>
2020-04-28 14:12:14 -0300

    use shorthand in _.get() return

--- get.js
+++ get.js
@@ -26,7 +26,7 @@ import baseGet from './.internal/baseGet.js'
  */
 function get(object, path, defaultValue) {
   const result = object == null ? undefined : baseGet(object, path)
-  return result === undefined ? defaultValue : result
+  return result || defaultValue
 }
 
 export default get

c84fe82760fb2d3e03a63379b297a1cc1a2fce12
Jakub Mikulas <jakub@mikul.as>
2020-07-02 23:47:49 +0200

    fix(zipObjectDeep): prototype pollution (#4759)

--- lodash.js
+++ lodash.js
@@ -3990,6 +3990,10 @@
         var key = toKey(path[index]),
             newValue = value;
 
+        if (key === '__proto__' || key === 'constructor' || key === 'prototype') {
+          return object;
+        }
+
         if (index != lastIndex) {
           var objValue = nested[key];
           newValue = customizer ? customizer(objValue, key, nested) : undefined;

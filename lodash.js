a370ac81408de2da77a82b3c4b61a01a3b9c2fac
Mathias Bynens <mathias@qiwi.be>
2020-07-08 14:00:48 +0200

    Bump to v4.17.17

--- lodash.js
+++ lodash.js
@@ -12,7 +12,7 @@
   var undefined;
 
   /** Used as the semantic version number. */
-  var VERSION = '4.17.16';
+  var VERSION = '4.17.17';
 
   /** Used as the size to enable large array optimizations. */
   var LARGE_ARRAY_SIZE = 200;

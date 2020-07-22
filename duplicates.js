ea0074f8218d2c988662e5d719d4b29373759d9c
Vignesh Murugan <vignesh328@gmail.com>
2020-07-22 13:49:40 +0530

    feat: duplicates feature added

---
+++ duplicates.js
@@ -0,0 +1,25 @@
+import uniq from './uniq';
+import difference from './difference';
+
+
+/**
+ * Creates a duplicate version of an array, using
+ * [`SameValueZero`](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero)
+ *
+ *
+ * @since 0.1.0
+ * @category Array
+ * @param {Array} array The array to inspect.
+ * @returns {Array} Returns the new duplicates array.
+ * @example
+ *
+ * duplicates([2, 1, 2])
+ * // => [2]
+ */
+function duplicates(array) {
+    return (array != null && array.length)
+      ? difference(array, uniq(array)) 
+      : []
+  }
+
+export default duplicates

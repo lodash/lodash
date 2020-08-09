1ec661c91ea005f2517944b2daca6b2f10008789
David Anitoiu <david.anitoiu@gmail.com>
2020-08-09 18:15:02 +0200

    Added optional pattern to snakeCase
    * added usage with pattern example for snakeCase
    * replaced reduce with map & join
    * added tests for snakeCase

--- snakeCase.js
+++ snakeCase.js
@@ -8,6 +8,7 @@ import toString from './toString.js'
  * @since 3.0.0
  * @category String
  * @param {string} [string=''] The string to convert.
+ * @param {RegExp|string} [pattern] The pattern to match words.
  * @returns {string} Returns the snake cased string.
  * @see camelCase, lowerCase, kebabCase, startCase, upperCase, upperFirst
  * @example
@@ -23,11 +24,18 @@ import toString from './toString.js'
  *
  * snakeCase('foo2bar')
  * // => 'foo_2_bar'
+ *
+ * snakeCase('FOOBARv1.0',/([A-Z]+)(v)(\d).(\d)/)
+ * // => 'foobar_v_1_0'
+ *
+ * snakeCase('FooBARs',/([A-Z][a-z]+)(\w+)/)
+ * // => 'foo_bars'
  */
-const snakeCase = (string) => (
-  words(toString(string).replace(/['\u2019]/g, '')).reduce((result, word, index) => (
-    result + (index ? '_' : '') + word.toLowerCase()
-  ), '')
-)
+const snakeCase = (string, pattern) => {
+  const splitWords = words(toString(string).replace(/['\u2019]/g, ''), pattern)
+  const wordsToJoin = pattern ? splitWords.slice(1) : splitWords
+
+  return wordsToJoin.join('_').toLowerCase()
+}
 
 export default snakeCase

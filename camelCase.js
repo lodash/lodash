f529abcd9cbbd5f6e217cd2b6e0e1fa608f31551
David Anitoiu <david.anitoiu@gmail.com>
2020-08-09 18:25:43 +0200

    Added optional pattern to camelCase
    * added tests for optional pattern
    * added usage with pattern example for camelCase
    * added test for optional pattern

--- camelCase.js
+++ camelCase.js
@@ -8,6 +8,7 @@ import toString from './toString.js'
  * @since 3.0.0
  * @category String
  * @param {string} [string=''] The string to convert.
+ * @param {RegExp|string} [pattern] The pattern to match words.
  * @returns {string} Returns the camel cased string.
  * @see lowerCase, kebabCase, snakeCase, startCase, upperCase, upperFirst
  * @example
@@ -20,12 +21,18 @@ import toString from './toString.js'
  *
  * camelCase('__FOO_BAR__')
  * // => 'fooBar'
+ *
+ * camelCase('FooBARs',/([A-Z][a-z]+)(\w+)/)
+ * // => 'fooBars'
  */
-const camelCase = (string) => (
-  words(toString(string).replace(/['\u2019]/g, '')).reduce((result, word, index) => {
+const camelCase = (string, pattern) => {
+  const splitWords = words(toString(string).replace(/['\u2019]/g, ''), pattern)
+  const wordsToJoin = pattern ? splitWords.slice(1) : splitWords
+
+  return wordsToJoin.reduce((result, word, index) => {
     word = word.toLowerCase()
     return result + (index ? upperFirst(word) : word)
   }, '')
-)
+}
 
 export default camelCase

4df4ddd370a2d5a06e64e60e904928069db71b5c
Bread <60681223+vBread@users.noreply.github.com>
2020-10-08 17:58:25 -0400

    Create square.js

---
+++ square.js
@@ -0,0 +1,18 @@
+/**
+ * Squares a number.
+ * 
+ * @since 5.0.0
+ * @category Math
+ * @param {number} base The base number to square.
+ * @returns {number} Returns the squared value
+ * @example
+ * 
+ * square(4)
+ * // => 16
+ */
+
+function square(number) {
+  return number * number
+}
+
+export default square

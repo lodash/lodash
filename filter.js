c8915e4044b60f75e6e2cc27405714809c10fdb5
jingchaocheng <2422447171@qq.com>
2020-10-21 19:55:43 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -23,14 +23,13 @@
  */
 function filter(array, predicate) {
   let index = -1
-  let resIndex = 0
   const length = array == null ? 0 : array.length
   const result = []
 
   while (++index < length) {
     const value = array[index]
     if (predicate(value, index, array)) {
-      result[resIndex++] = value
+      result.push(value)
     }
   }
   return result

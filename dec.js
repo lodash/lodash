cb9a6d19b4ef2fbd9ddd2a3148e4c970f3407eae
Andros Fenollosa <andros@fenollosa.email>
2021-01-15 19:48:39 +0100

    Add decrease

---
+++ dec.js
@@ -0,0 +1,29 @@
+import isNumber from './isNumber.js'
+
+/**
+ * Decrease the value of a `number`.
+ *
+ * @since 4.17.15
+ * @category Math
+ * @param {number} number The number to decrease.
+ * @param {number} number Decrease.
+ * @returns {number} Returns the decrease.
+ * @example
+ *
+ * dec(4)
+ * // => 5
+ *
+ * dec(5, 3)
+ * // => 2
+ *
+ * dec(7.3)
+ * // => 6.3
+ *
+ * inc("foo")
+ * // => false
+ */
+function dec(number, decrease=1) {
+  return isNumber(number) ? number + decrease : false
+}
+
+export default dec

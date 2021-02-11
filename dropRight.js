86c725c260a9b9be19e09725b7fb9ff1fd8f4c36
Yevgen Gorbunkov <yevgen.gorbunkov@gmail.com>
2021-02-11 02:01:08 +0200

    based dropRight upon native slice-method

--- dropRight.js
+++ dropRight.js
@@ -1,6 +1,3 @@
-import slice from './slice.js'
-import toInteger from './toInteger.js'
-
 /**
  * Creates a slice of `array` with `n` elements dropped from the end.
  *
@@ -23,10 +20,7 @@ import toInteger from './toInteger.js'
  * dropRight([1, 2, 3], 0)
  * // => [1, 2, 3]
  */
-function dropRight(array, n=1) {
-  const length = array == null ? 0 : array.length
-  n = length - toInteger(n)
-  return length ? slice(array, 0, n < 0 ? 0 : n) : []
-}
+const dropRight = (array, n = 1) =>
+  n && n > 0 ? array.slice(0, -n) : array
 
 export default dropRight

d016f6b605d4bc3d6dfb12c06abdd52691beaa44
sangharsh <spravinkumarsovitkar@cainc.com>
2021-05-27 10:53:59 +0530

    wip

--- pascalCase.js
+++ pascalCase.js
@@ -7,7 +7,7 @@ import toString from './toString.js'
  * @since 3.0.0
  * @category String
  * @param {string} [string=''] The string to convert.
- * @returns {string} Returns the kebab cased string.
+ * @returns {string} Returns the pascal cased string.
  * @see camelCase, lowerCase, snakeCase, startCase, upperCase, upperFirst
  * @example
  *

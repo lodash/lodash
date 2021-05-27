6de9ae80073b7e61e9434f338fa9854feb5923e0
sangharsh <spravinkumarsovitkar@cainc.com>
2021-05-27 11:07:05 +0530

    code cleanup

--- pascalCase.js
+++ pascalCase.js
@@ -4,7 +4,6 @@ import toString from './toString.js'
 /**
  * Converts `string` to [pascal case](https://wiki.c2.com/?PascalCase).
  *
- * @since 3.0.0
  * @category String
  * @param {string} [string=''] The string to convert.
  * @returns {string} Returns the pascal cased string.

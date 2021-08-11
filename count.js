8ba6f1e2254265dddf274312d517b5cf4d7351cf
Craig Weiss <craigweiss28@gmail.com>
2021-08-10 21:06:59 -0700

    Add _.count

---
+++ count.js
@@ -0,0 +1,24 @@
+import countBy from './countBy.js'
+
+/**
+ * Creates an object composed of keys generated from the values of `Collection`.
+ * The corresponding value of each key is the number of times the key was
+ * returned.
+ *
+ * @since 4.17.0
+ * @category Collection
+ * @param {Array} collection The collection to iterate over.
+ * @returns {Object} Returns the composed aggregate object.
+ * @example
+ *
+ * const colors = ['blue', 'red', 'green', 'red', 'red', 'blue']
+ *
+ * countBy(users, value => value.active);
+ * // => { 'blue': 2, 'red': 3, 'green': 1 }
+ */
+function count(collection, iteratee) {
+  return countBy(collection, value => value)
+}
+
+export default count
+

9b11eb21a7ea37e9d64df7c481524ecbb1ee27fa
chenzhenfei <chenzhf@haier.com>
2021-08-23 23:34:14 +0800

    [init]

--- test/findLonest.test.js
+++ test/findLonest.test.js
@@ -1,14 +1,26 @@
-import assert from 'assert';
-import last from '../findLongest';
+import assert from 'assert'
+import last from '../findLongest'
 
 describe('last', function () {
 
   it('should return the longest element', function () {
-    var array = ['abc', 'abcd', 'abcde'];
-    assert.strictEqual(last(array), 'abcde');
-  });
+    var array = ['abc', 'abcd', 'abcde']
+    assert.strictEqual(last(array), 'abcde')
+  })
+
   it('should return the longest element', function () {
     var array = [];
-    assert.strictEqual(last(array), '');
-  });
-});
+    assert.strictEqual(last(array), undefined)
+  })
+
+  it('should return the longest element', function () {
+    var array = null;
+    assert.strictEqual(last(array), undefined)
+  })
+
+  it('should return the longest element', function () {
+    var array = undefined
+    assert.strictEqual(last(array), undefined)
+  })
+
+})

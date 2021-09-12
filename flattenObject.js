2bb95817b89f086b26243f28e3acf044b1ac1697
Ethan <eitanwass@gmail.com>
2021-09-12 14:52:56 +0300

    Fixed default value, and updated tests to reflect

--- flattenObject.js
+++ flattenObject.js
@@ -1,6 +1,9 @@
 import isPlainObject from './isPlainObject'
 import forEach from './forEach'
 import filter from './filter'
+
+const DEFAULT_DELIMITER = '.'
+
 /**
  * This method will flatten the given object onto a single layer,
  * with keys composed of the keys of each layer separated by a separator.
@@ -19,6 +22,8 @@ function flattenObject(object, delimiter) {
   if (!isPlainObject(object)) {
     throw new TypeError('Expected an object')
   }
+
+  delimiter = delimiter === undefined ? DEFAULT_DELIMITER : delimiter
 
   const output = {}
 
--- test/flattenObject.test.js
+++ test/flattenObject.test.js
@@ -9,12 +9,12 @@ describe('flattenObject', function() {
   });
 
   it('should return same simple one layer object', function() {
-    assert.deepStrictEqual(flattenObject({'a': 1}, '.'), {'a': 1});
+    assert.deepStrictEqual(flattenObject({'a': 1}), {'a': 1});
     assert.deepStrictEqual(flattenObject({'a': 1, 'b': 2}, '.'), {'a': 1, 'b': 2});
   });
 
   it('should flatten two layer object', function() {
-    assert.deepStrictEqual(flattenObject({'a': {'b': 1, 'c': 2}}, '.'), {'a.b': 1, 'a.c': 2});
-    assert.deepStrictEqual(flattenObject({'a': 1, 'b': {'c': 1, 'd': 2}}, '.'), {'a': 1, 'b.c': 1, 'b.d': 2});
+    assert.deepStrictEqual(flattenObject({'a': {'b': 1, 'c': 2}}), {'a.b': 1, 'a.c': 2});
+    assert.deepStrictEqual(flattenObject({'a': 1, 'b': {'c': 1, 'd': 2}}, ','), {'a': 1, 'b,c': 1, 'b,d': 2});
   });
 });

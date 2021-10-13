6372036dd60f10581b2b7b9465a94ae8da0ef88d
ruining.duan <ruining.duan@amh-group.com>
2021-10-13 16:18:19 +0800

    chore: 添加注释

--- compact.js
+++ compact.js
@@ -5,11 +5,14 @@
  * @since 0.1.0
  * @category Array
  * @param {Array} array The array to compact.
+ * @param {...Array} [values] The values to default.
  * @returns {Array} Returns the new array of filtered values.
  * @example
  *
  * compact([0, 1, false, 2, '', 3])
  * // => [1, 2, 3]
+ * compact([0, 1, false, 2, '',undefined, 3],['',undefined,NaN,null])
+ * // => [0,1,false, 2, 3]
  */
 function compact(array,falseyValues=[0,'',false,undefined,NaN,null]) {
   let resIndex = 0

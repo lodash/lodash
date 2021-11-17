a636d8fb33924751d149649a982d1f9f7d063f2c
harry <awakelife93@gmail.com>
2021-11-17 23:18:17 +0900

    add isValidateObject

---
+++ isValidateObject.js
@@ -0,0 +1,64 @@
+import every from './every';
+import isArrayLikeObject from './isArrayLikeObject.js';
+import isEmpty from './isEmpty';
+import isNumber from './isNumber';
+import isObjectLike from './isObjectLike.js';
+import keys from './keys';
+/**
+ * Checks if `object` is `whole non empty value`.
+ *
+ * @category Lang
+ * @param {Object} object The value to check.
+ * @returns {boolean} Returns `true` if `whole non empty value` is a `object`, else `false`.
+ * @example
+ * 
+ * isValidateObject([-1, -2, -3]) 
+ * // => true
+ *
+ * isValidateObject([0, 1, 2]) 
+ * // => true
+ *
+ * isValidateObject([1, 2, 3]) 
+ * // => true
+ *
+ * isValidateObject({a: 'foo', b: 'bar'}) 
+ * // => true
+ *
+ * isValidateObject([1, undefined, 3])
+ * // => false
+ *
+ * isValidateObject({a: 'foo', bar: undefined})
+ * // => false
+ *
+ * isValidateObject({a: undefined, bar: undefined})
+ * // => false
+ *
+ * isValidateObject(null)
+ * // => false
+ *
+ * isValidateObject('')
+ * // => false
+ *
+ * isValidateObject(undefined)
+ * // => false
+ */
+
+function isValidateObject(object) {
+  function validate(element) {
+    return isNumber(element) ? true : !isEmpty(element)
+  }
+  
+  if (isArrayLikeObject(object)) {
+    return every(object, (element) => validate(element))
+  }
+  
+  if (isObjectLike(object)) {
+    const _keys = keys(object);
+    if (_keys.length < 1) return false;
+    return every(_keys, (key) => validate(object[key]))
+  }
+  
+  return false;
+}
+
+export default isValidateObject
---
+++ test/isValidateObject.test.js
@@ -0,0 +1,58 @@
+import assert from 'assert';
+import isValidateObject from '../isValidateObject.js';
+import { falsey, lodashStable, realm, slice, stubFalse, stubTrue, symbol } from './utils.js';
+
+describe('isValidateObject', function() {
+  it('should return `true` for `object` values', function() {
+    assert.strictEqual(isValidateObject(['foo','bar','foobar']), true);
+    assert.strictEqual(isValidateObject([-1,-2,-3]), true);
+    assert.strictEqual(isValidateObject([0,1,2]), true);
+    assert.strictEqual(isValidateObject([1,2,3]), true);
+    assert.strictEqual(isValidateObject({a: 1, b: 2, c: 3}), true);
+    assert.strictEqual(isValidateObject({a: 'foo', b: 'bar'}), true);
+  });
+
+  it('should return `false` for non `object` values', function() {
+    var expected = lodashStable.map(falsey, stubFalse);
+
+    var actual = lodashStable.map(falsey, function(value, index) {
+      return index ? isValidateObject(value) : isValidateObject();
+    });
+
+    assert.deepStrictEqual(actual, expected);
+
+    assert.strictEqual(isValidateObject(['', '', '']), false);
+    assert.strictEqual(isValidateObject([null, null, null]), false);
+    assert.strictEqual(isValidateObject([undefined, undefined, undefined]), false);
+    assert.strictEqual(isValidateObject([null, undefined, null]), false);
+    assert.strictEqual(isValidateObject([1, 'foo', null]), false);
+    assert.strictEqual(isValidateObject([1, 'foo', undefined]), false);
+    assert.strictEqual(isValidateObject([1, undefined, 3]), false);
+    assert.strictEqual(isValidateObject([1, 'foo', '']), false);
+    assert.strictEqual(isValidateObject({a: 'foo', b: ''}), false);
+    assert.strictEqual(isValidateObject({a: 'foo', b: null}), false);
+    assert.strictEqual(isValidateObject({a: 'foo', b: undefined}), false);
+    assert.strictEqual(isValidateObject({a: undefined, b: undefined}), false);
+    assert.strictEqual(isValidateObject(true), false);
+    assert.strictEqual(isValidateObject(new Date), false);
+    assert.strictEqual(isValidateObject(new Error), false);
+    assert.strictEqual(isValidateObject(_), false);
+    assert.strictEqual(isValidateObject(slice), false);
+    assert.strictEqual(isValidateObject(undefined), false);
+    assert.strictEqual(isValidateObject(1), false);
+    assert.strictEqual(isValidateObject(/x/), false);
+    assert.strictEqual(isValidateObject('a'), false);
+
+    if (Symbol) {
+      assert.strictEqual(isValidateObject(symbol), false);
+    }
+  });
+
+  it('should work with `object` from another realm', function() {
+    var values = [realm.arguments, realm.array, realm.string],
+    expected = lodashStable.map(values, stubTrue),
+    actual = lodashStable.map(values, isValidateObject);
+
+    assert.deepStrictEqual(actual, expected);
+  });
+});

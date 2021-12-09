c797f817cc91431361a12bcc5bfb615a0e9e2697
Mohamadamin Karami <mohamadaminkarami.ce@gmail.com>
2021-12-09 14:48:11 +0330

    Fix: add options field to groupBy function omit the keys with id null or undefined

--- groupBy.js
+++ groupBy.js
@@ -15,19 +15,23 @@ const hasOwnProperty = Object.prototype.hasOwnProperty
  * @category Collection
  * @param {Array|Object} collection The collection to iterate over.
  * @param {Function} iteratee The iteratee to transform keys.
+ * @param {{allowNull?: boolean, allowUndefined?: boolean}} [options = { allowNull: true , allowUndefined: true }] Let the keys to be null or undefined. Defaults are true.
  * @returns {Object} Returns the composed aggregate object.
  * @example
  *
  * groupBy([6.1, 4.2, 6.3], Math.floor)
  * // => { '4': [4.2], '6': [6.1, 6.3] }
  */
-function groupBy(collection, iteratee) {
+function groupBy(collection, iteratee, options = {}) {
+  const { allowNull = true, allowUndefined = true } = options
   return reduce(collection, (result, value, key) => {
     key = iteratee(value)
     if (hasOwnProperty.call(result, key)) {
       result[key].push(value)
     } else {
-      baseAssignValue(result, key, [value])
+      if ((allowNull || key !== null) && (allowUndefined || key !== undefined)) {
+        baseAssignValue(result, key, [value])
+      }
     }
     return result
   }, {})

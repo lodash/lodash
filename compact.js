ad60b766047529525eda42958234d694ab4d8da7
arhemd <mehrad.dadar@gmail.com>
2021-12-24 22:26:24 +0330

    _.compact object support (#5339)

--- compact.js
+++ compact.js
@@ -11,20 +11,34 @@
  * compact([0, 1, false, 2, '', 3])
  * // => [1, 2, 3]
  */
-function compact(array) {
-  let resIndex = 0
-  const result = []
-
-  if (array == null) {
-    return result
-  }
-
-  for (const value of array) {
-    if (value) {
-      result[resIndex++] = value
+ function compact(input) {
+  if (Array.isArray(input)) {
+    let resIndex = 0
+    const result = []
+      
+    if (input == null) {
+      return result
     }
+      
+    for (const value of input) {
+      if (value) {
+        result[resIndex++] = value
+      }
+    }
+    return result
+  } else
+  if (typeof input == "object") {
+    const result = JSON.parse(JSON.stringify(input));
+
+    for (const property in result) {
+      if (!result[property]) {
+        delete result[property]
+      }
+    }
+
+    return result
+
   }
-  return result
 }
 
 export default compact

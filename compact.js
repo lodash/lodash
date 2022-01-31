3cf968d191c1abbd65b6fc58b3651a01e3e0ada4
Mahammad <56025606+mahammad-sixberries@users.noreply.github.com>
2022-01-31 20:52:54 +0530

    _.compact object support (#5339)

--- compact.js
+++ compact.js
@@ -1,29 +1,40 @@
 /**
- * Creates an array with all falsey values removed. The values `false`, `null`,
+ * Creates a collection with all falsey values removed. The values `false`, `null`,
  * `0`, `""`, `undefined`, and `NaN` are falsey.
  *
- * @since 0.1.0
+ * @since 4.17.21
  * @category Array
- * @param {Array} array The array to compact.
- * @returns {Array} Returns the new array of filtered values.
+ * @param {Array|Object} collection The collection to compact.
+ * @returns {Array|Object} Returns the new collection of filtered values.
  * @example
  *
  * compact([0, 1, false, 2, '', 3])
  * // => [1, 2, 3]
+ * compact({ a: 0, b: 1, c: false, d: 2, e: '', f: 3 })
+ * // => { b: 1, d: 2, f: 3 }
  */
-function compact(array) {
-  let resIndex = 0
-  const result = []
+function compact(input) {
+  let result
 
-  if (array == null) {
-    return result
-  }
+  if (Array.isArray(input)) {
+    result = []
 
-  for (const value of array) {
-    if (value) {
-      result[resIndex++] = value
+    let resIndex = 0
+    for (const value of input) {
+      if (value) {
+        result[resIndex++] = value
+      }
+    }
+  } else {
+    result = {}
+
+    for (const key in input) {
+      if (input[key]) {
+        result[key] = input[key]
+      }
     }
   }
+
   return result
 }
 

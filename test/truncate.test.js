a1f1eaaff4c1a2a1949989f97ca6612b2cf543b3
Matheus <matheussilva.dd@gmail.com>
2022-06-19 03:00:58 -0300

    refactor(truncate): adequate existent test cases to  path 2

--- test/truncate.test.js
+++ test/truncate.test.js
@@ -5,85 +5,96 @@ import truncate from "../truncate.js";
 
 describe.only("truncate", () => {
   const string = "hi-diddly-ho there, neighborino";
+  
+  describe("flux control structural tests", () => {
+    it("path::2::should accept a `sepator` str option", () => {
+      const result = truncate("32-chars-string------------   -", {
+        separator: " ",
+      });
 
-  it("path2", () => {
-    const result = truncate("32-chars-string------------   -", {
-      separator: " ",
+      assert.strictEqual(result, "32-chars-string------------...");
     });
 
-    assert.strictEqual(result, "32-chars-string------------...");
+    it("path::2::should accept a `sepator` regex option", () => {
+      const result = truncate("32-chars-string------------,  -", {
+        separator:  /,? +/,
+      });
+
+      assert.strictEqual(result, "32-chars-string------------...");
+    });
+
+    it("path::2::should accept a `sepator` global match regex option", () => {
+      const result = truncate("32-chars-string------------,  -", {
+        separator: /,? +/g,
+      });
+
+      assert.strictEqual(result, "32-chars-string------------...");
+    });
+
+    it("path3", () => {
+      const result = truncate("32-chars-string\u00E9-----------   -");
+
+      assert.strictEqual(result, "32-chars-string\u00E9-----------...");
+    });
+
+    it("should not truncate if `string` is <= `length` default value (30)", () => {
+      const result = truncate("small string");
+
+      assert.strictEqual(result, "small string");
+    });
+
+    it("path6", () => {
+      const result = truncate("32-chars-string-----------------");
+
+      assert.strictEqual(result, "32-chars-string------------...");
+    });
+
+    it("path8", () => {
+      const result = truncate("32-chars-string-----------------");
+
+      assert.strictEqual(result, "32-chars-string------------...");
+    });
   });
 
-  it("path3", () => {
-    const result = truncate("32-chars-string\u00E9-----------   -");
+  it('should use a default `length` of `30`', () => {
+    assert.strictEqual(truncate(string), 'hi-diddly-ho there, neighbo...')
+  })
 
-    assert.strictEqual(result, "32-chars-string\u00E9-----------...");
-  });
+  it('should not truncate if `string` is <= `length`', () => {
+    assert.strictEqual(truncate(string, { length: string.length }), string)
+    assert.strictEqual(truncate(string, { length: string.length + 2 }), string)
+  })
 
-  it("path4", () => {
-    const result = truncate("small string");
+  it('should truncate string the given length', () => {
+    assert.strictEqual(
+      truncate(string, { length: 24 }),
+      'hi-diddly-ho there, n...'
+    )
+  })
 
-    assert.strictEqual(result, "small string");
-  });
+  it('should support a `omission` option', () => {
+    assert.strictEqual(
+      truncate(string, { omission: ' [...]' }),
+      'hi-diddly-ho there, neig [...]'
+    )
+  })
 
-  it("path6", () => {
-    const result = truncate("32-chars-string-----------------");
+  it('should coerce nullish `omission` values to strings', () => {
+    assert.strictEqual(
+      truncate(string, { omission: null }),
+      'hi-diddly-ho there, neighbnull'
+    )
+    assert.strictEqual(
+      truncate(string, { omission: undefined }),
+      'hi-diddly-ho there, nundefined'
+    )
+  })
 
-    assert.strictEqual(result, '32-chars-string------------...');
-  });
+  it('should support a `length` option', () => {
+    assert.strictEqual(truncate(string, { length: 4 }), 'h...')
+  })
 
-  // it('should use a default `length` of `30`', () => {
-  //   assert.strictEqual(truncate(string), 'hi-diddly-ho there, neighbo...')
-  // })
 
-  // it('should not truncate if `string` is <= `length`', () => {
-  //   assert.strictEqual(truncate(string, { length: string.length }), string)
-  //   assert.strictEqual(truncate(string, { length: string.length + 2 }), string)
-  // })
-
-  // it('should truncate string the given length', () => {
-  //   assert.strictEqual(
-  //     truncate(string, { length: 24 }),
-  //     'hi-diddly-ho there, n...'
-  //   )
-  // })
-
-  // it('should support a `omission` option', () => {
-  //   assert.strictEqual(
-  //     truncate(string, { omission: ' [...]' }),
-  //     'hi-diddly-ho there, neig [...]'
-  //   )
-  // })
-
-  // it('should coerce nullish `omission` values to strings', () => {
-  //   assert.strictEqual(
-  //     truncate(string, { omission: null }),
-  //     'hi-diddly-ho there, neighbnull'
-  //   )
-  //   assert.strictEqual(
-  //     truncate(string, { omission: undefined }),
-  //     'hi-diddly-ho there, nundefined'
-  //   )
-  // })
-
-  // it('should support a `length` option', () => {
-  //   assert.strictEqual(truncate(string, { length: 4 }), 'h...')
-  // })
-
-  // it('should support a `separator` option', () => {
-  //   assert.strictEqual(
-  //     truncate(string, { length: 24, separator: ' ' }),
-  //     'hi-diddly-ho there,...'
-  //   )
-  //   assert.strictEqual(
-  //     truncate(string, { length: 24, separator: /,? +/ }),
-  //     'hi-diddly-ho there...'
-  //   )
-  //   assert.strictEqual(
-  //     truncate(string, { length: 24, separator: /,? +/g }),
-  //     'hi-diddly-ho there...'
-  //   )
-  // })
 
   // it('should treat negative `length` as `0`', () => {
   //   lodashStable.each([0, -2], (length) => {

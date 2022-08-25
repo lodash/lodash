ccf2a9b30538f79d18d69e5e36a2fd95a85869b6
sinakadkhodaei <kadkhodaeisina@gmail.com>
2022-08-26 00:14:39 +0430

    Add `predicate` argument to `hasDeepEmpty` function

--- hasDeepEmpty.js
+++ hasDeepEmpty.js
@@ -1,34 +1,35 @@
 import { isArrayLikeObject, isEmpty, isNumber, isPlainObject, map, some } from 'lodash'
 
 /**
- * Checks an `object` or `array` that has an empty value by the `isEmpty`
- * function.
- *
+ * Checks an `object` or `array` that has an empty value by the `isEmpty`function.
  * If there are numeric values in 1 levels deeper (or more) of an object (or array),
  * it means that it's not empty.
  *
- * Objects has empty value if they have no own enumerable string keyed
- * properties or contains at least an empty value which rejected with `isEmpty`.
+ * When a value known empty by `hasDeepEmpty` then `predicate` function invoked by
+ * `value`, `key` and `parent` arguments.
  *
- * Array-like values such as `arguments` objects, arrays, buffers, strings, or
- * jQuery-like collections are considered empty if they have a `length` of `0`
+ * Objects has empty value if they have no own enumerable string keyed properties
  * or contains at least an empty value which rejected with `isEmpty`.
+ *
+ * Array-Like-Object values such as `arguments` objects, arrays, buffers, or
+ * jQuery-like collections are considered empty if they have a `length` of `0`
+ * or contains at least one empty value that rejects with `isEmpty` (except Numerical values).
  *
  * @since 0.1.0
  * @category Lang
  * @param {*} value The value to check.
- * @returns {boolean} Returns `true` if `value` contains at least an empty value, else `false`.
+ * @param {Function} predicate If an empty value is detected, the function invoked by `value`, `key` and `parent` arguments.
+ * @returns {boolean} Returns `true` if `value` contains at least one empty value, else `false`.
  * @example
- *
- * const val = anyValueExceptObjectAndArray
- * hasDeepEmpty(val)
- * // => isEmpty(val)
  *
  * hasDeepEmpty([])
  * // => true
  *
  * hasDeepEmpty([undefined])
  * // => true
+ *
+ * hasDeepEmpty([1])
+ * // => false
  *
  * hasDeepEmpty([1, [null], 3])
  * // => true
@@ -44,12 +45,19 @@ import { isArrayLikeObject, isEmpty, isNumber, isPlainObject, map, some } from '
  *
  * hasDeepEmpty({ 'a': { 'b': 5, 'c': 6, 'd': [1, 2 ,3] } })
  * // => false
+ *
+ * hasDeepEmpty({ 'a': '' }, (v) => !isString(v))
+ * // => false
  */
-function hasDeepEmpty(value) {
+function hasDeepEmpty(value, predicate = null) {
+  return recursiveHasDeepEmpty(value, null, null, predicate)
+}
+
+function recursiveHasDeepEmpty(value, key, parent, predicate) {
   if (isEmpty(value)) {
-    return true
+    return predicate ? predicate(value, key, parent) : true
   } else if (isPlainObject(value) || isArrayLikeObject(value)) {
-    const emptyState = map(value, (v) => isNumber(v) ? false : hasDeepEmpty(v))
+    const emptyState = map(value, (v, k) => isNumber(v) ? false : recursiveHasDeepEmpty(v, k, value, predicate))
     return some(emptyState)
   }
 
--- test/hasDeepEmpty.test.js
+++ test/hasDeepEmpty.test.js
@@ -4,27 +4,56 @@ import { empties } from './utils.js'
 
 import hasDeepEmpty from '../hasDeepEmpty'
 import { describe, it } from 'mocha'
-import { isEmpty } from 'lodash'
+import { isArray, isEmpty, isNull } from 'lodash'
 
 describe('hasDeepEmpty', () => {
-  it('should works as `isEmpty` works on flat values', () => {
-    for (const empty of empties) {
-      assert.strictEqual(hasDeepEmpty(empty), isEmpty(empty))
-    }
+  describe('Without `predicate`', () => {
+    it('should works as `isEmpty` works on flat values', () => {
+      for (const empty of empties) {
+        assert.strictEqual(hasDeepEmpty(empty), isEmpty(empty))
+      }
+    })
+
+    it('should return `true` for empty values in nested object', () => {
+      const object = { a: { b: {} } }
+      assert.strictEqual(hasDeepEmpty(object), true)
+    })
+
+    it('should return `false` for non-empty values in nested object', () => {
+      const object = { a: { b: { c: 'text' }, d: ['a', 'b'] } }
+      assert.strictEqual(hasDeepEmpty(object), false)
+    })
+
+    it('should return `false` for numeric values in nested object', () => {
+      const object = { a: { b: { c: 1 }, d: [2, 3] } }
+      assert.strictEqual(hasDeepEmpty(object), false)
+    })
   })
 
-  it('should return `true` for empty values in nested object', () => {
-    const value = { a: { b: {} } }
-    assert.strictEqual(hasDeepEmpty(value), true)
-  })
+  describe('With `predicate`', () => {
+    it('should return `false` for null values in nested levels', () => {
+      const object = { a: { b: { c: null }, d: [2, null] } }
+      const result=hasDeepEmpty(object, (value) => !isNull(value))
 
-  it('should return `false` for non-empty values in nested object', () => {
-    const value = { a: { b: { c: 'text' }, d: ['a', 'b'] } }
-    assert.strictEqual(hasDeepEmpty(value), false)
-  })
+      assert.strictEqual(result, false)
+    })
 
-  it('should return `false` for numeric values in nested object', () => {
-    const value = { a: { b: { c: 1 }, d: [2, 3] } }
-    assert.strictEqual(hasDeepEmpty(value), false)
+    it('should return `false` if `value` is `null` and `parent` is an `array`', () => {
+      const object = { a: [null, 1] }
+      const result = hasDeepEmpty(object, (value, key, parent) => {
+        if (isArray(parent) && isNull(value)) {return false}
+        return true
+      })
+
+      assert.strictEqual(result, false)
+    })
+
+    it('should return `false` if `key` is not required', () => {
+      const object = { fn: '', ln: null, phone: '9' }
+      const requiredKeys = ['phone']
+
+      const result = hasDeepEmpty(object, (value, key) => requiredKeys.includes(key))
+      assert.strictEqual(result, false)
+    })
   })
 })

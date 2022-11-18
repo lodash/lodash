91f467f2b91f0a7d35080f2694c1f5acaff155bb
Congb19 <347006087@qq.com>
2022-11-18 16:03:07 +0800

    Fix _.round error with negative numbers

--- .internal/createRound.js
+++ .internal/createRound.js
@@ -8,17 +8,30 @@
 function createRound(methodName) {
   const func = Math[methodName]
   return (number, precision) => {
+    let isNegative = number < 0;
     precision = precision == null ? 0 : (precision >= 0 ? Math.min(precision, 292) : Math.max(precision, -292))
+    // Shift with exponential notation to avoid floating-point issues.
+    // See [MDN](https://mdn.io/round#Examples) for more details.
     if (precision) {
-      // Shift with exponential notation to avoid floating-point issues.
-      // See [MDN](https://mdn.io/round#Examples) for more details.
-      let pair = `${number}e`.split('e')
-      const value = func(`${pair[0]}e${+pair[1] + precision}`)
+      if (isNegative) {
+        let t = -number;
+        let pair = `${t}e`.split('e')
+        const value = func(`${pair[0]}e${+pair[1] + precision}`)
 
-      pair = `${value}e`.split('e')
-      return +`${pair[0]}e${+pair[1] - precision}`
+        pair = `${value}e`.split('e')
+        return -`${pair[0]}e${+pair[1] - precision}`
+      } else {
+        let pair = `${number}e`.split('e')
+        const value = func(`${pair[0]}e${+pair[1] + precision}`)
+
+        pair = `${value}e`.split('e')
+        return +`${pair[0]}e${+pair[1] - precision}`
+      }
     }
-    return func(number)
+    else {
+      if (isNegative) return -func(-number)
+      else return func(number)
+    }
   }
 }
 

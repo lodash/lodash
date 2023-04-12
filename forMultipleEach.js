6cbe885851c9d8db933fff0cfdd99a182913c31a
Gyanendra <gyanendrapanwar371@gmail.com>
2023-04-13 01:36:40 +0530

    Added forMultipleEach.js

---
+++ forMultipleEach.js
@@ -0,0 +1,39 @@
+import forEach from './forEach.js';
+import map from './map.js';
+
+/**
+ * Iterates over collection of `collections` and invokes `iteratee` for each element of collection.
+ * The iteratee is invoked with three arguments: (...items, index|key, collection).
+ *
+ *
+ * @category Collections
+ * @param {Array[Array]} collections The collections to iterate over.
+ * @param {Function} iteratee The function invoked per iteration.
+ * @returns {Array[Array]} Returns `collections`.
+ * @see forEach, map
+ * @example
+ *
+ * forEach([[1, 2], [11, 12]], (itemOne, itemTwo) => console.log(itemOne, " - ", itemTwo))
+ * // => Logs `1 - 11` then `2 - 22`.
+ */
+function forMultipleEach(collections, iteratee) {
+    let maxLength = 0;
+    forEach(collections, function (collection) {
+        if (collection.length > maxLength) {
+            maxLength = collection.length;
+        }
+    });
+
+    const items = function (index) {
+        return map(collections, function (collection) {
+            return collection[index];
+        });
+    };
+
+    for (let i = 0; i < maxLength; i += 1) {
+        iteratee(...items(i), i, collections);
+    }
+
+}
+
+export default forMultipleEach;

4b01c0baca5a74ca470b09aede598266aa9b0d65
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-25 10:37:51 +0530

    Update xorBy.js

--- test/xorBy.js
+++ test/xorBy.js
@@ -4,7 +4,7 @@ import xorBy from '../xorBy.js';
 
 describe('xorBy', function() {
   it('should accept an `iteratee`', function() {
-    var actual = xorBy([2.1, 1.2], [2.3, 3.4], Math.floor);
+    let actual = xorBy([2.1, 1.2], [2.3, 3.4], Math.floor);
     assert.deepStrictEqual(actual, [1.2, 3.4]);
 
     actual = xorBy([{ 'x': 1 }], [{ 'x': 2 }, { 'x': 1 }], 'x');
@@ -12,7 +12,7 @@ describe('xorBy', function() {
   });
 
   it('should provide correct `iteratee` arguments', function() {
-    var args;
+    let args;
 
     xorBy([2.1, 1.2], [2.3, 3.4], function() {
       args || (args = slice.call(arguments));

af8b135769943737aac9271dea8a501dede7dc1a
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-25 10:35:14 +0530

    Update zipWith.js

--- test/zipWith.js
+++ test/zipWith.js
@@ -6,17 +6,17 @@ import zip from '../zip.js';
 
 describe('zipWith', function() {
   it('should zip arrays combining grouped elements with `iteratee`', function() {
-    var array1 = [1, 2, 3],
+    let array1 = [1, 2, 3],
         array2 = [4, 5, 6],
         array3 = [7, 8, 9];
 
-    var actual = zipWith(array1, array2, array3, function(a, b, c) {
+    let actual = zipWith(array1, array2, array3, function(a, b, c) {
       return a + b + c;
     });
 
     assert.deepStrictEqual(actual, [12, 15, 18]);
 
-    var actual = zipWith(array1, [], function(a, b) {
+    let actual = zipWith(array1, [], function(a, b) {
       return a + (b || 0);
     });
 
@@ -24,7 +24,7 @@ describe('zipWith', function() {
   });
 
   it('should provide correct `iteratee` arguments', function() {
-    var args;
+    let args;
 
     zipWith([1, 2], [3, 4], [5, 6], function() {
       args || (args = slice.call(arguments));
@@ -34,12 +34,12 @@ describe('zipWith', function() {
   });
 
   it('should perform a basic zip when `iteratee` is nullish', function() {
-    var array1 = [1, 2],
+    let array1 = [1, 2],
         array2 = [3, 4],
         values = [, null, undefined],
         expected = lodashStable.map(values, lodashStable.constant(zip(array1, array2)));
 
-    var actual = lodashStable.map(values, function(value, index) {
+    let actual = lodashStable.map(values, function(value, index) {
       return index ? zipWith(array1, array2, value) : zipWith(array1, array2);
     });
 

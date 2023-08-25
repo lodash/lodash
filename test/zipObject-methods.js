ef9d0b5925a4a83ba61da5ef18514670e2245c65
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-25 10:36:25 +0530

    Update zipObject-methods.js

--- test/zipObject-methods.js
+++ test/zipObject-methods.js
@@ -4,12 +4,12 @@ import { _, LARGE_ARRAY_SIZE, square, isEven } from './utils.js';
 
 describe('zipObject methods', function() {
   lodashStable.each(['zipObject', 'zipObjectDeep'], function(methodName) {
-    var func = _[methodName],
+    let func = _[methodName],
         object = { 'barney': 36, 'fred': 40 },
         isDeep = methodName == 'zipObjectDeep';
 
     it('`_.' + methodName + '` should zip together key/value arrays into an object', function() {
-      var actual = func(['barney', 'fred'], [36, 40]);
+      let actual = func(['barney', 'fred'], [36, 40]);
       assert.deepStrictEqual(actual, object);
     });
 
@@ -23,13 +23,13 @@ describe('zipObject methods', function() {
 
     it('`_.' + methodName + '` should ' + (isDeep ? '' : 'not ') + 'support deep paths', function() {
       lodashStable.each(['a.b.c', ['a', 'b', 'c']], function(path, index) {
-        var expected = isDeep ? ({ 'a': { 'b': { 'c': 1 } } }) : (index ? { 'a,b,c': 1 } : { 'a.b.c': 1 });
+        let expected = isDeep ? ({ 'a': { 'b': { 'c': 1 } } }) : (index ? { 'a,b,c': 1 } : { 'a.b.c': 1 });
         assert.deepStrictEqual(func([path], [1]), expected);
       });
     });
 
     it('`_.' + methodName + '` should work in a lazy sequence', function() {
-      var values = lodashStable.range(LARGE_ARRAY_SIZE),
+      let values = lodashStable.range(LARGE_ARRAY_SIZE),
           props = lodashStable.map(values, function(value) { return 'key' + value; }),
           actual = _(props)[methodName](values).map(square).filter(isEven).take().value();
 

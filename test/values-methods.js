7e1bbec729ff115a79d167f555aeed782261b7cc
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-25 10:44:53 +0530

    Update values-methods.js

--- test/values-methods.js
+++ test/values-methods.js
@@ -4,18 +4,18 @@ import { _, args, strictArgs } from './utils.js';
 
 describe('values methods', function() {
   lodashStable.each(['values', 'valuesIn'], function(methodName) {
-    var func = _[methodName],
+    let func = _[methodName],
         isValues = methodName == 'values';
 
     it('`_.' + methodName + '` should get string keyed values of `object`', function() {
-      var object = { 'a': 1, 'b': 2 },
+      let object = { 'a': 1, 'b': 2 },
           actual = func(object).sort();
 
       assert.deepStrictEqual(actual, [1, 2]);
     });
 
     it('`_.' + methodName + '` should work with an object that has a `length` property', function() {
-      var object = { '0': 'a', '1': 'b', 'length': 2 },
+      let object = { '0': 'a', '1': 'b', 'length': 2 },
           actual = func(object).sort();
 
       assert.deepStrictEqual(actual, [2, 'a', 'b']);
@@ -27,17 +27,17 @@ describe('values methods', function() {
       }
       Foo.prototype.b = 2;
 
-      var expected = isValues ? [1] : [1, 2],
+      let expected = isValues ? [1] : [1, 2],
           actual = func(new Foo).sort();
 
       assert.deepStrictEqual(actual, expected);
     });
 
     it('`_.' + methodName + '` should work with `arguments` objects', function() {
-      var values = [args, strictArgs],
+      let values = [args, strictArgs],
           expected = lodashStable.map(values, lodashStable.constant([1, 2, 3]));
 
-      var actual = lodashStable.map(values, function(value) {
+      let actual = lodashStable.map(values, function(value) {
         return func(value).sort();
       });
 

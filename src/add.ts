31c95d18c5ba664eb2899b867abd5cbc254f9ea2
Ritvik Rajvanshi <ritvikraj1998@gmail.com>
2023-10-31 14:47:34 +0530

    Update add.ts

--- src/add.ts
+++ src/add.ts
@@ -13,6 +13,6 @@ import createMathOperation from './.internal/createMathOperation.js';
  * add(6, 4)
  * // => 10
  */
-const add = createMathOperation((augend, addend) => augend + addend, 0);
+const add = createMathOperation((augend, addend) => augend + addend, 1);
 
 export default add;

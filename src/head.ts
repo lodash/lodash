3bf805e100f4b0687a2fef2131dce91ae76e6369
Daniel Bayley <daniel.bayley@me.com>
2024-04-08 16:23:09 +0100

    feat(head): add optional n elements parameter

--- src/head.ts
+++ src/head.ts
@@ -1,22 +1,27 @@
 /**
- * Gets the first element of `array`.
+ * Gets the first [n] element(s) of `array`.
  *
  * @since 0.1.0
  * @alias first
  * @category Array
  * @param {Array} array The array to query.
- * @returns {*} Returns the first element of `array`.
+ * @param {Number} n The number of elements.
+ * @returns {*} Returns the first [n] element(s) of `array`.
  * @see last
  * @example
  *
  * head([1, 2, 3])
  * // => 1
  *
+ * head([1, 2, 3], 2)
+ * // => [1, 2]
+ *
  * head([])
  * // => undefined
  */
-function head(array) {
-    return array != null && array.length ? array[0] : undefined;
+function head(array = [], n = 1) {
+    const results = array.slice(0, n);
+    return results.length > 1 ? results : results[0];
 }
 
 export default head;

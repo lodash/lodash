22329d9af2ef543e6d5709d32dfd126575f7f99c
Daniel Bayley <daniel.bayley@me.com>
2024-04-08 16:47:27 +0100

    feat(last): add optional n elements parameter

--- src/last.ts
+++ src/last.ts
@@ -1,18 +1,22 @@
 /**
- * Gets the last element of `array`.
+ * Gets the last [n] element(s) of `array`.
  *
  * @since 0.1.0
  * @category Array
  * @param {Array} array The array to query.
- * @returns {*} Returns the last element of `array`.
+ * @param {Number} n The number of elements.
+ * @returns {*} Returns the last [n] element(s) of `array`.
  * @example
  *
  * last([1, 2, 3])
  * // => 3
+ *
+ * last([1, 2, 3], 2)
+ * // => [2, 3]
  */
-function last(array) {
-    const length = array == null ? 0 : array.length;
-    return length ? array[length - 1] : undefined;
+function last(array = [], n = 1) {
+    const results = array.slice(-n)
+    return results.length > 1 ? results : results[0]
 }
 
 export default last;

5feb6b9b0359eaa1dbae68dd981e7267da188a03
jdalton <john.david.dalton@gmail.com>
2025-12-17 10:26:47 -0500

    Bump lodash npm to v4.17.22

--- lodash.js
+++ lodash.js
@@ -12,7 +12,7 @@
   var undefined;
 
   /** Used as the semantic version number. */
-  var VERSION = '4.17.21';
+  var VERSION = '4.17.22';
 
   /** Used as the size to enable large array optimizations. */
   var LARGE_ARRAY_SIZE = 200;

dc0cac28469b352c0a0c9f232f8ffe14730ace4f
Jon Church <me@jonchurch.com>
2026-02-23 02:07:36 -0500

    review requests

--- lodash.js
+++ lodash.js
@@ -9239,10 +9239,9 @@
     }
 
     /**
-     * The opposite of `_.reject`; this method iterates over elements of
-     * `collection`, returning an array of all elements `predicate` returns
-     * truthy for. The predicate is invoked with three arguments:
-     * (value, index|key, collection).
+     * Iterates over elements of `collection`, returning an array of all elements
+     * `predicate` returns truthy for. The predicate is invoked with three
+     * arguments: (value, index|key, collection). The opposite of `_.reject`.
      *
      * **Note:** Unlike `_.remove`, this method returns a new array.
      *
@@ -15257,7 +15256,7 @@
     }
 
     /**
-     * The opposite of `_.escape`; this method converts the HTML entities
+     * The inverse of `_.escape`; this method converts the HTML entities
      * `&amp;`, `&lt;`, `&gt;`, `&quot;`, and `&#39;` in `string` to
      * their corresponding characters.
      *
